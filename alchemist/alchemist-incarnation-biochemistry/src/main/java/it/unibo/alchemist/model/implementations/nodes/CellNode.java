283152f892cd09c457ff313ca6f16463b3f7b519
Gabriele Graffieti <gabriele.graffieti@gmail.com>
2016-05-26 18:27:13 +0200

    add junction management in cell node

--- alchemist/alchemist-incarnation-biochemistry/src/main/java/it/unibo/alchemist/model/implementations/nodes/CellNode.java
+++ alchemist/alchemist-incarnation-biochemistry/src/main/java/it/unibo/alchemist/model/implementations/nodes/CellNode.java
@@ -9,6 +9,12 @@
  */
 package it.unibo.alchemist.model.implementations.nodes;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import it.unibo.alchemist.model.implementations.molecules.Junction;
 import it.unibo.alchemist.model.interfaces.Environment;
 import it.unibo.alchemist.model.interfaces.ICellNode;
 import it.unibo.alchemist.model.interfaces.Molecule;
@@ -19,6 +25,8 @@ import it.unibo.alchemist.model.interfaces.Molecule;
 public class CellNode extends DoubleNode implements ICellNode {
 
     private static final long serialVersionUID = 837704874534888283L;
+
+    private final List<Junction> junctionList = new ArrayList<>(0);
 
     /**
      * create a new cell node.
@@ -39,6 +47,51 @@ public class CellNode extends DoubleNode implements ICellNode {
             super.setConcentration(mol, c);
         } else {
             removeConcentration(mol);
+        }
+    }
+
+    /**
+     * 
+     * @return a unmodifiable list of junctions contained in this node.
+     */
+    public List<Junction> getJunctions() {
+        return Collections.unmodifiableList(junctionList);
+    }
+
+    /**
+     * Add a junction to the current node.
+     * @param j the junction
+     */
+    public void addJunction(final Junction j) {
+        junctionList.add(j);
+    }
+
+    /**
+     * Return true if a junction is present in the current node, false otherwise.
+     * Note: a junction is considered present if the method junction.equals(j) return true. 
+     * The neighbor node should NOT be considered in this comparison, depend on the implementation of junction.
+     * See {@link Junction#equals(Object)} for more details. 
+     * 
+     * @param j the junction
+     * @return true if the junction is present, false otherwise.
+     */
+    public boolean containsJunction(final Junction j) {
+        return junctionList.contains(j);
+    }
+
+    /**
+     * Remove a junction from this node. If the junction is not present do nothing
+     * @param j the junction to remove
+     * @param neighbor the node at the other side.
+     */
+    public void removeJunction(final Junction j, final CellNode neighbor) {
+        final Iterator<Junction> it = junctionList.iterator();
+        for (int i = 0; it.hasNext(); i++) {
+            final Junction jun = it.next();
+            if (jun.equals(j) && jun.getNeighborNode().get().equals(neighbor)) {
+                junctionList.remove(i);
+                return;
+            }
         }
     }
 

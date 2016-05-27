35a13a3814946f1d6c35783e3c7fc71f5a862eab
Gabriele Graffieti <gabriele.graffieti@gmail.com>
2016-05-27 10:31:11 +0200

    add junctions methods to the interface of CellNode

--- alchemist/alchemist-incarnation-biochemistry/src/main/java/it/unibo/alchemist/model/interfaces/ICellNode.java
+++ alchemist/alchemist-incarnation-biochemistry/src/main/java/it/unibo/alchemist/model/interfaces/ICellNode.java
@@ -8,8 +8,41 @@
  */
 package it.unibo.alchemist.model.interfaces;
 
+import java.util.List;
+
+import it.unibo.alchemist.model.implementations.molecules.Junction;
+import it.unibo.alchemist.model.implementations.nodes.CellNode;
+
 /**
  */
 public interface ICellNode extends Node<Double> {
-    // TODO
+    /**
+     * 
+     * @return the list of junctions contained in this node.
+     */
+    List<Junction> getJunctions();
+
+    /**
+     * Add a junction to the current node.
+     * @param j the junction
+     */
+    void addJunction(final Junction j);
+
+    /**
+     * Return true if a junction is present in the current node, false otherwise.
+     * Note: a junction is considered present if the method junction.equals(j) return true. 
+     * The neighbor node should NOT be considered in this comparison, it depends on the implementation of junction.
+     * See {@link Junction#equals(Object)} for more details. 
+     * 
+     * @param j the junction
+     * @return true if the junction is present, false otherwise.
+     */
+    boolean containsJunction(final Junction j);
+
+    /**
+     * Remove a junction from this node.
+     * @param j the junction to remove
+     * @param neighbor the node at the other side of the junction.
+     */
+    void removeJunction(final Junction j, final CellNode neighbor);
 }

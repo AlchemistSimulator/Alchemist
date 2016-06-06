b97c04fa49ad2ccbd51089610c1d2fd7e75de716
Gabriele Graffieti <gabriele.graffieti@gmail.com>
2016-06-06 19:16:19 +0200

    create removeJunctionInCell class

--- alchemist/alchemist-incarnation-biochemistry/src/main/java/it/unibo/alchemist/model/implementations/actions/RemoveJunctionInCell.java
+++ alchemist/alchemist-incarnation-biochemistry/src/main/java/it/unibo/alchemist/model/implementations/actions/RemoveJunctionInCell.java
@@ -9,40 +9,70 @@
 package it.unibo.alchemist.model.implementations.actions;
 
 import it.unibo.alchemist.model.interfaces.Context;
+import it.unibo.alchemist.model.interfaces.Environment;
+import it.unibo.alchemist.model.interfaces.ICellNode;
+
+import org.apache.commons.math3.random.RandomGenerator;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import it.unibo.alchemist.model.implementations.molecules.Junction;
-import it.unibo.alchemist.model.interfaces.Action;
 import it.unibo.alchemist.model.interfaces.Node;
 import it.unibo.alchemist.model.interfaces.Reaction;
 
 /**
+ * Represent the action of removing a junction between the current node and a neighbor. <br/>
+ * This action only remove the junction reference inside this node, the neighbor totally ignore 
+ * that a junction has been removed. <br/>
+ * This is a part of the junction remotion process. <br/>
+ * See {@link RemoveJunctionInNeighbor} for the other part of the process
  */
-public class RemoveJunctionInCell extends AbstractAction<Double> {
+public class RemoveJunctionInCell extends AbstractNeighborAction<Double> {
 
     private static final long serialVersionUID = 3565077605882164314L;
+
+    private final Junction jun;
+    private final Environment<Double> env;
+    @SuppressFBWarnings(value = "SE_BAD_FIELD", justification = "All provided RandomGenerator implementations are actually Serializable")
+    private final RandomGenerator rand;
+    private final ICellNode node;
 
     /**
      * 
      * @param junction the junction
-     * @param node the node where the action is performed
+     * @param n the node where the action is performed
+     * @param e the environment
+     * @param rg the random generator
      */
-    public RemoveJunctionInCell(final Junction junction, final Node<Double> node) {
-        super(node);
-        // TODO Auto-generated constructor stub
+    public RemoveJunctionInCell(final Junction junction, final ICellNode n, final Environment<Double> e, final RandomGenerator rg) {
+        super(n, e, rg);
+        jun = junction;
+        node = n;
+        env = e;
+        rand = rg;
     }
 
     @Override
-    public Action<Double> cloneOnNewNode(final Node<Double> n, final Reaction<Double> r) {
+    public RemoveJunctionInCell cloneOnNewNode(final Node<Double> n, final Reaction<Double> r) {
         // TODO Auto-generated method stub
-        return null;
+        return new RemoveJunctionInCell(jun, (ICellNode) n, env, rand);
     }
 
+    /**
+     * If no target node is given DO NOTHING. The junction can not be removed.
+     */
     @Override
-    public void execute() {
-        // TODO Auto-generated method stub
-    }
+    public void execute() { }
 
     @Override
     public Context getContext() {
-        return Context.LOCAL; // TODO this is just a stub
+        return Context.NEIGHBORHOOD; // TODO try local
+    }
+
+    /**
+     * Removes the junction that links the node where this action is executed and the target node. 
+     */
+    @Override
+    public void execute(final Node<Double> targetNode) { 
+        node.removeJunction(jun, (ICellNode) targetNode);
     }
 }

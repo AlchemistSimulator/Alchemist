f36d6d97668848b3df6e553eda066f57b511d805
Gabriele Graffieti <gabriele.graffieti@gmail.com>
2016-06-07 00:20:29 +0200

    Fix a bug in tests, it wasn't considered that the creation and remotion of a junction inject 2 actions in the reaction

--- alchemist/alchemist-incarnation-biochemistry/src/test/java/it/unibo/alchemist/test/TestIncarnation.java
+++ alchemist/alchemist-incarnation-biochemistry/src/test/java/it/unibo/alchemist/test/TestIncarnation.java
@@ -148,11 +148,11 @@ public class TestIncarnation {
         testR("[A] --> [B] + [C + D in env]", 1, 4, 1, 2, 0, 0, 0, 2);
         testR("[A] + [B + 2C in neighbor] + [D in env] --> [E in cell] + [F + 4G in env]", 4, 7, 1, 2, 2, 2, 1, 3);
         testR("[A in env] + [B in env] + [C + 4D in neighbor] --> [E + F + G]", 4, 7, 0, 3, 2, 2, 2, 2);
-        testR("[A] + [B in neighbor] --> [junction A-B]", 2, 3, 1, 2, 1, 1, 0, 0);
-        testR("[A + 3B] + [C in neighbor] + [D in env] --> [junction A:3B-C] + [D in env]", 4, 6, 2, 3, 1, 1, 1, 2);
-        testR("[junction A-B] + [A in cell] --> [A in env]", 2, 3, 2, 2, 0, 0, 0, 1);
+        testR("[A] + [B in neighbor] --> [junction A-B]", 2, 4, 1, 2, 1, 2, 0, 0);
+        testR("[A + 3B] + [C in neighbor] + [D in env] --> [junction A:3B-C] + [D in env]", 4, 7, 2, 3, 1, 2, 1, 2);
+        testR("[junction A-B] + [A in cell] --> [A in env]", 2, 4, 2, 2, 0, 1, 0, 1);
         testR("[A in env] + [B + 2C] + [junction A-B] --> [junction A-B] + [D in neighbor]", 4, 4, 3, 2, 0, 1, 1, 1); // the junction is not removed like a biomolecule if the same junction is present in the right side.
-        testR("[junction A-B] + [junction C-D] --> [A in env]", 2, 3, 2, 2, 0, 0, 0, 1); // the junctions will be removed, because they are not present in the right side.
+        testR("[junction A-B] + [junction C-D] --> [A in env]", 2, 5, 2, 2, 0, 2, 0, 1); // the junctions will be removed, because they are not present in the right side.
         testR("[junction A-B] --> [junction A-B] + [A in cell]", 1, 1, 1, 1, 0, 0, 0, 0);
         testR("[A + B] --> [BrownianMove(0.1)]", 2, 3, 2, 2, 0, 0, 0, 0);
         testR("[] --> [B in env] if BiomolPresentInCell(A, 2)", 1, 1, 1, 0, 0, 0, 0, 1); // if a custom condition is used the molecules present in the custom condition will NOT be removed.

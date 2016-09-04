5425dc651375b6a966f50ed1efa6cac30efc2072
mfrancia <matteo.francia2@studio.unibo.it>
2016-09-04 11:41:39 +0200

    added serialization test

--- alchemist/alchemist-swingui/src/test/java/it/unibo/alchemist/test/TestEffectSerialization.java
+++ alchemist/alchemist-swingui/src/test/java/it/unibo/alchemist/test/TestEffectSerialization.java
@@ -1,5 +1,6 @@
 package it.unibo.alchemist.test;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.danilopianini.io.FileUtilities;
@@ -7,6 +8,7 @@ import org.junit.Test;
 
 import it.unibo.alchemist.boundary.gui.effects.Effect;
 import it.unibo.alchemist.boundary.gui.effects.EffectFactory;
+import it.unibo.alchemist.boundary.gui.effects.EffectSerializationFactory;
 import it.unibo.alchemist.boundary.wormhole.implementation.Wormhole2D;
 
 /**
@@ -15,17 +17,32 @@ import it.unibo.alchemist.boundary.wormhole.implementation.Wormhole2D;
 public class TestEffectSerialization {
 
     private static final String FILEPATH = "test.aes";
+    private static final File FILE = new File(FILEPATH);
+    private static final Effect E = EffectFactory.buildDefaultEffect();
+
+    /**
+     * Make sure that effects can be (de) serialized with Gson.
+     * 
+     * @throws IOException
+     *             in case of errors
+     */
+    @Test
+    public void testGsonEffectSerialization() throws IOException {
+        EffectSerializationFactory.effectToFile(FILE, E);
+        EffectSerializationFactory.effectsFromFile(FILE);
+    }
 
     /**
      * Make sure that effects can be (de) serialized.
      * 
-     * @throws IOException in case of errors
-     * @throws ClassNotFoundException if some very serious bug happen
+     * @throws IOException
+     *             in case of errors
+     * @throws ClassNotFoundException
+     *             if some very serious bug happen
      */
     @Test
     public void testDefaultEffectSerialization() throws IOException, ClassNotFoundException {
-        final Effect e = EffectFactory.buildDefaultEffect();
-        FileUtilities.objectToFile(e, FILEPATH, false);
+        FileUtilities.objectToFile(E, FILEPATH, false);
         FileUtilities.fileToObject(FILEPATH);
     }
 

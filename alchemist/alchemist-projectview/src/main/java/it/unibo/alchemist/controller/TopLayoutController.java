07a63f2791c9f80135793e9c76d04f74438306d3
Elisa Casadio <elisa.casadio7@studio.unibo.it>
2016-11-06 12:14:15 +0100

    Add method for import button.

--- alchemist/alchemist-projectview/src/main/java/it/unibo/alchemist/controller/TopLayoutController.java
+++ alchemist/alchemist-projectview/src/main/java/it/unibo/alchemist/controller/TopLayoutController.java
@@ -1,10 +1,52 @@
 package it.unibo.alchemist.controller;
 
+import java.io.File;
+//import java.io.IOException;
+
+import it.unibo.alchemist.Main;
+import javafx.fxml.FXML;
+import javafx.stage.DirectoryChooser;
+
 /**
  * 
  *
  */
 public class TopLayoutController {
 
+    private Main main;
+
+    /**
+     * Sets the main class.
+     * @param main main class
+     */
+    public void setMain(final Main main) {
+        this.main = main;
+    }
+
+    @FXML
+    private void clickImport() {
+        final DirectoryChooser dirChooser = new DirectoryChooser();
+        dirChooser.setTitle("Import project folder");
+        dirChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        File dir = dirChooser.showDialog(this.main.getStage());  // eccezione se non si seleziona nulla!
+        System.out.println("base: " + dir.getName());
+        displayDirectoryContent(dir);
+    }
+
+    private void displayDirectoryContent(final File dir) {
+        //try {
+            File[] files = dir.listFiles();
+            for (File file: files) {
+                if (file.isDirectory()) {
+                    System.out.println("   directory: " + file.getName());
+                    displayDirectoryContent(file);
+                } else {
+                    System.out.println("      file: " + file.getName());
+                }
+            }
+        /*} catch (IOException e) {
+            e.printStackTrace();
+        }*/
+    }
 
 }

3c71a7be9473684a5bfb3a940d581e9670a48060
Danilo Pianini <danilo.pianini@gmail.com>
2016-12-31 15:29:05 +0100

    Remove useless modifiers

--- alchemist/alchemist-implementationbase/src/main/java/it/unibo/alchemist/boundary/monitors/EnvironmentSampler.java
+++ alchemist/alchemist-implementationbase/src/main/java/it/unibo/alchemist/boundary/monitors/EnvironmentSampler.java
@@ -89,10 +89,10 @@ public abstract class EnvironmentSampler<S, T> extends EnvironmentInspector<T> {
      * @return an {@link Iterable} along samples
      */
     protected abstract Iterable<S> computeSamples(
-            final Environment<T> env,
-            final Reaction<T> r,
-            final Time time,
-            final long step);
+            Environment<T> env,
+            Reaction<T> r,
+            Time time,
+            long step);
 
     private static void expandList(final List<TDoubleCollection> vpn, final int i) {
         if (i >= vpn.size()) {

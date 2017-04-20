d73a102c55d73b60267d417fba71404b9ac4b98b
Danilo Pianini <danilo.pianini@unibo.it>
2017-04-20 11:40:22 +0200

    Reformat comment

--- alchemist/alchemist-runner/src/main/java/it/unibo/alchemist/AlchemistRunner.java
+++ alchemist/alchemist-runner/src/main/java/it/unibo/alchemist/AlchemistRunner.java
@@ -251,13 +251,13 @@ public final class AlchemistRunner {
                     .map(Optional::get)
                     .findAny();
             /*
-             * findAny does NOT short-circuit the stream due to a known
-             * bug in the JDK: https://bugs.openjdk.java.net/browse/JDK-8075939
-             * Thus, to date, if an exception occurs in a thread
-             * which is running a simulation, that exception will be effectively
-             * thrown outside that thread only when all the threads 
-             * have completed their execution.
-             * Blame Oracle for this.
+             * findAny does NOT short-circuit the stream due to a known bug in
+             * the JDK: https://bugs.openjdk.java.net/browse/JDK-8075939
+             * 
+             * Thus, to date, if an exception occurs in a thread which is
+             * running a simulation, that exception will be effectively thrown
+             * outside that thread only when all the threads have completed
+             * their execution. Blame Oracle for this.
              */
             start.ifPresent(s -> System.out.printf("Total simulation running time (nanos): %d \n", (System.nanoTime() - s))); //NOPMD: I want to show the result in any case
             executor.shutdown();

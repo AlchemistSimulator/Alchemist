361591f3795ed23ff282d79f37daa8ba944de835
Niccolò Maltoni <niccolo.maltoni@studio.unibo.it>
2017-05-02 15:59:57 +0200

    Continued implementation of simulator GUI (FXML and CSS)
    Removed small PMD and Checkstyle warnings from Java code and added
    modules to FXML file; styled some modules with CSS

--- alchemist/alchemist-swingui/src/main/java/it/unibo/alchemist/boundary/gui/MainApp.java
+++ alchemist/alchemist-swingui/src/main/java/it/unibo/alchemist/boundary/gui/MainApp.java
@@ -1,35 +1,45 @@
 package it.unibo.alchemist.boundary.gui;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+/**
+ * Main class to start an empty simulator visualization.
+ */
 public class MainApp extends Application {
     private Stage primaryStage;
     private FXResourceLoader loader;
-    private BorderPane rootLayout;
+    private Pane rootLayout;
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(final Stage primaryStage) throws Exception {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("Alchemist Simulator base UI");
-        
+
         this.initRootLayout();
-        this.primaryStage.setScene(new Scene(this.rootLayout, 640, 480));
+        this.primaryStage.setScene(new Scene(this.rootLayout));
         this.primaryStage.show();
     }
 
-    private void initRootLayout() throws Exception {
+    private void initRootLayout() throws IOException, NoLayoutSpecifiedException {
         if (this.loader == null) {
             this.loader = new FXResourceLoader();
         }
 
         this.loader.setLayoutName(FXResourceLoader.DefaultLayout.ROOT_LAYOUT.getName());
-        this.rootLayout = this.loader.getLayout(BorderPane.class);
+        this.rootLayout = this.loader.getLayout(AnchorPane.class);
     }
 
-    public static void main(String[] args) {
+    /**
+     * Method that launches the application.
+     * @param args arguments
+     */
+    public static void main(final String[] args) {
         launch(args);
     }
 }

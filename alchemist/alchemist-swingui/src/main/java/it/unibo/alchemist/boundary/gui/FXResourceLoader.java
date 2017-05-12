b29b3c5522d92f02e944676d3b60a61e26bf4176
Niccolò Maltoni <niccolo.maltoni@studio.unibo.it>
2017-05-12 09:16:50 +0200

    Better FXResourceLoader

--- alchemist/alchemist-swingui/src/main/java/it/unibo/alchemist/boundary/gui/FXResourceLoader.java
+++ alchemist/alchemist-swingui/src/main/java/it/unibo/alchemist/boundary/gui/FXResourceLoader.java
@@ -6,8 +6,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import jiconfont.IconCode;
+import jiconfont.icons.GoogleMaterialDesignIcons;
+import jiconfont.javafx.IconFontFX;
 import jiconfont.javafx.IconNode;
 
+/**
+ * Utility class for loading resources related to layout.
+ */
 public final class FXResourceLoader {
     private static final String XML_RESOURCE_PATH = "/it/unibo/alchemist/gui/view/";
     private static final String EXTENSION = ".fxml";
@@ -19,31 +24,55 @@ public final class FXResourceLoader {
     }
 
     /**
-     * The same as call the default constructor and
-     * {@link #setLayoutName(String)}.
+     * Loads a layout FXML file returning the base pane defined by the layout.
      * 
+     * @param <T>
+     *            the generic type of pane
+     * @param paneInstance
+     *            the class of pain to load
+     * @param controller
+     *            the controller to associate to that layout
      * @param layoutName
-     *            the layout name; if null or empty String, the parameter will
-     *            be unset.
+     *            the name of the layout; it should be the file name without
+     *            extension
+     * @return the pane defined by the layout
      * @throws IOException
+     *             if it cannot load the file for some reason
      */
+    @SuppressWarnings("unchecked") // Passing a wrong class would be stupid
     public static <T extends Pane> T getLayout(final Class<T> paneInstance, final Object controller, final String layoutName)
             throws IOException {
-        FXMLLoader loader = new FXMLLoader();
-        // this.setLayoutName(layoutName);
+        final FXMLLoader loader = new FXMLLoader();
         loader.setLocation(FXResourceLoader.class.getResource(XML_RESOURCE_PATH + layoutName + EXTENSION));
         loader.setController(controller);
         return (T) loader.load();
     }
 
+    /**
+     * Returns the standard message for a problem in class injection from FXML.
+     * 
+     * @param nodeName
+     *            the name of the node that was not injected
+     * @param layoutFileName
+     *            the layout file name
+     * @return the message String
+     */
     public static String getInjectionErrorMessage(final String nodeName, final String layoutFileName) {
-        return new StringBuilder("fx:id=\"").append(nodeName).append("\" was not injected: check your FXML file '").append(layoutFileName)
-                .append("'").toString();
+        return "fx:id=\"" + nodeName + "\" was not injected: check your FXML file \"" + layoutFileName + "\"";
 
     }
 
+    /**
+     * Loads an icon from Google Material Design Icons filled in white.
+     * 
+     * @param iconCode
+     *            the IconCode (from {@link GoogleMaterialDesignIcons}
+     * @return the IconNode with the specified icon
+     */
     public static IconNode getWhiteIcon(final IconCode iconCode) {
-        IconNode icon = new IconNode(iconCode);
+        IconFontFX.register(GoogleMaterialDesignIcons.getIconFont());
+
+        final IconNode icon = new IconNode(iconCode);
         icon.setFill(Color.WHITE);
         return icon;
     }

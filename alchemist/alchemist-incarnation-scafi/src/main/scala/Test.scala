e8e8f741cfb3e4639705e57a23808691536076a4
Danilo Pianini <danilo.pianini@unibo.it>
2017-05-26 14:36:06 +0200

    Implement a mini-application test

--- alchemist/alchemist-incarnation-scafi/src/main/scala/Test.scala
+++ alchemist/alchemist-incarnation-scafi/src/main/scala/Test.scala
@@ -1,3 +1,28 @@
-class Test extends App {
-  println("Cazzoduro")
+import it.unibo.scafi.incarnations.BasicAbstractIncarnation
+
+object ScafiIncarnationForAlchemist  extends BasicAbstractIncarnation {
+}
+
+import ScafiIncarnationForAlchemist.factory
+import ScafiIncarnationForAlchemist.AggregateProgram
+import ScafiIncarnationForAlchemist.NBR_RANGE_NAME
+import ScafiIncarnationForAlchemist.ContextImpl
+
+object MyAggregateProgram extends AggregateProgram {
+  def gradient(source: Boolean): Double =
+    rep(Double.PositiveInfinity){
+      println("traaaaaaaaaaaaaiaaaaa")
+      distance => mux(source) { 0.0 } {
+        foldhood(Double.MaxValue)(Math.min)(nbr{distance}+nbrvar[Double](NBR_RANGE_NAME))
+      }
+    }
+
+  def isSource = sense[Boolean]("source")
+
+  override def main() = gradient(isSource)
+}
+
+
+object MyTest extends App {
+ println(MyAggregateProgram.round(new ContextImpl(0, Map(), Map("source" -> true), Map("nbrRange" -> Map(0 -> 0d)))))
 }

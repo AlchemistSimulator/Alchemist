ba893f1d8301882621aa7053e76bf8bba6aa36e6
Danilo Pianini <danilo.pianini@unibo.it>
2017-06-15 15:27:50 +0200

    Suppress FindBugs warning on implicit class

--- alchemist/alchemist-implementationbase/src/main/scala/it/unibo/alchemist/scala/PimpMyAlchemist.scala
+++ alchemist/alchemist-implementationbase/src/main/scala/it/unibo/alchemist/scala/PimpMyAlchemist.scala
@@ -6,11 +6,13 @@ import it.unibo.alchemist.model.interfaces.Position
 import it.unibo.alchemist.model.interfaces.Molecule
 import it.unibo.alchemist.model.implementations.molecules.SimpleMolecule
 import com.google.common.cache.CacheLoader
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
 
 object PimpMyAlchemist {
   /**
    * Wraps a Position, providing + and - operations.
    */
+  @SuppressFBWarnings(Array("NM_METHOD_NAMING_CONVENTION"))
   implicit class RichPosition(position: Position) {
     def -(p: Position) = position.subtract(p)
     def +(p: Position) = position.add(p)

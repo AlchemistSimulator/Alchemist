d9b81dad741cd4002cd6c3b019cc828be71b7b43
Placu95 <andrea.placuzzi@studio.unibo.it>
2017-07-24 12:30:03 +0200

    modified exception

--- alchemist/alchemist-maps/src/main/java/it/unibo/alchemist/boundary/gpsload/LoadGPSTraceStrategy.java
+++ alchemist/alchemist-maps/src/main/java/it/unibo/alchemist/boundary/gpsload/LoadGPSTraceStrategy.java
@@ -1,11 +1,8 @@
 package it.unibo.alchemist.boundary.gpsload;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-
 import org.openstreetmap.osmosis.osmbinary.file.FileFormatException;
-
 import it.unibo.alchemist.model.interfaces.GPSTrace;
 
 /**
@@ -17,20 +14,20 @@ public interface LoadGPSTraceStrategy {
      * 
      * @param stream file with the trace request
      * @return GPSTrace readed
-     * @throws FileNotFoundException 
+     * @throws IllegalStateException if the trace or a segment's trace not contain GPSPoint
      * @throws FileFormatException
      * @throws IOException 
      */
-    GPSTrace readTrace(InputStream stream) throws FileNotFoundException, FileFormatException, IOException;
+    GPSTrace readTrace(InputStream stream) throws FileFormatException, IllegalStateException, IOException;
     /**
      * 
      * @param stream file with the trace request
      * @param traceName name of GPSTrace request
      * @return GPSTrace readed
-     * @throws FileNotFoundException
+     * @throws IllegalStateException if the trace or a segment's trace not contain GPSPoint
      * @throws FileFormatException
      * @throws IOException 
      */
-    GPSTrace readTrace(InputStream stream, String traceName) throws FileNotFoundException, FileFormatException, IOException;
+    GPSTrace readTrace(InputStream stream, String traceName) throws FileFormatException, IllegalStateException, IOException;
 
 }

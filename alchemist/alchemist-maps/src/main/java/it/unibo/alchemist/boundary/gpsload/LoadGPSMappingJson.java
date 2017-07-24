7a1c4c0d2a3c6755f2b34e6732c4957c70423e6c
Placu95 <andrea.placuzzi@studio.unibo.it>
2017-07-24 12:27:45 +0200

    added control(resource exist and json field is correct type)

--- alchemist/alchemist-maps/src/main/java/it/unibo/alchemist/boundary/gpsload/LoadGPSMappingJson.java
+++ alchemist/alchemist-maps/src/main/java/it/unibo/alchemist/boundary/gpsload/LoadGPSMappingJson.java
@@ -2,8 +2,11 @@ package it.unibo.alchemist.boundary.gpsload;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Objects;
+import java.util.Optional;
+
 import com.google.gson.stream.JsonReader;
 import gnu.trove.map.TIntObjectMap;
 import gnu.trove.map.hash.TIntObjectHashMap;
@@ -16,16 +19,21 @@ public class LoadGPSMappingJson implements LoadGPSMappingStrategy {
     private static final String JSON_FILE_NAME = "/mapping-config.json";
     private TIntObjectMap<MappingTrace> mapping;
 
-    
+
     @Override
     public TIntObjectMap<MappingTrace> loadMapping(final String pathFile) throws FileNotFoundException, IOException {
-        this.mapping = new TIntObjectHashMap<>();     
-        this.loadJson(pathFile + JSON_FILE_NAME);
+        this.mapping = new TIntObjectHashMap<>(); 
+        final String configPath = pathFile + JSON_FILE_NAME;
+        this.loadJson(configPath);
         return this.mapping;
     }
 
     private void loadJson(final String path) throws FileNotFoundException, IOException {
-        try (JsonReader reader = new JsonReader(new InputStreamReader(LoadGPSMappingJson.class.getResourceAsStream(path)))) {
+        final InputStream stream = LoadGPSMappingJson.class.getResourceAsStream(path);
+        if (stream == null) {
+            throw new FileNotFoundException("file with mapping not found");
+        }
+        try (JsonReader reader = new JsonReader(new InputStreamReader(stream))) {
             reader.beginArray();
             while (reader.hasNext()) {
                 final MappingTrace map = this.readMapping(reader);
@@ -44,11 +52,23 @@ public class LoadGPSMappingJson implements LoadGPSMappingStrategy {
         while (reader.hasNext()) {
             final String nameField = reader.nextName();
             if (nameField.equals("idNode")) {
-                idNode = reader.nextInt();
+                try {
+                    idNode = reader.nextInt();
+                } catch (IllegalStateException | NumberFormatException e) {
+                    throw new IllegalArgumentException("the value of idNode token must be an int");
+                }
             } else if (nameField.equals("pathFile")) {
-                pathFile = reader.nextString();
+                try {
+                    pathFile = reader.nextString();
+                } catch (IllegalStateException e) {
+                    throw new IllegalArgumentException("the value of pathFile token must be a String not null");
+                }
             } else if (nameField.equals("tarceName")) {
-                tarceName = reader.nextString();
+                try {
+                    tarceName = reader.nextString();
+                } catch (IllegalStateException e) {
+                    throw new IllegalArgumentException("the value of tarceName token, if present, must be a String not null");
+                }
             } else {
                 reader.skipValue();
             }
@@ -56,7 +76,7 @@ public class LoadGPSMappingJson implements LoadGPSMappingStrategy {
         reader.endObject();
         return new MappingTrace(Objects.requireNonNull(idNode, "node id mustn't is null"), 
                                 Objects.requireNonNull(pathFile, "file path mustn't is null"),
-                                tarceName);
+                                Optional.ofNullable(tarceName));
 
     }
 

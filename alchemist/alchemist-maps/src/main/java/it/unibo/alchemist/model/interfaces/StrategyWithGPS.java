c65fbd8769f6849030c7b09c5e34fdd42d77b8d6
andrea.placuzzi@studio.unibo.it <andrea.placuzzi@studio.unibo.it>
2017-07-31 17:48:49 +0200

    strategy for enable setter trace for moveStrategy

---
+++ alchemist/alchemist-maps/src/main/java/it/unibo/alchemist/model/interfaces/StrategyWithGPS.java
@@ -0,0 +1,14 @@
+package it.unibo.alchemist.model.interfaces;
+
+/**
+ *
+ */
+public interface StrategyWithGPS {
+
+    /**
+     * 
+     * @param trace the {@link GPSTrace} to follow
+     */
+    void setTrace(GPSTrace trace);
+
+}

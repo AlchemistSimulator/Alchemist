fb3b717ab92eccd405de4e85441b9f5aea683c39
andrea.placuzzi@studio.unibo.it <andrea.placuzzi@studio.unibo.it>
2017-08-01 15:50:38 +0200

    fix checkStyle problem

--- alchemist/alchemist-maps/src/test/java/it/unibo/alchemist/test/TestGPSLoader.java
+++ alchemist/alchemist-maps/src/test/java/it/unibo/alchemist/test/TestGPSLoader.java
@@ -19,18 +19,15 @@ public class TestGPSLoader {
     private static final String BASE_DIR = "/trace/";
     private static final String BASE_OK_TEST = BASE_DIR + "ok/";
     private static final String BASE_NOT_OK_TEST = BASE_DIR + "no_ok/";
-    
     private static final String DIRECTORY_WITH_FILES = BASE_OK_TEST + "sub1/";
     private static final String DIRECTORY_WITH_SUBDIRECTORIES = BASE_OK_TEST;
-    
     private static final String WRONG_EXTENSION = BASE_NOT_OK_TEST + "/wrong_extension/";
     private static final String UNRECOGNIZED_EXTENSION = BASE_NOT_OK_TEST + "unrecognized_extension/";
-    
     private static final String TIME_NORMALIZER_CLASS = "NormalizeTimeWithFirstOfAll";
     private static final GPSTimeNormalizer NORMALIZER = new NormalizeTimeWithFirstOfAll();
-    
+
     private TraceLoader loaderGpx;
-    
+    private static final int NUM_MAX_TRACES = 6;
     private static final int TOTAL_POINTS = 12196; 
 
 
@@ -39,12 +36,12 @@ public class TestGPSLoader {
      */
     @Test
     public void testOk() {
-        
+
         try {
             this.loaderGpx = new TraceLoader(DIRECTORY_WITH_FILES, TIME_NORMALIZER_CLASS);
             final Iterator<GPSTrace> trace = this.loaderGpx.iterator();
             for (int i = 0; i < 3; i++) {
-                if(trace.hasNext()) {
+                if (trace.hasNext()) {
                     trace.next();
                 } else {
                     fail("not loading all trace");
@@ -54,14 +51,14 @@ public class TestGPSLoader {
         } catch (Exception e) {
             fail(e.getMessage());
         } 
-        
+
         try {
             this.loaderGpx = new TraceLoader(DIRECTORY_WITH_SUBDIRECTORIES, true,  NORMALIZER);
             final Iterator<GPSTrace> trace = this.loaderGpx.iterator();
             int points = 0;
-            for (int i = 0; i < 6; i++) {
-                if(trace.hasNext()) {
-                    points += trace.next().size();                 
+            for (int i = 0; i < NUM_MAX_TRACES; i++) {
+                if (trace.hasNext()) {
+                    points += trace.next().size();
                 } else {
                     fail("not loading all trace");
                 }
@@ -71,12 +68,15 @@ public class TestGPSLoader {
         } catch (Exception e) {
             fail(e.getMessage());
         }
-        
+
     }
 
+    /**
+     * 
+     */
     @Test
     public void testError() {
-        
+
         try {
             this.loaderGpx = new TraceLoader(WRONG_EXTENSION, TIME_NORMALIZER_CLASS);
             fail("not exception for wrong extension");
@@ -85,7 +85,7 @@ public class TestGPSLoader {
         } catch (Exception e) {
             fail(e.getMessage());
         } 
-        
+
         try {
             this.loaderGpx = new TraceLoader(UNRECOGNIZED_EXTENSION, NORMALIZER);
             fail("not exception for unrecognized extension");
@@ -94,6 +94,6 @@ public class TestGPSLoader {
         } catch (Exception e) {
             fail(e.getMessage());
         }
-        
+
     }
 }

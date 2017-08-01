143d314afccf3086ef8ade6c54992d0877f7c1f6
andrea.placuzzi@studio.unibo.it <andrea.placuzzi@studio.unibo.it>
2017-08-01 15:37:43 +0200

    Added abstract class for normalize time of all trace

---
+++ alchemist/alchemist-maps/src/main/java/it/unibo/alchemist/boundary/gpsload/api/AbstractGPSTimeNormalizer.java
@@ -0,0 +1,30 @@
+package it.unibo.alchemist.boundary.gpsload.api;
+
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+
+import it.unibo.alchemist.model.interfaces.GPSTrace;
+import it.unibo.alchemist.model.interfaces.Time;
+
+/**
+ * 
+ */
+public abstract class AbstractGPSTimeNormalizer implements GPSTimeNormalizer {
+
+    @Override
+    public ImmutableList<GPSTrace> normalizeTime(final List<GPSTrace> traces) {
+        return traces.stream()
+                .map(trace -> trace.startAt(computeStartTime(traces, trace)))
+                .collect(ImmutableList.toImmutableList());
+    }
+
+    /**
+     * 
+     * @param allTraces all {@link GPSTrace} to normalize
+     * @param currentTrace current {@link GPSTrace} to normalize
+     * @return the time from which the trace should begin
+     */
+    protected abstract Time computeStartTime(List<GPSTrace> allTraces, GPSTrace currentTrace);
+
+}

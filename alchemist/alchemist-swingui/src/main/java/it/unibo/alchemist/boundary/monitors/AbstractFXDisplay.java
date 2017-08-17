4ddef4cf5932660cade4234fd0d8756d610efe50
Niccolò Maltoni <niccolo.maltoni@studio.unibo.it>
2017-08-17 19:43:27 +0200

    Working on AbstractFXDisplay

--- alchemist/alchemist-swingui/src/main/java/it/unibo/alchemist/boundary/monitors/AbstractFXDisplay.java
+++ alchemist/alchemist-swingui/src/main/java/it/unibo/alchemist/boundary/monitors/AbstractFXDisplay.java
@@ -1,15 +1,22 @@
 package it.unibo.alchemist.boundary.monitors;
 
 import it.unibo.alchemist.boundary.gui.effects.EffectFX;
+import it.unibo.alchemist.boundary.gui.utility.ResourceLoader;
 import it.unibo.alchemist.boundary.interfaces.FXOutputMonitor;
-import it.unibo.alchemist.model.interfaces.Concentration;
-import it.unibo.alchemist.model.interfaces.Environment;
-import it.unibo.alchemist.model.interfaces.Reaction;
-import it.unibo.alchemist.model.interfaces.Time;
+import it.unibo.alchemist.boundary.wormhole.interfaces.IWormhole2D;
+import it.unibo.alchemist.core.interfaces.Simulation;
+import it.unibo.alchemist.model.implementations.times.DoubleTime;
+import it.unibo.alchemist.model.interfaces.*;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.MouseButton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.Collection;
 
 /**
@@ -34,7 +41,9 @@ public abstract class AbstractFXDisplay<T> extends Canvas implements FXOutputMon
      * The default radius of freedom of representation.
      */
     protected static final /* TODO byte */ double FREEDOM_RADIUS = 1;
-    /** Default logger for this class. */
+    /**
+     * Default logger for this class.
+     */
     private static final Logger L = LoggerFactory.getLogger(AbstractFXDisplay.class);
     // TODO
 
@@ -42,8 +51,83 @@ public abstract class AbstractFXDisplay<T> extends Canvas implements FXOutputMon
         System.setProperty("sun.java2d.opengl", "true");
     }
 
-    AbstractFXDisplay() {
+    private int step;
+    private IWormhole2D wormhole;
+    private double mouseX;
+    private double mouseY;
+    private Node<T> nearest;
+    private ViewStatus status = ViewStatus.MARK_CLOSER;
+    private Environment<T> currentEnv;
+    private double lastTime;
+
+    public AbstractFXDisplay() {
+        this(1);
+    }
+
+    public AbstractFXDisplay(final int step) {
+        super();
+        setStep(step);
+        setStyle("-fx-background-color: #FFF;");
+        setMouseListener();
+        setkeyboardListener();
+    }
+
+    private void setkeyboardListener() {
         // TODO
+    }
+
+    protected void setMouseListener() {
+        setOnMouseClicked(e -> {
+            setDist(e.getX(), e.getY());
+            if (isCloserNodeMarked() && nearest != null && e.getButton().equals(MouseButton.PRIMARY) && e.getClickCount() == 2) {
+                final NodeTracker<T> monitor = new NodeTracker<>(nearest);
+                monitor.stepDone(currentEnv, null, new DoubleTime(lastTime), step);
+                final Simulation<T> simulation = currentEnv.getSimulation();
+                // TODO open a drawer to the right of MainApp v
+//                makeFrame("Tracker for node " + nearest.getId(), monitor, jf -> {
+//                    final JFrame frame = (JFrame) jf;
+//                    if (sim != null) {
+//                        sim.addOutputMonitor(monitor);
+//                        frame.addWindowListener(new WindowAdapter() {
+//                            @Override
+//                            public void windowClosing(final WindowEvent e) {
+//                                sim.removeOutputMonitor(monitor);
+//                            }
+//                        });
+//                    }
+//                });
+                // TODO open a drawer to the right of MainApp ^
+            }
+        });
+        // TODO
+    }
+
+    private final boolean isCloserNodeMarked() {
+        return status == AbstractFXDisplay.ViewStatus.MARK_CLOSER;
+    }
+
+    /**
+     * Sets a new {@link Tooltip} on this component with nearest node from the mouse position.
+     *
+     * @param x x coordinate of the mouse
+     * @param y y coordinate of the mouse
+     */
+    private void setDist(final double x, final double y) {
+        if (wormhole != null) {
+            mouseX = x;
+            mouseY = y;
+            final Position envMouse = wormhole.getEnvPoint(new Point((int) mouseX, (int) mouseY));
+            final StringBuilder sb = new StringBuilder();
+            sb.append(envMouse);
+            if (nearest != null) {
+                sb.append(" -- ");
+                sb.append(ResourceLoader.getStringRes("nearest_node_is"));
+                sb.append(": ");
+                sb.append(nearest.getId());
+            }
+            final Tooltip tooltip = new Tooltip(sb.toString());
+            Tooltip.install(this, tooltip);
+        }
     }
 
     @Override
@@ -52,9 +136,17 @@ public abstract class AbstractFXDisplay<T> extends Canvas implements FXOutputMon
         return 0;
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @throws IllegalArgumentException if the step is not bigger than 0
+     */
     @Override
-    public void setStep(final int step) {
-        // TODO
+    public void setStep(final int step) throws IllegalArgumentException {
+        if (step <= 0) {
+            throw new IllegalArgumentException("The parameter must be a positive integer");
+        }
+        this.step = step;
     }
 
     @Override
@@ -102,5 +194,21 @@ public abstract class AbstractFXDisplay<T> extends Canvas implements FXOutputMon
     @Override
     public void stepDone(final Environment<T> environment, final Reaction<T> reaction, final Time time, final long step) {
         // TODO
+    }
+
+    private enum ViewStatus {
+        VIEW_ONLY,
+
+        MARK_CLOSER,
+
+        SELECTING,
+
+        MOVING,
+
+        CLONING,
+
+        DELETING,
+
+        MOLECULING;
     }
 }

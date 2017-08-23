82923c0b821be3269ebf9ef1c725c70ec8f0256a
Niccolò Maltoni <niccolo.maltoni@studio.unibo.it>
2017-08-24 00:46:16 +0200

    Create new OutputMonitor compatible with JavaFX that will replace MapDisplay
    Even if SwingNode is not perfect, as MapForge is based on AWT components, probably this will be implemented with SwingNodes wrapping existing implementation

---
+++ alchemist/alchemist-swingui/src/main/java/it/unibo/alchemist/boundary/monitors/FXMapDisplay.java
@@ -0,0 +1,47 @@
+package it.unibo.alchemist.boundary.monitors;
+
+import it.unibo.alchemist.boundary.interfaces.FX2DOutputMonitor;
+import it.unibo.alchemist.model.interfaces.Position;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FXMapDisplay<T> extends AbstractFXDisplay<T> implements FX2DOutputMonitor<T> {
+    /**
+     * Default logger for this class. It hides {@link AbstractFXDisplay}'s one.
+     */
+    private static final Logger L = LoggerFactory.getLogger(FXMapDisplay.class);
+    // TODO
+    // TODO
+    // TODO
+    // TODO
+
+    /**
+     * Default constructor. The number of steps is set to default ({@value #DEFAULT_NUMBER_OF_STEPS}).
+     */
+    public FXMapDisplay() {
+        super();
+        // TODO
+    }
+
+    /**
+     * Main constructor. It lets the developer specify the number of steps.
+     *
+     * @param step the number of steps
+     * @see #setStep(int)
+     */
+    public FXMapDisplay(final int step) {
+        super(step);
+        // TODO
+    }
+
+    @Override
+    public void zoomTo(final Position center, final double zoomLevel) {
+        assert center.getDimensions() == 2;
+        getWormhole().zoomOnPoint(getWormhole().getViewPoint(center), zoomLevel);
+    }
+
+    @Override
+    protected Logger getLogger() {
+        return FXMapDisplay.L;
+    }
+}

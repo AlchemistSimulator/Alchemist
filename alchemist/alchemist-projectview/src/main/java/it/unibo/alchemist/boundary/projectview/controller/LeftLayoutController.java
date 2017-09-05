5ce389cce548a7fa102390cfd70fe3a31e2c48e9
andrea.placuzzi@studio.unibo.it <andrea.placuzzi@studio.unibo.it>
2017-09-05 12:24:20 +0200

    remove unused import

--- alchemist/alchemist-projectview/src/main/java/it/unibo/alchemist/boundary/projectview/controller/LeftLayoutController.java
+++ alchemist/alchemist-projectview/src/main/java/it/unibo/alchemist/boundary/projectview/controller/LeftLayoutController.java
@@ -5,7 +5,6 @@ import java.awt.Desktop.Action;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ResourceBundle;
 

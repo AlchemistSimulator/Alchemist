6aaee19d8499ded12616917fdc768a6cefbf14d9
andrea.placuzzi@studio.unibo.it <andrea.placuzzi@studio.unibo.it>
2017-09-08 14:59:27 +0200

    add basic move strategy that use a GPSTrace

---
+++ alchemist/alchemist-maps/src/main/java/it/unibo/alchemist/model/implementations/movestrategies/AbstractStrategyWithGPS.java
@@ -0,0 +1,32 @@
+package it.unibo.alchemist.model.implementations.movestrategies;
+
+import java.util.Objects;
+
+import it.unibo.alchemist.model.interfaces.GPSTrace;
+import it.unibo.alchemist.model.interfaces.StrategyWithGPS;
+
+/**
+ * basic move strategy that use a {@link GPSTrace}.
+ */
+public abstract class AbstractStrategyWithGPS implements StrategyWithGPS {
+
+    private GPSTrace trace;
+
+    /**
+     * 
+     * @return the {@link GPSTrace} used from this strategy
+     */
+    protected GPSTrace getTrace() {
+        return trace;
+    }
+
+    @Override
+    public final void setTrace(final GPSTrace trace) {
+        if (this.trace == null) {
+            this.trace = Objects.requireNonNull(trace);
+        } else {
+            throw new IllegalStateException("The GPS trace can be set only once");
+        }
+    }
+
+}

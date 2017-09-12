22cb3146b8ed0294637a91e096618517b8f027d4
andrea.placuzzi@studio.unibo.it <andrea.placuzzi@studio.unibo.it>
2017-09-12 17:26:57 +0200

    add behavior for trace with single point

--- alchemist/alchemist-maps/src/main/java/it/unibo/alchemist/boundary/gpsload/api/AbstractGPSTimeAlignment.java
+++ alchemist/alchemist-maps/src/main/java/it/unibo/alchemist/boundary/gpsload/api/AbstractGPSTimeAlignment.java
@@ -1,6 +1,8 @@
 package it.unibo.alchemist.boundary.gpsload.api;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import com.google.common.collect.ImmutableList;
 
@@ -12,11 +14,48 @@ import it.unibo.alchemist.model.interfaces.Time;
  */
 public abstract class AbstractGPSTimeAlignment implements GPSTimeAlignment {
 
+    private final SinglePointBehavior policy;
+    /**
+     * 
+     */
+    protected enum SinglePointBehavior {
+        /**
+         * 
+         */
+        RETAIN_SINGLE_POINTS,
+        /**
+         * 
+         */
+        DISCARD_SINGLE_POINTS,
+        /**
+         * 
+         */
+        THROW_EXCEPTION_ON_SINGLE_POINTS
+    }
+
+    /**
+     * 
+     * @param policy define policy for trace with single point
+     */
+    protected AbstractGPSTimeAlignment(final SinglePointBehavior policy) {
+        this.policy = policy;
+    }
+
     @Override
     public ImmutableList<GPSTrace> alignTime(final List<GPSTrace> traces) {
-        return traces.stream()
-                .map(trace -> trace.startAt(computeStartTime(traces, trace)))
-                .collect(ImmutableList.toImmutableList());
+        Stream<GPSTrace> stream = traces.stream().map(trace -> trace.startAt(computeStartTime(traces, trace)));
+        if (policy == SinglePointBehavior.DISCARD_SINGLE_POINTS) {
+            stream = stream.filter(trace -> trace.size() <= 1);
+        }
+        if (policy == SinglePointBehavior.THROW_EXCEPTION_ON_SINGLE_POINTS) {
+            final ImmutableList<GPSTrace> reified = stream.collect(ImmutableList.toImmutableList());
+            final Optional<GPSTrace> single = reified.stream().filter(t -> t.size() <= 1).findAny();
+            if (single.isPresent()) {
+                throw new IllegalStateException("Time alignment produced a trace with a single point: " + single.get());
+            }
+            stream = reified.stream();
+        }
+        return stream.collect(ImmutableList.toImmutableList());
     }
 
     /**

7aa8a35e35ced32ea7fb05f1b4600a6e5d3b7ca0
Niccolò Maltoni <niccolo.maltoni@studio.unibo.it>
2017-10-28 16:44:08 +0200

    Very small code improvements

--- alchemist/alchemist-swingui/src/main/java/it/unibo/alchemist/boundary/gui/effects/json/EffectGroupAdapter.java
+++ alchemist/alchemist-swingui/src/main/java/it/unibo/alchemist/boundary/gui/effects/json/EffectGroupAdapter.java
@@ -1,9 +1,5 @@
 package it.unibo.alchemist.boundary.gui.effects.json;
 
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
-
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
@@ -12,31 +8,43 @@ import com.google.gson.JsonParseException;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 import com.google.gson.reflect.TypeToken;
-
 import it.unibo.alchemist.boundary.gui.effects.EffectFX;
 import it.unibo.alchemist.boundary.gui.effects.EffectGroup;
 import it.unibo.alchemist.boundary.gui.effects.EffectStack;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class should be registered in a {@link com.google.gson.GsonBuilder} to
  * serialize and deserialize a {@link EffectGroup} compatible class.
  */
 public class EffectGroupAdapter implements JsonSerializer<EffectGroup>, JsonDeserializer<EffectGroup> {
-    /** Name given to {@code name} field in JSON file. */
+    /**
+     * Name given to {@code name} field in JSON file.
+     */
     private static final String NAME = "name";
-    /** Name given to {@code visibility} field in JSON file. */
+    /**
+     * Name given to {@code visibility} field in JSON file.
+     */
     private static final String VISIBILITY = "visibility";
-    /** Name given to {@code transparency} field in JSON file. */
+    /**
+     * Name given to {@code transparency} field in JSON file.
+     */
     private static final String TRANSPARENCY = "transparency";
-    /** Name given to {@code effects} list field in JSON file. */
+    /**
+     * Name given to {@code effects} list field in JSON file.
+     */
     private static final String EFFECTS = "effects";
-    /** Type of {@link List}<{@link EffectFX}>. */
-    private static final Type EFFECTS_LIST_TYPE = new TypeToken<List<EffectFX>>() { }.getType();
+    /**
+     * Type of {@link List}<{@link EffectFX}>.
+     */
+    private static final Type EFFECTS_LIST_TYPE = new TypeToken<List<EffectFX>>() {
+    }.getType();
 
     @Override
-    public EffectGroup deserialize(final JsonElement json, final Type typeOfT, final JsonDeserializationContext context)
-            throws JsonParseException {
-        final JsonObject jObj = (JsonObject) json;
+    public EffectGroup deserialize(final JsonElement json, final Type typeOfT, final JsonDeserializationContext context) throws JsonParseException {
+        final JsonObject jObj = json.getAsJsonObject();
 
         final String name = jObj.get(NAME).getAsString();
         final EffectGroup group = new EffectStack(name);
@@ -58,7 +66,7 @@ public class EffectGroupAdapter implements JsonSerializer<EffectGroup>, JsonDese
         jObj.addProperty(VISIBILITY, src.isVisible());
         jObj.addProperty(TRANSPARENCY, src.getTransparency());
         final List<EffectFX> list = new ArrayList<>();
-        src.forEach(e -> list.add(e));
+        list.addAll(src);
         final JsonElement effects = context.serialize(list, EFFECTS_LIST_TYPE);
         jObj.add(EFFECTS, effects);
 

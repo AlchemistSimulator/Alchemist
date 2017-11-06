1ea5a2fed7cd749b841d65a2587265137526ab3c
KaikiNoKitsune <matteo.magnani18@studio.unibo.it>
2017-11-06 11:24:25 +0100

    Add javadoc for WorkingDirectory

--- alchemist/alchemist-grid/src/main/java/it/unibo/alchemist/grid/util/WorkingDirectory.java
+++ alchemist/alchemist-grid/src/main/java/it/unibo/alchemist/grid/util/WorkingDirectory.java
@@ -16,14 +16,30 @@ import org.apache.commons.io.FileUtils;
 
 import com.google.common.io.Files;
 
+/**
+ * Class that manage a temp local working directory.
+ *
+ */
 public class WorkingDirectory implements AutoCloseable {
 
     private final File directory;
 
+    /**
+     * Create new local temp working directory.
+     */
     public WorkingDirectory() {
         this.directory = Files.createTempDir();
     }
 
+    /**
+     * Add temp directory to classpath.
+     * @throws NoSuchMethodException 
+     * @throws SecurityException 
+     * @throws ReflectiveOperationException 
+     * @throws IllegalArgumentException 
+     * @throws InvocationTargetException 
+     * @throws MalformedURLException 
+     */
     public void addToClasspath() throws NoSuchMethodException, SecurityException, ReflectiveOperationException, IllegalArgumentException, InvocationTargetException, MalformedURLException {
         final URI u = directory.toURI();
         final URLClassLoader urlClassLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
@@ -33,12 +49,23 @@ public class WorkingDirectory implements AutoCloseable {
         method.invoke(urlClassLoader, new Object[]{u.toURL()});
     }
 
-    public String getFileContent(String filename) throws IOException {
+    /**
+     * Get folder's file content.
+     * @param filename File's name
+     * @return File's content
+     * @throws IOException 
+     */
+    public String getFileContent(final String filename) throws IOException {
         final File f = new File(this.getFileAbsolutePath(filename));
         return FileUtils.readFileToString(f, StandardCharsets.UTF_8);
     }
 
-    public void writeFiles(Map<String, String> files) throws IOException {
+    /**
+     * Write multiple files inside the directory.
+     * @param files A map with relative paths + files names as kay and files content as value.
+     * @throws IOException 
+     */
+    public void writeFiles(final Map<String, String> files) throws IOException {
         for (final Entry<String, String> e : files.entrySet()) {
             final File f = new File(this.directory.getAbsolutePath() + File.separator + e.getKey());
             f.getParentFile().mkdirs();

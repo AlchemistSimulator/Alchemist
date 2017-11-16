fb1f1fd1f96721a475e0f55c1c4db0ad32192e9c
Danilo Pianini <danilo.pianini@unibo.it>
2017-11-16 19:22:13 +0100

    Fix warnings

--- alchemist/alchemist-projectview/src/main/java/it/unibo/alchemist/boundary/projectview/controller/NewProjLayoutFolderController.java
+++ alchemist/alchemist-projectview/src/main/java/it/unibo/alchemist/boundary/projectview/controller/NewProjLayoutFolderController.java
@@ -4,10 +4,8 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.io.File;
 import java.io.IOException;
-import java.util.Optional;
 import java.util.ResourceBundle;
 
-import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,10 +15,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Label;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;

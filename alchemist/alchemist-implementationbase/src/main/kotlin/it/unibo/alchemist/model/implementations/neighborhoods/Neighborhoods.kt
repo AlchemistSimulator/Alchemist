c6009a066ba16df52620ad968f9bcaa2fd877d2c
Vuksaa <mihajlovic.vuksa@gmail.com>
2018-05-21 15:55:52 +0200

    Change function Neighborhoods#make to a companion object function

--- alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/implementations/neighborhoods/Neighborhoods.kt
+++ alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/implementations/neighborhoods/Neighborhoods.kt
@@ -1,12 +1,15 @@
-@file:JvmName("Neighborhoods")
 package it.unibo.alchemist.model.implementations.neighborhoods
 
 import it.unibo.alchemist.model.interfaces.Environment
 import it.unibo.alchemist.model.interfaces.Node
 import java.util.Collections
 
-@JvmOverloads
-fun <T> make(env: Environment<T>,
-             center: Node<T>,
-             neighbors: Iterable<Node<T>> = Collections.emptyList()) =
-        SimpleNeighborhood(env, center, neighbors)
+class Neighborhoods {
+    companion object {
+        @JvmStatic @JvmOverloads fun <T> make(
+                env: Environment<T>,
+                center: Node<T>,
+                neighbors: Iterable<Node<T>> = Collections.emptyList()) =
+            SimpleNeighborhood(env, center, neighbors)
+    }
+}

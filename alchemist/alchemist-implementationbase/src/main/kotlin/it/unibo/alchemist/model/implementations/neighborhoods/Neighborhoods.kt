812d5e27d3c4dc2c531c8ad001470244c791c565
Vuksaa <mihajlovic.vuksa@gmail.com>
2018-05-28 15:34:45 +0200

    Change SimpleNeighborhood.make to default to an empty mutable list instead of an empty immutable list

--- alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/implementations/neighborhoods/Neighborhoods.kt
+++ alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/implementations/neighborhoods/Neighborhoods.kt
@@ -2,14 +2,13 @@ package it.unibo.alchemist.model.implementations.neighborhoods
 
 import it.unibo.alchemist.model.interfaces.Environment
 import it.unibo.alchemist.model.interfaces.Node
-import java.util.Collections
 
 class Neighborhoods {
     companion object {
         @JvmStatic @JvmOverloads fun <T> make(
                 env: Environment<T>,
                 center: Node<T>,
-                neighbors: Iterable<Node<T>> = Collections.emptyList()) =
+                neighbors: Iterable<Node<T>> = mutableListOf()) =
             SimpleNeighborhood(env, center, neighbors)
     }
 }

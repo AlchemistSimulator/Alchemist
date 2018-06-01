803cbc4ce6751398b49cbe22c4ac4e14f7bad614
Vuksaa <mihajlovic.vuksa@gmail.com>
2018-06-01 11:53:14 +0200

    Make StableForSteps closed and add its Kdoc

--- alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/implementations/terminators/StableForSteps.kt
+++ alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/implementations/terminators/StableForSteps.kt
@@ -9,13 +9,38 @@ import it.unibo.alchemist.model.interfaces.Node
 import it.unibo.alchemist.model.interfaces.Position
 import java.util.function.Predicate
 
-open class StableForSteps<T>(
+/**
+ * A [Predicate] that [tests][test] if an environment's
+ * nodes (meaning their position and concentration) have
+ * remained unchanged for a certain amount of steps.
+ *
+ * The check isn't performed on every [step][it.unibo.alchemist.core.interfaces.Simulation.getStep]
+ * of a [simulation][it.unibo.alchemist.core.interfaces.Simulation],
+ * instead an interval that determines how many steps are
+ * to be skipped between each check is specified.
+ * For [test] to return true, an environment must remain unchanged for
+ * `checkInterval * equalIntervals` steps. This result might not be
+ * entirely consistent, since the check isn't performed every step so
+ * as not to cause performance issues. Therefore it might happen that
+ * some changes occur in the environment but are reverted before the
+ * next check is performed.
+ *
+ * [test] should be called at every step of the simulation in order
+ * to avoid missing checks.
+ *
+ * @constructor Creates a new [StableForSteps] with the given values.
+ *
+ * @param checkInterval The recurrence of the test
+ * @param equalIntervals The amount of [checkInterval] intervals that need to pass
+ * (during which the environment doesn't change) for [test] to return true
+ */
+class StableForSteps<T>(
     private val checkInterval: Long,
     private val equalIntervals: Long
 ) : Predicate<Environment<T>> {
-    var success: Long = 0
-    var positions: Map<Node<T>, Position> = emptyMap()
-    var contents = makeTable<T>(0)
+    private var success: Long = 0
+    private var positions: Map<Node<T>, Position> = emptyMap()
+    private var contents = makeTable<T>(0)
 
     init {
         if (checkInterval <= 0 || equalIntervals <= 0) {

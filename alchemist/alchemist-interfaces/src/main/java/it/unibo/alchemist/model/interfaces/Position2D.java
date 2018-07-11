de245c81f5598abeebcdf8a58e74f13b6f64dd50
Danilo Pianini <danilo.pianini@unibo.it>
2018-07-11 17:28:36 +0200

    Write Javadoc for Position2D

--- alchemist/alchemist-interfaces/src/main/java/it/unibo/alchemist/model/interfaces/Position2D.java
+++ alchemist/alchemist-interfaces/src/main/java/it/unibo/alchemist/model/interfaces/Position2D.java
@@ -1,9 +1,20 @@
 package it.unibo.alchemist.model.interfaces;
 
+/**
+ * A bidimensional position.
+ *
+ * @param <P>
+ */
 public interface Position2D<P extends Position2D<? extends P>> extends Position<P> {
-    
+
+    /**
+     * @return horizontal position
+     */
     double getX();
-    
+
+    /**
+     * @return vertical position
+     */
     double getY();
 
 }

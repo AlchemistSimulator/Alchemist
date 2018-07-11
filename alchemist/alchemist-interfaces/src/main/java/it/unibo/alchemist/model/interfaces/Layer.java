8bf0847f953d69d92cb0b2a35241b443126c41c5
Danilo Pianini <danilo.pianini@unibo.it>
2018-07-11 17:14:00 +0200

    Improve Javadoc

--- alchemist/alchemist-interfaces/src/main/java/it/unibo/alchemist/model/interfaces/Layer.java
+++ alchemist/alchemist-interfaces/src/main/java/it/unibo/alchemist/model/interfaces/Layer.java
@@ -4,16 +4,21 @@ import java.io.Serializable;
 
 /**
  * 
- * Interface for static layer, containing a substance or a molecule with a spatial distribution.
+ * Interface for static layer, containing a substance or a molecule with a
+ * spatial distribution.
  *
- * @param <T> the value that measure the substance in a point.
+ * @param <T>
+ *            the value that measure the substance in a point.
+ * @param <P>
+ *            Concentration type
  */
 @FunctionalInterface
 public interface Layer<T, P extends Position<? extends P>> extends Serializable {
 
     /**
      * 
-     * @param p the {@link Position}.
+     * @param p
+     *            the {@link Position}.
      * @return the value in the requested {@link Position}.
      */
     T getValue(P p);

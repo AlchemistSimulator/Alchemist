4d73a52ae7647159360b83701457bf6e8f1d2a41
Vuksaa <mihajlovic.vuksa@gmail.com>
2018-08-02 12:23:16 +0200

    Refactor the inner workings of SelectionBox

--- alchemist/alchemist-swingui/src/main/kotlin/it/unibo/alchemist/boundary/monitors/utility/SelectionBox.kt
+++ alchemist/alchemist-swingui/src/main/kotlin/it/unibo/alchemist/boundary/monitors/utility/SelectionBox.kt
@@ -20,6 +20,11 @@ import java.awt.Point
 import kotlin.math.abs
 import kotlin.math.min
 
+enum class SelectionBoxState {
+    SELECTING,
+    SELECTED
+}
+
 /**
  * Allows basic multi-element box selections.
  * @param T the concentration of the simulation
@@ -29,44 +34,37 @@ import kotlin.math.min
  */
 class SelectionBox<T>(private val anchorPoint: Point, private val context: GraphicsContext, private val wormhole: BidimensionalWormhole<Position2D<*>>) {
     private val movingPoint = Point(anchorPoint.x, anchorPoint.y)
-    var elements: Collection<Node<T>>? = null
+    var elements: Map<Node<T>, Position2D<*>>? = null
         private set
     private var rectangle = Rectangle()
         get() = anchorPoint.makeRectangleWith(movingPoint)
+    var state = SelectionBoxState.SELECTING
+        private set
 
     /**
      * Updates the box, clearing the old one and drawing the updated one.
      * @param newPoint the cursor's new position
      */
-    fun update(newPoint: Point) {
-        clear()
-        movingPoint.x = newPoint.x
-        movingPoint.y = newPoint.y
-        draw()
-    }
+    fun update(newPoint: Point): Rectangle = checkFinalized().clear().setNewPoint(newPoint).draw().rectangle
 
     /**
      * Cancels the current selection
      */
-    fun cancel() {
-        clear()
-    }
+    fun cancel(): SelectionBox<T> = clear()
 
     /**
      * Locks the elements and writes the items selected to [elements].
      * @param nodes a map having nodes as keys and their positions as values
      */
-    fun finalize(nodes: Map<Node<T>, Position2D<*>>): Collection<Node<T>> =
-        rectangle.let { selection ->
-            nodes.filterValues { selection.contains(wormhole.getViewPoint(it)) }
-                .keys
-        }.also { elements = it }.also { clear() }
+    fun finalize(nodes: Map<Node<T>, Position2D<*>>): Map<Node<T>, Position2D<*>> =
+        checkFinalized().rectangle.let { area ->
+            nodes.filterValues { wormhole.getViewPoint(it) in area }
+        }.also { elements = it }.also { clear() }.also { state = SelectionBoxState.SELECTED }
 
     /**
      * Draws the box.
      */
-    private fun draw() {
-        rectangle.let {
+    private fun draw(): SelectionBox<T> = also { rectangle.let {
             Platform.runLater {
                 // can edit the elements box's style here
                 context.fill = Paint.valueOf("#8e99f3")
@@ -78,16 +76,34 @@ class SelectionBox<T>(private val anchorPoint: Point, private val context: Graph
     /**
      * Clears the box.
      */
-    private fun clear() {
-        rectangle.let {
-            Platform.runLater {
+    private fun clear(): SelectionBox<T> = also { rectangle.let {
+        Platform.runLater {
                 context.clearRect(it.x, it.y, it.width, it.height)
             }
         }
     }
+
+    /**
+     * Sets the moving point to the given point
+     */
+    private fun setNewPoint(point: Point): SelectionBox<T> = apply {
+        movingPoint.x = point.x
+        movingPoint.y = point.y
+    }
+
+    /**
+     * Checks if the selection has been finalized
+     * @throws IllegalStateException if the selection has been finalized
+     */
+    private fun checkFinalized(): SelectionBox<T> {
+        if (state == SelectionBoxState.SELECTED) {
+            throw IllegalStateException("Selection " + this + " has already been finalized")
+        }
+        return this
+    }
 }
 
-private fun Rectangle.contains(point: Point): Boolean =
+private operator fun Rectangle.contains(point: Point): Boolean =
     point.x in this.x..(this.x + this.width) &&
     point.y in this.y..(this.y + this.height)
 

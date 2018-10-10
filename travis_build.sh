2f65165a73a99eaa8a94b19e99a5a20f8a239d99
Danilo Pianini <danilo.pianini@gmail.com>
2018-10-10 21:10:29 +0200

    Generate dokka

--- travis_build.sh
+++ travis_build.sh
@@ -3,9 +3,8 @@ set -e
 openssl aes-256-cbc -K $encrypted_9993846e3f84_key -iv $encrypted_9993846e3f84_iv -in prepare_environment.sh.enc -out prepare_environment.sh -d
 sh prepare_environment.sh
 cd alchemist
-./gradlew check projectReport uploadArchives
+./gradlew check projectReport dokka uploadArchives
 ./gradlew fatJar
 mkdir -p report
 cp --parent */build/reports build/reports report -R
-pwd
-cp dokka/index.html build/dokka/index.html
+cp dokka/index.html build/dokka/

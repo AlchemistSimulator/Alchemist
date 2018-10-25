82d43c96af40e10be175a6729baedecdf28f766c
Danilo Pianini <danilo.pianini@unibo.it>
2018-10-25 11:03:18 +0200

    Fix Javadoc of TargetSelectionStrategy

--- alchemist/alchemist-implementationbase/src/main/java/it/unibo/alchemist/model/interfaces/movestrategies/TargetSelectionStrategy.java
+++ alchemist/alchemist-implementationbase/src/main/java/it/unibo/alchemist/model/interfaces/movestrategies/TargetSelectionStrategy.java
@@ -1,13 +1,13 @@
-/*******************************************************************************
+/**
  * Copyright (C) 2010-2018, Danilo Pianini and contributors listed in the main
  * project's alchemist/build.gradle file.
- * 
+ * <p>
  * This file is part of Alchemist, and is distributed under the terms of the
  * GNU General Public License, with a linking exception, as described in the file
  * LICENSE in the Alchemist distribution's top directory.
- ******************************************************************************/
+ */
 /**
- * 
+ *
  */
 package it.unibo.alchemist.model.interfaces.movestrategies;
 
@@ -17,13 +17,14 @@ import it.unibo.alchemist.model.interfaces.Position;
 
 /**
  * This interface models a strategy for selecting positions where to move.
- * 
+ *
+ * @param <P> Position type
  */
 @FunctionalInterface
 public interface TargetSelectionStrategy<P extends Position<? extends P>> extends Serializable {
 
     /**
-     * @return the next target where the {@link Node} is directed
+     * @return the next target where the {@link it.unibo.alchemist.model.interfaces.Node} is directed
      */
     P getTarget();
 

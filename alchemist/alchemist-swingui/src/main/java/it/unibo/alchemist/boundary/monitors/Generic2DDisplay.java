a6751d14f23a1ce22c6ba781a0ce12c54f312b2b
Danilo Pianini <danilo.pianini@unibo.it>
2018-10-25 16:53:19 +0200

    remove old copyright header

--- alchemist/alchemist-swingui/src/main/java/it/unibo/alchemist/boundary/monitors/Generic2DDisplay.java
+++ alchemist/alchemist-swingui/src/main/java/it/unibo/alchemist/boundary/monitors/Generic2DDisplay.java
@@ -6,14 +6,6 @@
  * GNU General Public License, with a linking exception, as described in the file
  * LICENSE in the Alchemist distribution's top directory.
  ******************************************************************************/
-/*
- * Copyright (C) 2010-2015, Danilo Pianini and contributors
- * listed in the project's pom.xml file.
- * 
- * This file is part of Alchemist, and is distributed under the terms of
- * the GNU General Public License, with a linking exception, as described
- * in the file LICENSE in the Alchemist distribution's top directory.
- */
 package it.unibo.alchemist.boundary.monitors;
 
 import java.awt.Color;

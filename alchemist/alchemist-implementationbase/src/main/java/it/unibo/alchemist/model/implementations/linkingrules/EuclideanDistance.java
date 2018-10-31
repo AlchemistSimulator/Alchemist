5fa25664ce5014d976f370470e02f1f283be9676
Danilo Pianini <danilo.pianini@unibo.it>
2018-10-31 16:15:51 +0100

    Complete deprecation of "EuclideanDistance"

--- alchemist/alchemist-implementationbase/src/main/java/it/unibo/alchemist/model/implementations/linkingrules/EuclideanDistance.java
+++ alchemist/alchemist-implementationbase/src/main/java/it/unibo/alchemist/model/implementations/linkingrules/EuclideanDistance.java
@@ -9,12 +9,29 @@
 package it.unibo.alchemist.model.implementations.linkingrules;
 
 import it.unibo.alchemist.model.interfaces.Position;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ *
+ * @param <T>
+ * @param <P>
+ */
 @Deprecated
 public class EuclideanDistance<T, P extends Position<P>> extends ConnectWithinDistance<T, P> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(EuclideanDistance.class);
+
+    /**
+     *
+     * @param radius connection range.
+     */
     public EuclideanDistance(final double radius) {
         super(radius);
+        LOGGER.warn("{} has deprecated in favor of {}. Please update your simulation configuration to use the latter",
+            this.getClass().getSimpleName(),
+            ConnectWithinDistance.class.getSimpleName()
+        );
     }
 
 }

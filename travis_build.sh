6e56630aedcc7f4d79be51f0adb09b735a5f5021
Danilo Pianini <danilo.pianini@gmail.com>
2019-01-19 16:48:10 +0100

    Work around Dokka spamming on the shell as per Kotlin/dokka#184

--- travis_build.sh
+++ travis_build.sh
@@ -3,7 +3,7 @@ set -e
 openssl aes-256-cbc -K $encrypted_9993846e3f84_key -iv $encrypted_9993846e3f84_iv -in prepare_environment.sh.enc -out prepare_environment.sh -d
 sh prepare_environment.sh
 cd alchemist
-./gradlew --scan
+./gradlew --scan > >(egrep -v "(null:-1:-1)|(Can't find node by signature)")
 mkdir -p report
 cp --parent */build/reports build/reports report -R
 cp dokka/index.html build/dokka/ 

53db92da4a8e318b0c54d6618b3e524608547a39
Danilo Pianini <danilo.pianini@unibo.it>
2019-02-11 11:57:58 +0100

    Fix typo in task name for PR build script

--- travis_build_pull_request.sh
+++ travis_build_pull_request.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 cd alchemist
-./gradlew -x dokka -x javadoc check install jatJar > >(egrep -v "(null:-1:-1)|(Can't find node by signature)")
+./gradlew -x dokka -x javadoc check install fatJar > >(egrep -v "(null:-1:-1)|(Can't find node by signature)")

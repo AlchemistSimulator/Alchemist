2b63ce71a8e38e527289651cb244849f75448de1
Diego Mazzieri <diego.maz97@gmail.com>
2019-04-17 09:34:58 +0200

    Implement geometric shapes delegating, for the 2D case, AWT ones

---
+++ alchemist/alchemist-influence-sphere/src/main/kotlin/it/unibo/alchemist/shapes/GeometricShape.kt
@@ -0,0 +1,15 @@
+package it.unibo.alchemist.shapes
+
+import it.unibo.alchemist.model.interfaces.Position
+
+/**
+ * Defines a generic n-dimensional shape.
+ */
+interface GeometricShape<P: Position<P>> {
+
+    /**
+     * Whenever or not a given point is inside this shape.
+     */
+    fun contains(point: P): Boolean
+
+}

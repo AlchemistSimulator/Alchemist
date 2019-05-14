c0c5038f45d72efdc2dd8d81fc669a953d2444a5
Danilo Pianini <danilo.pianini@unibo.it>
2019-05-14 16:01:10 +0200

    [KTS-migration] migrate loading

--- alchemist/alchemist-loading/build.gradle.kts
+++ alchemist/alchemist-loading/build.gradle.kts
@@ -11,22 +11,19 @@ dependencies {
     implementation(project(":alchemist-interfaces"))
     implementation(project(":alchemist-time"))
     implementation(project(":alchemist-maps"))
-    implementation("com.google.guava:guava:${guavaVersion}")
-    implementation("org.codehaus.groovy:groovy:${groovyVersion}")
-    implementation("org.apache.commons:commons-lang3:${lang3Version}")
-    implementation("org.danilopianini:jirf:${jirfVersion}")
-    implementation("org.yaml:snakeyaml:$snakeyamlVersion")
+    implementation("com.google.guava:guava:${extra["guavaVersion"]}")
+    implementation("org.codehaus.groovy:groovy:${extra["groovyVersion"]}")
+    implementation("org.apache.commons:commons-lang3:${extra["lang3Version"]}")
+    implementation("org.danilopianini:jirf:${extra["jirfVersion"]}")
+    implementation("org.yaml:snakeyaml:${extra["snakeyamlVersion"]}")
     testImplementation(project(":alchemist-engine"))
     testImplementation(project(":alchemist-maps"))
-    testImplementation("com.google.code.gson:gson:$gsonVersion")
-    testImplementation("io.kotlintest:kotlintest-runner-junit5:$kotlinTestVersion")
+    testImplementation("com.google.code.gson:gson:${extra["gsonVersion"]}")
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:${extra["kotlinTestVersion"]}")
     testRuntimeOnly(project(":alchemist-incarnation-sapere"))
 }
 
-test {
+tasks.withType<Test> {
     useJUnitPlatform()
     maxHeapSize = "2g"
 }
-
-compileKotlin.dependsOn(project(":alchemist-implementationbase").compileScala)
-compileJava.dependsOn(project(":alchemist-implementationbase").compileScala)

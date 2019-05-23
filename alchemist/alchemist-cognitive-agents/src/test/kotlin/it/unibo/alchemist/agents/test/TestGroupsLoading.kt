5a08e0fda2126e5f3d27fc6b0128933bea989929
Diego Mazzieri <diego.maz97@gmail.com>
2019-05-23 18:01:53 +0200

    Test loading of some families inside a simulation

---
+++ alchemist/alchemist-cognitive-agents/src/test/kotlin/it/unibo/alchemist/agents/test/TestGroupsLoading.kt
@@ -0,0 +1,39 @@
+package it.unibo.alchemist.agents.test
+
+import io.kotlintest.TestCase
+import io.kotlintest.specs.StringSpec
+import it.unibo.alchemist.agents.Pedestrian
+import it.unibo.alchemist.groups.Family
+import it.unibo.alchemist.loader.displacements.Circle
+import it.unibo.alchemist.loader.displacements.Displacement
+import it.unibo.alchemist.model.ProtelisIncarnation
+import it.unibo.alchemist.model.implementations.environments.Continuous2DEnvironment
+import it.unibo.alchemist.model.implementations.linkingrules.NoLinks
+import it.unibo.alchemist.model.implementations.positions.Euclidean2DPosition
+import org.apache.commons.math3.random.MersenneTwister
+import kotlin.properties.Delegates
+
+private const val NUM_FAMILIES = 3
+
+private val INCARNATION = ProtelisIncarnation<Euclidean2DPosition>()
+private val ENVIRONMENT = Continuous2DEnvironment<Any>()
+private val LINKING_RULE = NoLinks<Any, Euclidean2DPosition>()
+private val RANDOM = MersenneTwister(1)
+private var displacement: Displacement<Euclidean2DPosition> by Delegates.notNull()
+private var nodes: Collection<Pedestrian<Any>> by Delegates.notNull()
+
+class TestGroupsLoading : StringSpec({
+
+    "families loading" {
+        nodes = (0..NUM_FAMILIES).flatMap { Family(ENVIRONMENT, RANDOM).members }
+        displacement = Circle<Euclidean2DPosition>(ENVIRONMENT, RANDOM, nodes.size, 0.0, 0.0, 10.0)
+        with(displacement.iterator()) {
+            nodes.onEach { println(it.membershipGroup) }.forEach { ENVIRONMENT.addNode(it, this.next()) }
+        }
+        ENVIRONMENT.startSimulationWithoutParameters()
+    }
+}) {
+    override fun beforeTest(testCase: TestCase) {
+        ENVIRONMENT.linkingRule = LINKING_RULE
+    }
+}

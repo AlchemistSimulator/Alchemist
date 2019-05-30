6ee1bcd25d007fbbcb0a23207f3a64c1dedb5dd5
Diego Mazzieri <diego.maz97@gmail.com>
2019-05-30 10:11:31 +0200

    Fix ktlintCheck errors

--- alchemist/alchemist-influence-sphere/src/main/kotlin/it/unibo/alchemist/sensory/FieldOfView2D.kt
+++ alchemist/alchemist-influence-sphere/src/main/kotlin/it/unibo/alchemist/sensory/FieldOfView2D.kt
@@ -5,11 +5,12 @@ import it.unibo.alchemist.shapes.GeometricShape2D
 import java.awt.geom.Arc2D
 
 // To be better implemented following https://legends2k.github.io/2d-fov/design.html
-class FieldOfView2D<P: Position2D<P>>(originX: Double,
-                                      originY: Double,
-                                      direction: Double,
-                                      aperture: Double,
-                                      distance: Double)
-    : InfluenceSphere2D<P>(GeometricShape2D<P>(Arc2D.Double(originX-distance, originY-distance,
-                                                            distance*2, distance*2,
+class FieldOfView2D<P : Position2D<P>>(
+    originX: Double,
+    originY: Double,
+    direction: Double,
+    aperture: Double,
+    distance: Double
+) : InfluenceSphere2D<P>(GeometricShape2D<P>(Arc2D.Double(originX - distance, originY - distance,
+                                                            distance * 2, distance * 2,
                                                             -direction, -aperture, Arc2D.PIE)))
--- alchemist/alchemist-influence-sphere/src/main/kotlin/it/unibo/alchemist/sensory/InfluenceSphere.kt
+++ alchemist/alchemist-influence-sphere/src/main/kotlin/it/unibo/alchemist/sensory/InfluenceSphere.kt
@@ -5,11 +5,10 @@ import it.unibo.alchemist.model.interfaces.Position
 /**
  * A sphere of influence in any n-dimensional space.
  */
-interface InfluenceSphere<P: Position<P>> {
+interface InfluenceSphere<P : Position<P>> {
 
     /**
      * Whenever or not a given point has an influence on the node equipped with this sphere.
      */
     fun isInfluenced(point: P): Boolean
-
 }

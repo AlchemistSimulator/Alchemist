8a58cdd6fbec0e0b01c824a4f63d370b893ec36b
Danilo Pianini <danilo.pianini@unibo.it>
2019-06-07 17:17:48 +0200

    Avoid empty catch blocks

--- alchemist/alchemist-swingui/src/main/java/it/unibo/alchemist/boundary/monitors/MapDisplay.java
+++ alchemist/alchemist-swingui/src/main/java/it/unibo/alchemist/boundary/monitors/MapDisplay.java
@@ -107,11 +107,7 @@ public class MapDisplay<T> extends Generic2DDisplay<T, GeoPosition> {
 
     @Override
     protected void setMouseTooltipTo(final int x, final int y) {
-        try {
-            super.setMouseTooltipTo(x, y);
-        } catch (final IllegalArgumentException e) {
-            // TODO: why is this silenced???
-        }
+        super.setMouseTooltipTo(x, y);
     }
 
     @Override

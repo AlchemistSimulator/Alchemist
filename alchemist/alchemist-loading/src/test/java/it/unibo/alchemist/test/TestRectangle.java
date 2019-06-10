830e100832ce517c65bd669ebd6ea484b2cb8aa0
Danilo Pianini <danilo.pianini@unibo.it>
2019-06-10 14:31:22 +0200

    disable checkstyle magic number check on test

--- alchemist/alchemist-loading/src/test/java/it/unibo/alchemist/test/TestRectangle.java
+++ alchemist/alchemist-loading/src/test/java/it/unibo/alchemist/test/TestRectangle.java
@@ -7,13 +7,12 @@
  */
 package it.unibo.alchemist.test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.junit.jupiter.api.Test;
-
 import it.unibo.alchemist.loader.shapes.Rectangle;
 import it.unibo.alchemist.loader.shapes.Shape;
 import it.unibo.alchemist.model.implementations.positions.LatLongPosition;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  *
@@ -26,9 +25,9 @@ public class TestRectangle {
     @Test
     public void test() {
         final Shape s = new Rectangle(12, 44, 1, 1);
-        // CHECKSTYLE:OFF
+        // CHECKSTYLE: MagicNumber OFF
         assertTrue(s.contains(new LatLongPosition(44.132300, 12.233000)));
-        // CHECKSTYLE:ON
+        // CHECKSTYLE: MagicNumber ON
     }
 
 }

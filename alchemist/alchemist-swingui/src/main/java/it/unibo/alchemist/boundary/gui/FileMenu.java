07780b7e868c518f6b6bffc9c2a73bc501fe780a
Danilo Pianini <danilo.pianini@unibo.it>
2019-06-10 11:06:33 +0200

    class is not designed for extension

--- alchemist/alchemist-swingui/src/main/java/it/unibo/alchemist/boundary/gui/FileMenu.java
+++ alchemist/alchemist-swingui/src/main/java/it/unibo/alchemist/boundary/gui/FileMenu.java
@@ -7,6 +7,14 @@
  */
 package it.unibo.alchemist.boundary.gui;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import org.danilopianini.view.GUIUtilities;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.swing.JFileChooser;
+import javax.swing.JMenuItem;
+import javax.swing.filechooser.FileFilter;
 import java.awt.event.ActionEvent;
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
@@ -15,16 +23,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 
-import javax.swing.JFileChooser;
-import javax.swing.JMenuItem;
-import javax.swing.filechooser.FileFilter;
-
-import org.danilopianini.view.GUIUtilities;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-
 import static it.unibo.alchemist.boundary.l10n.LocalizedResourceBundle.getString;
 
 /**
@@ -32,7 +30,7 @@ import static it.unibo.alchemist.boundary.l10n.LocalizedResourceBundle.getString
 @Deprecated
 @SuppressFBWarnings
 @SuppressWarnings("PMD")
-public class FileMenu extends AbstractMenu {
+public final class FileMenu extends AbstractMenu {
 
     private static final long serialVersionUID = 5209455686362711386L;
     private static final JMenuItem[] ITEMS = {

5fcfdd6036ed4189817a7e649989264254c6f5a8
Danilo Pianini <danilo.pianini@unibo.it>
2019-06-10 12:04:55 +0200

    improve imports and documentation

--- alchemist/alchemist-swingui/src/main/java/it/unibo/alchemist/boundary/gui/ReactivityPanel.java
+++ alchemist/alchemist-swingui/src/main/java/it/unibo/alchemist/boundary/gui/ReactivityPanel.java
@@ -7,23 +7,21 @@
  */
 package it.unibo.alchemist.boundary.gui;
 
-import static it.unibo.alchemist.boundary.gui.AlchemistSwingUI.loadScaledImage;
 import it.unibo.alchemist.boundary.gui.tape.JTapeFeatureStack;
 import it.unibo.alchemist.boundary.gui.tape.JTapeGroup;
 import it.unibo.alchemist.boundary.gui.tape.JTapeMainFeature;
 import it.unibo.alchemist.boundary.gui.tape.JTapeSection;
 
+import javax.swing.Icon;
+import javax.swing.JSlider;
+import javax.swing.JToggleButton;
+import javax.swing.event.ChangeListener;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
-import javax.swing.AbstractButton;
-import javax.swing.Icon;
-import javax.swing.JSlider;
-import javax.swing.JToggleButton;
-import javax.swing.event.ChangeListener;
-
+import static it.unibo.alchemist.boundary.gui.AlchemistSwingUI.loadScaledImage;
 import static it.unibo.alchemist.boundary.l10n.LocalizedResourceBundle.getString;
 
 /**
@@ -101,7 +99,7 @@ public class ReactivityPanel extends JTapeGroup implements ItemListener {
     }
 
     /**
-     * See {@link AbstractButton#addActionListener(ActionListener)}.
+     * See {@link JToggleButton#addActionListener(ActionListener)}.
      * 
      * @param l
      *            the {@link ActionListener} to add

375ed69aa5740d0a16bc9355f667d480beba6db3
Federico Pettinari <federico.pettinari2@studio.unibo.it>
2019-07-12 22:00:07 +0200

    Support rotating toward a direction

--- alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/interfaces/geometry/euclidean/twod/Euclidean2DTransformation.kt
+++ alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/interfaces/geometry/euclidean/twod/Euclidean2DTransformation.kt
@@ -2,6 +2,7 @@ package it.unibo.alchemist.model.interfaces.geometry.euclidean.twod
 
 import it.unibo.alchemist.model.implementations.positions.Euclidean2DPosition
 import it.unibo.alchemist.model.interfaces.geometry.GeometricTransformation
+import kotlin.math.atan2
 
 /**
  * Defines the possible transformations for a {@link GeometricShape} in a bidimensional euclidean space.
@@ -11,7 +12,15 @@ interface Euclidean2DTransformation : GeometricTransformation<Euclidean2DPositio
     /**
      * Counter clockwise rotation
      *
-     * @parameter angle the angle in radians
+     * @param angle the angle in radians
      */
     fun rotate(angle: Double)
+
+    /**
+     * Rotates toward the specified direction.
+     *
+     * @param direction the direction vector
+     */
+    fun rotate(direction: Euclidean2DPosition) =
+        rotate(atan2(direction.y, direction.x))
 }

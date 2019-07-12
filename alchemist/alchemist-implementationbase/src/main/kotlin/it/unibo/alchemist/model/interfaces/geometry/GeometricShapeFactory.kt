0642f7b30078e3e45abd7b4e013a9ce8ae1ce4e0
Federico Pettinari <federico.pettinari2@studio.unibo.it>
2019-07-12 21:26:05 +0200

    Redefine the shape factory

--- alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/interfaces/geometry/GeometricShapeFactory.kt
+++ alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/interfaces/geometry/GeometricShapeFactory.kt
@@ -1,75 +1,58 @@
 package it.unibo.alchemist.model.interfaces.geometry
 
-import it.unibo.alchemist.model.implementations.geometry.AwtGeometricShape2DFactory
-import it.unibo.alchemist.model.implementations.positions.Euclidean2DPosition
-import it.unibo.alchemist.model.interfaces.Position2D
+import it.unibo.alchemist.model.implementations.geometry.AdimensionalShape
+import it.unibo.alchemist.model.implementations.geometry.euclidean.twod.AwtEuclidean2DShapeFactory
+import it.unibo.alchemist.model.interfaces.geometry.euclidean.twod.Euclidean2DShapeFactory
 
 /**
- * Factory for {@link GeometricShape}.
+ * Generic factory for {@link GeometricShape}.
  */
-interface GeometricShape2DFactory<P : Position2D<P>> {
+interface GeometricShapeFactory<S : Vector<S>, A : GeometricTransformation<S>> {
 
     /**
-     * A circle extends in the first and second axis by its diameter.
-     *
-     * @param radius the radius
-     * @return the shape
+     * A special shape which does not occupy space and does not intersect with any other, not even with itself.
      */
-    fun circle(radius: Double): GeometricShape<P>
+    fun adimensional(): AdimensionalShape<S, A>
 
     /**
-     * A rectangle extends in the first and second axis by its width and height.
+     * Requires that the given shape is compatible with the ones provided by this factory, otherwise throws an exception.
      *
-     * @param width the width
-     * @param height the height
-     * @return the shape
+     * @param shape the shape to check
+     * @return the same shape
      */
-    fun rectangle(width: Double, height: Double): GeometricShape<P>
-
-    /**
-     * A circle sector is the portion of a disk enclosed by two radii and an arc
-     * and it extends in the first and second axis by its radius and angle.
-     *
-     *
-     * @param radius the radius of the circle from which the sector is extracted
-     * @param angle the angle of the arc in radians, it determines its dimension in the second axis
-     * @param heading the angle in radians of the median segment which bisects the sector.
-     *                  It's used to determine the sector's heading.
-     * @return the shape
-     */
-    fun circleSector(radius: Double, angle: Double, heading: Double): GeometricShape<P>
-
-    /**
-     * A punctiform shape occupies no space.
-     * @return the shape
-     */
-    fun punctiform(): GeometricShape<P>
+    fun requireCompatible(shape: GeometricShape<*, *>): GeometricShape<S, A>
 
     companion object {
         /**
-         * Retrieves a factory of {@link GeometricShape} compatible with the given {@link Position} type.
-         * Meant for java compatibility.
+         * Retrieves a factory of {@link GeometricShape} compatible with the given vector type.
          *
-         * @param <P> The type of the {@link Position}
-         * @param type The type of the {@link Position} wanted
+         * @param <S> The type of vector used
+         * @param <A> The supported geometric transformations
+         * @param <F> The interface of the factory requested
+         * @return the factory
+         */
+        inline fun <reified S : Vector<S>, reified A : GeometricTransformation<S>, reified F : GeometricShapeFactory<S, A>> getInstance(): F =
+            getInstance(F::class.java)
+
+        /**
+         * Retrieves a factory of {@link GeometricShape} compatible with the given space.
+         * (This method is meant for compatibility with java).
+         *
+         * @param <S> The type of vector used
+         * @param <A> The supported geometric transformations
+         * @param <F> The interface of the factory requested
+         * @param type The interface of the factory requested
          * @return the factory
          */
         @JvmStatic
         @Suppress("UNCHECKED_CAST")
-        fun <P : Position2D<P>> getInstance(type: Class<P>): GeometricShape2DFactory<P> =
+        fun <S : Vector<S>, A : GeometricTransformation<S>, F : GeometricShapeFactory<S, A>> getInstance(
+            type: Class<F>
+        ): F =
             when (type) {
-                Euclidean2DPosition::class.java -> AwtGeometricShape2DFactory()
-                else -> throw NotImplementedError("GeometricShape2DFactory<" + type.simpleName + "> isn't implemented yet")
-            } as GeometricShape2DFactory<P>
-
-        /**
-         * Retrieves a factory of {@link GeometricShape} compatible with the given {@link Position} type.
-         *
-         * @param <P> The type of the {@link Position}
-         * @return the factory
-         */
-        inline fun <reified P : Position2D<P>> getInstance(): GeometricShape2DFactory<P> =
-            getInstance(P::class.java)
+                Euclidean2DShapeFactory::class.java -> AwtEuclidean2DShapeFactory()
+                else -> throw NotImplementedError("No implementation found for GeometricShapeFactory<" + type.simpleName + ">")
+            } as F
     }
 }
 

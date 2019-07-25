074dd470340622b08bd57794b416c02c686f1399
Federico Pettinari <federico.pettinari2@studio.unibo.it>
2019-07-25 18:28:35 +0200

    Fix doc

--- alchemist/alchemist-smartcam/src/main/kotlin/it/unibo/alchemist/model/smartcam/FieldOfView2D.kt
+++ alchemist/alchemist-smartcam/src/main/kotlin/it/unibo/alchemist/model/smartcam/FieldOfView2D.kt
@@ -3,6 +3,9 @@ package it.unibo.alchemist.model.smartcam
 import it.unibo.alchemist.model.implementations.positions.Euclidean2DPosition
 import it.unibo.alchemist.model.interfaces.environments.EuclideanPhysics2DEnvironment
 
+/**
+ * TODO: Use influence spheres
+ */
 class FieldOfView2D<T> (
     private val env: EuclideanPhysics2DEnvironment<T>,
     position: Euclidean2DPosition,
@@ -10,8 +13,12 @@ class FieldOfView2D<T> (
     angle: Double,
     heading: Double = 0.0
 ) {
-    private val shape = env.shapeFactory.circleSector(distance, angle, heading).transformed { origin(position) }
 
+    private val shape = env.shapeFactory.circleSector(distance, angle, heading).transformed { origin(position) }
+    
+    /**
+     * TODO: Use influence spheres
+     */
     fun influencedNodes() =
         env.getNodesWithin(shape)
 }

2896d805e172522295e3e8ce7f3247edabea4fc0
Danilo Pianini <danilo.pianini@unibo.it>
2019-08-02 13:25:31 +0200

    Require Protelis 13.0.2

--- alchemist/buildSrc/src/main/kotlin/Versions.kt
+++ alchemist/buildSrc/src/main/kotlin/Versions.kt
@@ -123,7 +123,7 @@ object Versions {
 
     const val pegdown: String = "1.6.0" 
 
-    const val org_protelis: String = "13.0.0"
+    const val org_protelis: String = "13.0.2"
 
     const val org_scala_lang: String = "2.12.2" // available: "2.13.0"
 

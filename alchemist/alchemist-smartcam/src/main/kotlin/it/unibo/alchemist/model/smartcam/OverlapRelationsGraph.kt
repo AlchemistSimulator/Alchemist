042441bf13ed331b2c1dcaeb5f6535967315c1cb
Federico Pettinari <federico.pettinari2@studio.unibo.it>
2019-08-13 21:03:18 +0200

    Add OverlapRelationsGraph

---
+++ alchemist/alchemist-smartcam/src/main/kotlin/it/unibo/alchemist/model/smartcam/OverlapRelationsGraph.kt
@@ -0,0 +1,43 @@
+package it.unibo.alchemist.model.smartcam
+
+/**
+ * Models the partial view of each camera of the overlap relations graph
+ * as described in "Online Multi-object k-coverage with Mobile Smart Cameras" by Lukas Esterle and Peter Lewis.
+ * It is a map from the camera id to the relative link strength.
+ * Links can be strengthen, increasing their values by [strengthenValue].
+ * Links can be evaporated, multiplying their values with the factors [evaporationBaseFactor] and [evaporationMovementFactor].
+ */
+class OverlapRelationsGraph(
+    private val strengthenValue: Double,
+    private val evaporationBaseFactor: Double,
+    private val evaporationMovementFactor: Double
+) {
+
+    /**
+     * Returns all the links in form of a map from the camera id to the link strength.
+     */
+    val links: Map<Int, Double>
+        get() = _links
+    private val _links = mutableMapOf<Int, Double>()
+
+    /**
+     * Increase link strength by [strengthenValue].
+     */
+    fun strengthenLink(cameraId: Int) {
+        _links.merge(cameraId, strengthenValue) {v1, v2 -> v1 + v2}
+    }
+
+    /**
+     * Decrease link strength by [evaporationBaseFactor] and additionally decreased it by [evaporationMovementFactor]
+     * if [moved] is true.
+     */
+    fun evaporateAllLinks(moved: Boolean) {
+        _links.replaceAll { _, value ->
+            var newValue = value * evaporationBaseFactor
+            if (moved) {
+                newValue *= evaporationMovementFactor
+            }
+            newValue
+        }
+    }
+}

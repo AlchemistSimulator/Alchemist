68452439f3428ff073d6395131b89698d391ef56
Federico Pettinari <federico.pettinari2@studio.unibo.it>
2019-08-13 20:33:33 +0200

    Spin in degrees

--- alchemist/alchemist-smartcam/src/main/kotlin/it/unibo/alchemist/model/implementations/actions/Spin.kt
+++ alchemist/alchemist-smartcam/src/main/kotlin/it/unibo/alchemist/model/implementations/actions/Spin.kt
@@ -7,32 +7,31 @@ import it.unibo.alchemist.model.interfaces.Context
 import it.unibo.alchemist.model.interfaces.Node
 import it.unibo.alchemist.model.interfaces.Reaction
 import it.unibo.alchemist.model.interfaces.environments.EuclideanPhysics2DEnvironment
+import org.apache.commons.math3.util.FastMath
 import kotlin.math.cos
 import kotlin.math.sin
 
 /**
- * Spins a node around itself.
- * @param <T> concentration type
- * @param node the node
- * @param reaction the reaction hosting this action
- * @param env the environment containing the node
- * @param speed angular speed in radians
+ * Spins a [node] around itself at [angularSpeedDegrees] normalized according to the speed of the [reaction].
  */
 class Spin<T>(
     node: Node<T>,
     private val reaction: Reaction<T>,
     private val env: EuclideanPhysics2DEnvironment<T>,
-    private val speed: Double
+    private val angularSpeedDegrees: Double
 ) : AbstractAction<T>(node) {
+
+    private val angularSpeedRadians = FastMath.toRadians(angularSpeedDegrees)
+
     override fun cloneAction(n: Node<T>, r: Reaction<T>) =
-        Spin(n, r, env, speed)
+        Spin(n, r, env, angularSpeedDegrees)
 
     /**
      * Spins the node around itself.
      */
     @SuppressFBWarnings("SA_LOCAL_SELF_ASSIGNMENT")
     override fun execute() {
-        val realSpeed = speed / reaction.rate
+        val realSpeed = angularSpeedRadians / reaction.rate
         val headingAngle = env.getHeading(node).asAngle() + realSpeed
         env.setHeading(node, Euclidean2DPosition(cos(headingAngle), sin(headingAngle)))
     }

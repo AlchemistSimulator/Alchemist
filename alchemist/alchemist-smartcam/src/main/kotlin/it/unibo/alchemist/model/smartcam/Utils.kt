17b4623d9017c78c77abca1328febbc8e4aed410
Federico Pettinari <federico.pettinari2@studio.unibo.it>
2019-08-26 14:15:00 +0200

    Fix formatting

--- alchemist/alchemist-smartcam/src/main/kotlin/it/unibo/alchemist/model/smartcam/Utils.kt
+++ alchemist/alchemist-smartcam/src/main/kotlin/it/unibo/alchemist/model/smartcam/Utils.kt
@@ -10,17 +10,17 @@ import kotlin.math.sin
 
 internal inline fun <reified P : Position<P>> Any?.toPosition(env: Environment<*, P>): P = when (this) {
     is P -> this
-        is Tuple -> {
-            this.asIterable()
-                .map<Any?, Number> {
-                    require(it is Number) {
-                        "The Tuple must contain only Numbers but {$it::class} has been found"
-                    }
-                    it
-                }.let { env.makePosition(*it.toTypedArray()) }
-        }
-    else -> throw IllegalArgumentException("Expected a Protelis Tuple or Euclidean2DPosition but got a ${this?.javaClass}")
+    is Tuple -> {
+        this.asIterable()
+            .map<Any?, Number> {
+                require(it is Number) {
+                    "The Tuple must contain only Numbers but {$it::class} has been found"
+                }
+                it
+            }.let { env.makePosition(*it.toTypedArray()) }
     }
+    else -> throw IllegalArgumentException("Expected a Protelis Tuple or Euclidean2DPosition but got a ${this?.javaClass}")
+}
 
 internal fun offsetPositionAtDistance(env: Environment<*, Euclidean2DPosition>, source: Euclidean2DPosition, direction: Euclidean2DPosition, distance: Double) =
     with(direction.asAngle()) {

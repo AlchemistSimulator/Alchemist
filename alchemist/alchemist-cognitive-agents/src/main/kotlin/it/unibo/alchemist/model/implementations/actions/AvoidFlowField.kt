42831032231911017b41ae596f0d33cfa940ec06
Diego Mazzieri <diego.maz97@gmail.com>
2019-08-27 15:29:13 +0200

    Rename gradient in flow field according to Reynolds' formulation

--- alchemist/alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/implementations/actions/AvoidFlowField.kt
+++ alchemist/alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/implementations/actions/AvoidFlowField.kt
@@ -15,12 +15,12 @@ import it.unibo.alchemist.model.interfaces.environments.EuclideanPhysics2DEnviro
  * @param targetMolecule
  *          the {@link Molecule} you want to know the concentration in the different positions of the environment.
  */
-open class AvoidGradient<T>(
-    env: EuclideanPhysics2DEnvironment<T>,
-    reaction: Reaction<T>,
-    pedestrian: Pedestrian2D<T>,
+open class AvoidFlowField(
+    env: EuclideanPhysics2DEnvironment<Double>,
+    reaction: Reaction<Double>,
+    pedestrian: Pedestrian2D<Double>,
     targetMolecule: Molecule
-) : GradientSteeringAction<T>(
+) : FlowFieldSteeringAction<Double>(
     env,
     reaction,
     pedestrian,
@@ -28,8 +28,8 @@ open class AvoidGradient<T>(
     { molecule ->
         val currentPosition = env.getPosition(pedestrian)
         val layer = env.getLayer(molecule).get()
-        val currentConcentration = layer.getValue(currentPosition) as Double
-        this.map { it to layer.getValue(it) as Double }
+        val currentConcentration = layer.getValue(currentPosition)
+        this.map { it to layer.getValue(it) }
             .filter { it.second < currentConcentration }
             .minBy { it.second }?.first ?: currentPosition
     }

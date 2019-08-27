c841ef98ce22ddad39d017b4dff60222dbc7adaf
Placu <andrea.placuzzi@studio.unibo.it>
2019-08-27 16:43:13 +0200

    add test for rectangle shape with negative dimension

--- alchemist/alchemist-loading/src/test/java/it/unibo/alchemist/test/TestRectangle.java
+++ alchemist/alchemist-loading/src/test/java/it/unibo/alchemist/test/TestRectangle.java
@@ -9,6 +9,7 @@ package it.unibo.alchemist.test;
 
 import it.unibo.alchemist.loader.shapes.Rectangle;
 import it.unibo.alchemist.loader.shapes.Shape;
+import it.unibo.alchemist.model.implementations.positions.Euclidean2DPosition;
 import it.unibo.alchemist.model.implementations.positions.LatLongPosition;
 import it.unibo.alchemist.model.interfaces.GeoPosition;
 import org.junit.jupiter.api.Test;
@@ -28,6 +29,20 @@ public class TestRectangle {
         final Shape<GeoPosition> s = new Rectangle<>(12, 44, 1, 1);
         // CHECKSTYLE: MagicNumber OFF
         assertTrue(s.contains(new LatLongPosition(44.132300, 12.233000)));
+        // CHECKSTYLE: MagicNumber ON
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void rectangleWithNegativeDimension() {
+        /*
+         * In this rectangle the x should go from 10 to 15 and the y from 45 to 35
+         */
+        final Shape<Euclidean2DPosition> s = new Rectangle<>(15, 45, -5, -10);
+        // CHECKSTYLE: MagicNumber OFF
+        assertTrue(s.contains(new Euclidean2DPosition(12, 40)));
         // CHECKSTYLE: MagicNumber ON
     }
 

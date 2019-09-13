7f8a4ed707f9e275affbdd2a837a8ad8bb7c06fc
Federico Pettinari <federico.pettinari2@studio.unibo.it>
2019-09-13 16:35:39 +0200

    Make LevyWalk a RandomWalker

--- alchemist/alchemist-smartcam/src/main/kotlin/it/unibo/alchemist/model/implementations/actions/LevyWalk.kt
+++ alchemist/alchemist-smartcam/src/main/kotlin/it/unibo/alchemist/model/implementations/actions/LevyWalk.kt
@@ -1,18 +1,16 @@
 package it.unibo.alchemist.model.implementations.actions
 
-import it.unibo.alchemist.model.implementations.movestrategies.LevyWalkTarget
-import it.unibo.alchemist.model.implementations.movestrategies.speed.GloballyConstantSpeed
 import it.unibo.alchemist.model.implementations.positions.Euclidean2DPosition
-import it.unibo.alchemist.model.implementations.routes.PolygonalChain
 import it.unibo.alchemist.model.interfaces.Environment
 import it.unibo.alchemist.model.interfaces.Node
 import it.unibo.alchemist.model.interfaces.Reaction
-import it.unibo.alchemist.model.interfaces.movestrategies.RoutingStrategy
+import org.apache.commons.math3.distribution.ParetoDistribution
 import org.apache.commons.math3.random.RandomGenerator
 
 /**
- * Selects a target based on a random direction extracted from [rng] (which should be an uniform random generator),
- * and a random distance extracted from a lévy distribution of parameters [location] (aka mu) and [scale] (aka c).
+ * Selects a target based on a random direction extracted from [rng],
+ * and a random distance extracted from a [ParetoDistribution] of parameters [scale] and [shape].
+ * Moves toward the targets at a constant [speed] and changes targets on collision.
  */
 class LevyWalk<T> @JvmOverloads constructor(
     node: Node<T>,
@@ -20,15 +18,9 @@ class LevyWalk<T> @JvmOverloads constructor(
     private val env: Environment<T, Euclidean2DPosition>,
     private val rng: RandomGenerator,
     private val speed: Double,
-    private val location: Double = 0.0, // default parameters for the "standard" lévy walk distribution
-    private val scale: Double = 1.0
-) : AbstractConfigurableMoveNodeWithAccurateEuclideanDestination<T>(
-    env,
-    node,
-    RoutingStrategy { p1, p2 -> PolygonalChain<Euclidean2DPosition>(listOf(p1, p2)) },
-    LevyWalkTarget<T>(node, env, rng, location, scale),
-    GloballyConstantSpeed(reaction, speed)
-) {
+    private val scale: Double = 1.0, // default parameters for the Pareto distribution
+    private val shape: Double = 1.0
+) : RandomWalker<T>(node, reaction, env, rng, speed, ParetoDistribution(rng, scale, shape)) {
     override fun cloneAction(n: Node<T>, r: Reaction<T>) =
-        LevyWalk(n, r, env, rng, speed, location, scale)
+        LevyWalk(n, r, env, rng, speed, scale, shape)
 }

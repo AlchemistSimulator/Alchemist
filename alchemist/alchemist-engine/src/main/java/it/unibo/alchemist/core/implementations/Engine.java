03e7b200b8d72e34263162c0ea50f3757097e5f8
Vuksa <mihajlovic.vuksa@gmail.com>
2019-10-16 13:13:55 +0200

    Fix checkstyle issues

--- alchemist/alchemist-engine/src/main/java/it/unibo/alchemist/core/implementations/Engine.java
+++ alchemist/alchemist-engine/src/main/java/it/unibo/alchemist/core/implementations/Engine.java
@@ -33,8 +33,6 @@ import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Queue;
 import java.util.Set;

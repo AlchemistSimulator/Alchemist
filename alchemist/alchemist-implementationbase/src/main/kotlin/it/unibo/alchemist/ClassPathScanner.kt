cdecf7538c3cdd3792d6ca166fc088967c199024
Vuksa <mihajlovic.vuksa@gmail.com>
2019-10-23 18:35:59 +0200

    Replace ClassPathScanner.kt with mainline version

--- alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/ClassPathScanner.kt
+++ alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/ClassPathScanner.kt
@@ -8,7 +8,6 @@
 
 package it.unibo.alchemist
 
-import com.google.common.reflect.TypeToken
 import io.github.classgraph.ClassGraph
 import java.io.InputStream
 import java.net.URL
@@ -32,36 +31,12 @@ object ClassPathScanner {
     @JvmStatic
     @JvmOverloads
     @Suppress("UNCHECKED_CAST")
-    fun <T> subTypesOf(superClass: Class<T>, inPackage: String? = null): List<Class<out T>> =
-        subTypesOf(object : TypeToken<T>(superClass) {}, inPackage)
-
-    /**
-     * This function loads all subtypes of the provided Java class that can be discovered on the current classpath.
-     *
-     * This function cannot use `reified` and `inline` (as it should have) due to Java being unaware of the required
-     * transformation to use them.
-     */
-    @JvmStatic
-    @JvmOverloads
-    @Suppress("UNCHECKED_CAST")
-    inline fun <reified T> subTypesOf(inPackage: String? = null): List<Class<out T>> =
-        subTypesOf(T::class.java, inPackage)
-
-    /**
-     * This function loads all subtypes of the provided Java class that can be discovered on the current classpath.
-     *
-     * This function cannot use `reified` and `inline` (as it should have) due to Java being unaware of the required
-     * transformation to use them.
-     */
-    @JvmStatic
-    @JvmOverloads
-    @Suppress("UNCHECKED_CAST")
-    fun <T> subTypesOf(superClass: TypeToken<T>, inPackage: String? = null): List<Class<out T>> = classGraphForPackage(inPackage)
+    fun <T> subTypesOf(superClass: Class<T>, inPackage: String? = null): List<Class<out T>> = classGraphForPackage(inPackage)
         .enableClassInfo().scan().let { scanResult ->
-            if (superClass.rawType.isInterface) {
-                scanResult.getClassesImplementing(superClass.rawType.name)
+            if (superClass.isInterface) {
+                scanResult.getClassesImplementing(superClass.name)
             } else {
-                scanResult.getSubclasses(superClass.rawType.name)
+                scanResult.getSubclasses(superClass.name)
             }.filter { it -> !it.isAbstract }
                 .loadClasses()
                 .map { it -> it as Class<out T> }

b5b33559178a5e8aff6f59b35807d7bdb7c3b439
Danilo Pianini <danilo.pianini@unibo.it>
2019-11-06 16:31:38 +0100

    Fix wrong spacing

--- alchemist/alchemist-incarnation-scafi/src/main/scala/it/unibo/alchemist/model/implementations/nodes/ScafiNode.scala
+++ alchemist/alchemist-incarnation-scafi/src/main/scala/it/unibo/alchemist/model/implementations/nodes/ScafiNode.scala
@@ -9,7 +9,7 @@ package it.unibo.alchemist.model.implementations.nodes
 
 import it.unibo.alchemist.model.interfaces.{Environment, Position}
 
-class ScafiNode[T,P<:Position[P]](env: Environment[T, P]) extends AbstractNode[T](env) {
+class ScafiNode[T, P<:Position[P]](env: Environment[T, P]) extends AbstractNode[T](env) {
 
   override def createT = throw new Exception("The molecule does not exist and cannot create empty concentration")
 

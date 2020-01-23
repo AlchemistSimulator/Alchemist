834b823611aa8a2ad7abe16ab1dffe457ea9bc7a
Lorenzo Paganelli <lorenzo.paganelli3@studio.unibo.it>
2020-01-23 23:21:44 +0100

    suppress pmd warning

--- alchemist/alchemist-swingui/src/main/java/it/unibo/alchemist/boundary/gui/effects/DrawNavigationMesh.java
+++ alchemist/alchemist-swingui/src/main/java/it/unibo/alchemist/boundary/gui/effects/DrawNavigationMesh.java
@@ -55,7 +55,7 @@ public class DrawNavigationMesh implements Effect {
     @ExportForGUI(nameToExport = "env end y (north-east)")
     private String envEndY = "600";
     @ExportForGUI(nameToExport = "to be drawn")
-    private boolean toBeDrawn = false;
+    private boolean toBeDrawn = false; // NOPMD = if not initialized a NullPointerException is thrown
     private Color colorCache = Color.BLUE;
     private Collection<ConvexPolygon> regions = new ArrayList<>();
     @SuppressFBWarnings("SE_TRANSIENT_FIELD_NOT_RESTORED")

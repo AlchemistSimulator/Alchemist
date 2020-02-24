3f938c4d129b5c767274bcb4f38ed18a53bfc60c
Lorenzo Paganelli <lorenzo.paganelli3@studio.unibo.it>
2020-02-24 12:15:04 +0100

    add orienting pedestrian and orienting cognitive pedestrian

---
+++ alchemist/alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/interfaces/OrientingCognitivePedestrian.kt
@@ -0,0 +1,17 @@
+package it.unibo.alchemist.model.interfaces
+
+import it.unibo.alchemist.model.interfaces.geometry.ConvexGeometricShape
+import it.unibo.alchemist.model.interfaces.geometry.GeometricTransformation
+import it.unibo.alchemist.model.interfaces.geometry.Vector
+import it.unibo.alchemist.model.interfaces.geometry.graph.GraphEdge
+
+/**
+ * A heterogeneous pedestrian with cognitive and orienting capabilities.
+ *
+ * @param V the [Vector] type for the space this pedestrian is inside.
+ * @param A the transformations supported by the shapes in this space.
+ * @param N the type of landmarks stored in the pedestrian's [cognitiveMap].
+ * @param E the type of edges of the [cognitiveMap].
+ * @param T the concentration type.
+ */
+interface OrientingCognitivePedestrian<V : Vector<V>, A : GeometricTransformation<V>, N : ConvexGeometricShape<V, A>, E : GraphEdge<N>, T> : OrientingPedestrian<V, A, N, E, T>, CognitivePedestrian<T>
---
+++ alchemist/alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/interfaces/OrientingPedestrian.kt
@@ -0,0 +1,18 @@
+package it.unibo.alchemist.model.interfaces
+
+import it.unibo.alchemist.model.interfaces.geometry.ConvexGeometricShape
+import it.unibo.alchemist.model.interfaces.geometry.GeometricTransformation
+import it.unibo.alchemist.model.interfaces.geometry.Vector
+import it.unibo.alchemist.model.interfaces.geometry.graph.GraphEdge
+import it.unibo.alchemist.model.interfaces.geometry.graph.OrientingAgent
+
+/**
+ * A pedestrian capable of orienting itself.
+ *
+ * @param V the [Vector] type for the space this pedestrian is inside.
+ * @param A the transformations supported by the shapes in this space.
+ * @param N the type of landmarks stored in the pedestrian's [cognitiveMap].
+ * @param E the type of edges of the [cognitiveMap].
+ * @param T the concentration type.
+ */
+interface OrientingPedestrian<V : Vector<V>, A : GeometricTransformation<V>, N : ConvexGeometricShape<V, A>, E : GraphEdge<N>, T> : OrientingAgent<V, A, N, E>, Pedestrian<T>

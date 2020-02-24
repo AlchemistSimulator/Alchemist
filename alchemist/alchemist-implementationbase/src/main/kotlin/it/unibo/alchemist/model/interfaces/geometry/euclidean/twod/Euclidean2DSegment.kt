314d520e2aba68760b26692249bfd36a356fa470
Lorenzo Paganelli <lorenzo.paganelli3@studio.unibo.it>
2020-02-24 12:01:54 +0100

    add graph interface

--- alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/interfaces/geometry/euclidean/twod/Euclidean2DSegment.kt
+++ alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/interfaces/geometry/euclidean/twod/Euclidean2DSegment.kt
@@ -3,6 +3,6 @@ package it.unibo.alchemist.model.interfaces.geometry.euclidean.twod
 import it.unibo.alchemist.model.implementations.positions.Euclidean2DPosition
 
 /**
- * Defines an euclidean edge (or side) in a bidimensional space.
+ * Defines a segment in a bidimensional space.
  */
-typealias Euclidean2DEdge = Pair<Euclidean2DPosition, Euclidean2DPosition>
+typealias Euclidean2DSegment = Pair<Euclidean2DPosition, Euclidean2DPosition>
---
+++ alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/interfaces/geometry/graph/Graph.kt
@@ -0,0 +1,51 @@
+package it.unibo.alchemist.model.interfaces.geometry.graph
+
+/**
+ * Models a basic directed edge.
+ *
+ * @param N the type of the node this edge connects.
+ */
+open class GraphEdge<N>(
+    /**
+     */
+    val from: N,
+    /**
+     */
+    val to: N
+)
+
+/**
+ * A [GraphEdge] storing some kind of data.
+ *
+ * @param N the type nodes this edge connects.
+ * @param D the type of data.
+ */
+open class GraphEdgeWithData<N, D>(
+    from: N,
+    to: N,
+    /**
+     */
+    val data: D
+) : GraphEdge<N>(from, to)
+
+/**
+ * A graph composed by a set of nodes and a set of edges
+ * connecting such nodes. Edges are directed, undirected
+ * graphs can be obtained by duplicating each edge.
+ *
+ * @param N the type of nodes.
+ * @param E the type of edges.
+ */
+interface Graph<N, E : GraphEdge<N>> {
+
+    /**
+     * A list is used to allow predictable iteration order.
+     */
+    fun nodes(): List<N>
+
+    /**
+     * @returns the edges outgoing from the specified node.
+     * A list is used to allow predictable iteration order.
+     */
+    fun edgesFrom(node: N): List<E>
+}

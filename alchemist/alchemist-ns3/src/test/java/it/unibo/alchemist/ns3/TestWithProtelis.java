eef010539993103e094bbfc6820901ac183d1fd4
gscaparrotti@gmail.com <trackmania944@>
2020-02-26 18:33:53 +0100

    add new test for ns3

--- alchemist/alchemist-ns3/src/test/java/it/unibo/alchemist/ns3/TestWithProtelis.java
+++ alchemist/alchemist-ns3/src/test/java/it/unibo/alchemist/ns3/TestWithProtelis.java
@@ -34,30 +34,42 @@ public class TestWithProtelis {
      * Tests the integration with ns3 via ns3asy by running a simulation consisting in some nodes
      * executing a Protelis program which makes them send messages to each other. These messages
      * are delivered using ns3.
-     *
-     * @param <T> Position type
-     * @param <P> Concentration type
      */
     @Test
-    public <T, P extends Position<P>> void testNs3asy() {
+    public void testNs3asySimple() {
         if (System.getProperty("os.name").toLowerCase(Locale.ENGLISH).contains("linux")) {
-            final InputStream res = ResourceLoader.getResourceAsStream("ns3asy.yml");
-            final Environment<T, P> env = new YamlLoader(res).getWith(Collections.emptyMap());
-            final Simulation<T, P> sim = new Engine<>(env, 50);
-            sim.play();
-            sim.run();
+            final var env = testInSimulator("ns3asy.yml", 50);
             if (env.getIncarnation().isPresent()) {
                 for (final var node : env.getNodes()) {
-                    final int received = ((Double) node.getConcentration(env.getIncarnation().get().createMolecule("msgs_received"))).intValue();
+                    final var received = ((Double) node.getConcentration(env.getIncarnation().get().createMolecule("msgs_received"))).intValue();
                     assertTrue(received > 0);
+                    //System.out.println("The node " + node.toString() + " received " + received + " messages");
                 }
+                //System.out.println("Time is " + env.getSimulation().getTime());
             } else {
                 fail("Incarnation not present");
             }
-            sim.getError().ifPresent(Unchecked.consumer(e ->  {
-                throw e;
-            }));
         }
+    }
 
+    @Test
+    public void testChannel() {
+        if (System.getProperty("os.name").toLowerCase(Locale.ENGLISH).contains("linux")) {
+            final var env = testInSimulator("channel.yml", Long.MAX_VALUE);
+            //System.out.println("Step is " + env.getSimulation().getStep());
+            //System.out.println("Time is " + env.getSimulation().getTime());
+        }
+    }
+
+    private <T, P extends Position<P>> Environment<T, P> testInSimulator(final String ymlName, final long steps) {
+        final InputStream res = ResourceLoader.getResourceAsStream(ymlName);
+        final Environment<T, P> env = new YamlLoader(res).getWith(Collections.emptyMap());
+        final Simulation<T, P> sim = new Engine<>(env, steps);
+        sim.play();
+        sim.run();
+        sim.getError().ifPresent(Unchecked.consumer(e -> {
+            throw e;
+        }));
+        return env;
     }
 }

8260a4b688f0f94810e1f932cf64ebb0b23e6c00
Lorenzo Paganelli <lorenzo.paganelli3@studio.unibo.it>
2020-02-28 11:23:03 +0100

    refactor doc

--- alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/interfaces/geometry/ConvexGeometricShape.kt
+++ alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/interfaces/geometry/ConvexGeometricShape.kt
@@ -1,7 +1,7 @@
 package it.unibo.alchemist.model.interfaces.geometry
 
 /**
- * A convex shape.
+ * A convex [GeometricShape].
  *
  * This interface models a property instead of an object, this may be
  * unusual but consider it as a contract: interfaces are often said to

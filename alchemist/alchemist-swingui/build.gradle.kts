6b7c04f5c7d4a468a645c18e74d22efe5307d7a0
Lorenzo Paganelli <lorenzo.paganelli3@studio.unibo.it>
2020-02-28 16:25:15 +0100

    add cognitive agents project dependency

--- alchemist/alchemist-swingui/build.gradle.kts
+++ alchemist/alchemist-swingui/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     implementation(project(":alchemist-maps"))
     implementation(project(":alchemist-smartcam"))
     implementation(project(":alchemist-time"))
+    implementation(project(":alchemist-cognitive-agents"))
     implementation(Libs.miglayout_swing)
     implementation(Libs.mapsforge_map_awt) {
         exclude(group = "com.github.blackears", module = "svgSalamander")

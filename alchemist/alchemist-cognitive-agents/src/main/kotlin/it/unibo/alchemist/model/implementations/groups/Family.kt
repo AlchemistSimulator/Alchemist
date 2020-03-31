dd27783e3885da3ac283766e30fbd0b814994489
Danilo Pianini <danilo.pianini@gmail.com>
2020-03-31 11:00:51 +0200

    Documentation/style improvement

--- alchemist/alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/implementations/groups/Family.kt
+++ alchemist/alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/implementations/groups/Family.kt
@@ -3,9 +3,13 @@ package it.unibo.alchemist.model.implementations.groups
 import it.unibo.alchemist.model.interfaces.GroupWithLeader
 import it.unibo.alchemist.model.interfaces.Pedestrian
 
+/**
+ * A [Family] is modeled as a grup of pedestrians with a leader.
+ */
 class Family<T>(
     comparator: Comparator<Pedestrian<T>>
 ) : GenericGroup<T, Pedestrian<T>>(), GroupWithLeader<T, Pedestrian<T>> {
 
-    override val leader: Pedestrian<T> = members.minWith(comparator) ?: throw IllegalStateException("Can't determine a leader.")
+    override val leader: Pedestrian<T> =
+        members.minWith(comparator) ?: throw IllegalStateException("Can't determine a leader.")
 }

03ab77e9f7b837825c96fbf289e42b74c2208388
Danilo Pianini <danilo.pianini@gmail.com>
2020-03-31 20:11:59 +0200

    Use a more informative message

--- alchemist/alchemist-loading/src/main/kotlin/it/unibo/alchemist/loader/variables/JSR223Variable.kt
+++ alchemist/alchemist-loading/src/main/kotlin/it/unibo/alchemist/loader/variables/JSR223Variable.kt
@@ -38,7 +38,7 @@ data class JSR223Variable<R>(val language: String, val formula: String) : Depend
     override fun getWith(variables: Map<String, Any>): R = try {
         engine.eval(formula, variables.asBindings()) as R
     } catch (e: ScriptException) {
-        throw IllegalStateException(e)
+        throw IllegalStateException("Unable to evaluate $formula with bindings: $variables", e)
     }
 
     fun Map<String, Any>.asBindings(): Bindings = object : Bindings, MutableMap<String, Any> by this.toMutableMap() { }

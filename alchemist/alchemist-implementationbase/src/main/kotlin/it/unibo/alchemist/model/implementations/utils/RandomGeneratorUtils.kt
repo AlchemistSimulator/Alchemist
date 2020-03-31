e5abaf4b7f317434f3ffe9c1d1edea995c42eedc
Lorenzo Paganelli <lorenzo.paganelli3@studio.unibo.it>
2020-03-31 18:03:19 +0200

    suppress a warning

--- alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/implementations/utils/RandomGeneratorUtils.kt
+++ alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/implementations/utils/RandomGeneratorUtils.kt
@@ -27,6 +27,7 @@ fun RandomGenerator.position() = direction().let {
     Euclidean2DPosition(it.x * distance, it.y * distance)
 }
 
+@Suppress("MaxLineLength") // I think these are links for markdown.
 /**
  * [Fisher–Yates shuffle algorithm](https://www.worldcat.org/title/statistical-tables-for-biological-agricultural-and-medical-research/oclc/14222135)
  * using Apache random number generator. More information [on Wikipedia](https://en.wikipedia.org/wiki/Fisher-Yates_shuffle).

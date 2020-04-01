0f78b85ec78c7a3b0bee6b2d41a8bbd3c9c93b8c
Lorenzo Paganelli <lorenzo.paganelli3@studio.unibo.it>
2020-04-01 12:26:44 +0200

    add empty doc to AfterTime

--- alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/implementations/terminators/AfterTime.kt
+++ alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/implementations/terminators/AfterTime.kt
@@ -4,6 +4,8 @@ import it.unibo.alchemist.model.interfaces.Environment
 import it.unibo.alchemist.model.interfaces.Time
 import java.util.function.Predicate
 
+/**
+ */
 class AfterTime(val endTime: Time) : Predicate<Environment<*, *>> {
 
     /**

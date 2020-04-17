12826475db3a6ab41b160e1457154149468e6cd0
Lorenzo Paganelli <lorenzo.paganelli3@studio.unibo.it>
2020-04-17 13:16:43 +0200

    improve code quality

--- alchemist/alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/implementations/actions/ObstacleAvoidance.kt
+++ alchemist/alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/implementations/actions/ObstacleAvoidance.kt
@@ -2,6 +2,7 @@ package it.unibo.alchemist.model.implementations.actions
 
 import it.unibo.alchemist.model.implementations.positions.Euclidean2DPosition
 import it.unibo.alchemist.model.implementations.reactions.SteeringBehavior
+import it.unibo.alchemist.model.implementations.utils.origin
 import it.unibo.alchemist.model.interfaces.Action
 import it.unibo.alchemist.model.interfaces.Node
 import it.unibo.alchemist.model.interfaces.Obstacle2D
@@ -43,7 +44,7 @@ class ObstacleAvoidance<W : Obstacle2D<Euclidean2DPosition>, T>(
             /*
              * Otherwise we just don't apply any repulsion force.
              */
-            ?: env.makePosition(0.0, 0.0)
+            ?: env.origin()
     }
 
     /*

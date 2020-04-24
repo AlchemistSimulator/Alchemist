5f87d0b91c41c12986794a341b04a7c8b9f6cfd0
Lorenzo Paganelli <lorenzo.paganelli3@studio.unibo.it>
2020-04-24 11:27:22 +0200

    avoid flow field moves the pedestrian only when danger is percepted

--- alchemist/alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/implementations/actions/AvoidFlowField.kt
+++ alchemist/alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/implementations/actions/AvoidFlowField.kt
@@ -1,10 +1,14 @@
 package it.unibo.alchemist.model.implementations.actions
 
+import it.unibo.alchemist.model.implementations.nodes.CognitivePedestrian2D
+import it.unibo.alchemist.model.implementations.utils.origin
 import it.unibo.alchemist.model.interfaces.Action
 import it.unibo.alchemist.model.interfaces.Environment
+import it.unibo.alchemist.model.interfaces.EnvironmentWithObstacles
 import it.unibo.alchemist.model.interfaces.Layer
 import it.unibo.alchemist.model.interfaces.Molecule
 import it.unibo.alchemist.model.interfaces.Node
+import it.unibo.alchemist.model.interfaces.Pedestrian
 import it.unibo.alchemist.model.interfaces.Pedestrian2D
 import it.unibo.alchemist.model.interfaces.Position2D
 import it.unibo.alchemist.model.interfaces.Reaction
@@ -21,12 +25,15 @@ import it.unibo.alchemist.model.interfaces.geometry.Vector2D
  *          the owner of this action.
  * @param targetMolecule
  *          the {@link Molecule} you want to know the concentration in the different positions of the environment.
+ * @param viewDepth
+ *          the depth of view of the pedestrian, defaults to infinity.
  */
-open class AvoidFlowField<P>(
+class AvoidFlowField<P> @JvmOverloads constructor(
     env: Environment<Number, P>,
     reaction: Reaction<Number>,
-    pedestrian: Pedestrian2D<Number>,
-    targetMolecule: Molecule
+    override val pedestrian: Pedestrian2D<Number>,
+    targetMolecule: Molecule,
+    private val viewDepth: Double = Double.POSITIVE_INFINITY
 ) : FlowFieldSteeringAction<P>(env, reaction, pedestrian, targetMolecule)
     where
         P : Position2D<P>,
@@ -34,6 +41,15 @@ open class AvoidFlowField<P>(
 
     override fun cloneAction(n: Node<Number>, r: Reaction<Number>): Action<Number> =
         AvoidFlowField(env, r, n as Pedestrian2D<Number>, targetMolecule)
+
+    /**
+     * Moves the pedestrian only if he percepts the danger (either because it is in sight or
+     * due to social contagion).
+     */
+    override fun nextPosition(): P = when {
+        pedestrian.wantsToEvacuate() || isDangerInSight() -> super.nextPosition()
+        else -> env.origin()
+    }
 
     override fun Sequence<P>.selectPosition(layer: Layer<Number, P>, currentConcentration: Double): P = this
         .let {
@@ -50,4 +66,16 @@ open class AvoidFlowField<P>(
         .map { it to layer.concentrationIn(it) }
         .filter { it.second < currentConcentration }
         .minBy { it.second }?.first ?: currentPosition
+
+    /**
+     * Checks whether the center of the layer (if there's one) is in sight. If the layer
+     * has no center true is returned.
+     */
+    private fun isDangerInSight(): Boolean = getLayerOrFail().center()?.let {
+        it.distanceTo(currentPosition) <= viewDepth &&
+            (env !is EnvironmentWithObstacles<*, *, P> || !env.intersectsObstacle(currentPosition, it))
+    } ?: true
+
+    private fun Pedestrian<*>.wantsToEvacuate(): Boolean =
+        this is CognitivePedestrian2D<*> && this.danger == targetMolecule && this.wantsToEvacuate()
 }

6c0c515a2eb80a9cd798330a7727a30242f8ce23
Danilo Pianini <danilo.pianini@unibo.it>
2020-06-05 17:50:01 +0200

    Lift FollowAtDistance to implementationbase

--- alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/implementations/actions/FollowAtDistance.kt
+++ alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/implementations/actions/FollowAtDistance.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2010-2020, Danilo Pianini and contributors
+ * listed in the main project's alchemist/build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
 package it.unibo.alchemist.model.implementations.actions
 
 import it.unibo.alchemist.model.implementations.movestrategies.speed.GloballyConstantSpeed
@@ -33,7 +42,15 @@ class FollowAtDistance<T>(
 
     private val speedStrategy = GloballyConstantSpeed<Euclidean2DPosition>(reaction, speed)
 
-    override fun cloneAction(n: Node<T>, r: Reaction<T>) = FollowAtDistance(n, r, env, target, distance, speed)
+    override fun cloneAction(n: Node<T>, r: Reaction<T>) =
+        FollowAtDistance(
+            n,
+            r,
+            env,
+            target,
+            distance,
+            speed
+        )
 
     override fun execute() {
         node.getConcentration(target)?.also {

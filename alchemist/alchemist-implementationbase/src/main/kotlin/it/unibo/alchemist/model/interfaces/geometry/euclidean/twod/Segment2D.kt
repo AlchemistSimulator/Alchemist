d425dccab8477d1add66e8f29f94ceb238bdd4c0
Danilo Pianini <danilo.pianini@unibo.it>
2020-06-05 11:14:15 +0200

    Sort members

--- alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/interfaces/geometry/euclidean/twod/Segment2D.kt
+++ alchemist/alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/interfaces/geometry/euclidean/twod/Segment2D.kt
@@ -21,18 +21,6 @@ data class Segment2D<P : Vector2D<P>>(val first: P, val second: P) {
     val length: Double = toVector().magnitude
 
     /**
-     * @returns a shrunk version of the segment, [factor] is a percentage in [0, 0.5] indicating how much
-     * the segment should be reduced on each size.
-     */
-    fun shrunk(factor: Double): Segment2D<P> = when (factor) {
-        !in 0.0..0.5 -> throw IllegalArgumentException("$factor not in [0, 0.5]")
-        else -> copy(
-            first = first + (second - first).resized(factor * length),
-            second = second + (first - second).resized(factor * length)
-        )
-    }
-
-    /**
      * Indicates if the segment is aligned to the x axis.
      */
     val xAxisAligned: Boolean get() = MathUtils.fuzzyEquals(first.y, second.y)
@@ -46,11 +34,6 @@ data class Segment2D<P : Vector2D<P>>(val first: P, val second: P) {
      * Indicates if the segment is axis-aligned.
      */
     val isAxisAligned: Boolean get() = xAxisAligned || yAxisAligned
-
-    /**
-     * @returns the vector representing the movement from [first] to [second].
-     */
-    fun toVector() = second - first
 
     /**
      * The slope of the segment. If the two points coincide, this is [Double.NaN].
@@ -69,6 +52,28 @@ data class Segment2D<P : Vector2D<P>>(val first: P, val second: P) {
         MathUtils.fuzzyEquals(first.x, second.x) && MathUtils.fuzzyEquals(first.y, second.y)
 
     /**
+     * The medium point of the segment.
+     */
+    val midPoint get() = Euclidean2DPosition((first.x + second.x) / 2, (first.y + second.y) / 2)
+
+    /**
+     * @returns a shrunk version of the segment, [factor] is a percentage in [0, 0.5] indicating how much
+     * the segment should be reduced on each size.
+     */
+    fun shrunk(factor: Double): Segment2D<P> = when (factor) {
+        !in 0.0..0.5 -> throw IllegalArgumentException("$factor not in [0, 0.5]")
+        else -> copy(
+            first = first + (second - first).resized(factor * length),
+            second = second + (first - second).resized(factor * length)
+        )
+    }
+
+    /**
+     * @returns the vector representing the movement from [first] to [second].
+     */
+    fun toVector() = second - first
+
+    /**
      * Checks if the segment contains a [point]. Doubles are not directly compared,
      * [MathUtils.fuzzyEquals] is used instead.
      */
@@ -76,11 +81,6 @@ data class Segment2D<P : Vector2D<P>>(val first: P, val second: P) {
         areCollinear(first, second, point) &&
         point.x fuzzyIn rangeFromUnordered(first.x, second.x) &&
         point.y fuzzyIn rangeFromUnordered(first.y, second.y)
-
-    /**
-     * The medium point of the segment.
-     */
-    val midPoint get() = Euclidean2DPosition((first.x + second.x) / 2, (first.y + second.y) / 2)
 
     /**
      * Finds the point of the segment which is closest to the provided position.

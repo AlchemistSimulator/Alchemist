773783945a7a13c64624dc23103d0cb34a0dba09
Danilo Pianini <danilo.pianini@unibo.it>
2020-06-05 16:48:35 +0200

    Use Vector2D

--- alchemist/alchemist-smartcam/src/main/kotlin/it/unibo/alchemist/model/smartcam/Utils.kt
+++ alchemist/alchemist-smartcam/src/main/kotlin/it/unibo/alchemist/model/smartcam/Utils.kt
@@ -2,21 +2,20 @@ package it.unibo.alchemist.model.smartcam
 
 import it.unibo.alchemist.model.implementations.positions.Euclidean2DPosition
 import it.unibo.alchemist.model.interfaces.Environment
+import it.unibo.alchemist.model.interfaces.geometry.Vector2D
 import kotlin.math.cos
 import kotlin.math.sin
 
-internal fun offsetPositionAtDistance(
-    env: Environment<*, Euclidean2DPosition>,
-    source: Euclidean2DPosition,
-    direction: Euclidean2DPosition,
+internal fun <P: Vector2D<P>> offsetPositionAtDistance(
+    source: P,
+    direction: P,
     distance: Double
 ) = with(direction.asAngle) {
-    source + env.makePosition(cos(this) * distance, sin(this) * distance)
+    source + source.newFrom(cos(this) * distance, sin(this) * distance)
 }
 
-internal fun closestPositionToTargetAtDistance(
-    env: Environment<*, Euclidean2DPosition>,
-    source: Euclidean2DPosition,
-    target: Euclidean2DPosition,
+internal fun <P: Vector2D<P>> closestPositionToTargetAtDistance(
+    source: P,
+    target: P,
     distance: Double
-) = offsetPositionAtDistance(env, target, source - target, distance)
+) = offsetPositionAtDistance(target, source - target, distance)

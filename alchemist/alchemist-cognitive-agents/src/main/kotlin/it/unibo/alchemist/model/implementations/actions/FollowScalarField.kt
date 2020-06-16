ff11a9fb7a0725c69f21b575629104a9f4b502d2
Lorenzo Paganelli <lorenzo.paganelli3@studio.unibo.it>
2020-06-16 19:36:00 +0200

    replace scalar field actions with single one

--- alchemist/alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/implementations/actions/FollowScalarField.kt
+++ alchemist/alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/implementations/actions/FollowScalarField.kt
@@ -9,35 +9,104 @@
 
 package it.unibo.alchemist.model.implementations.actions
 
+import it.unibo.alchemist.model.implementations.layers.BidimensionalGaussianLayer
 import it.unibo.alchemist.model.interfaces.Action
 import it.unibo.alchemist.model.interfaces.Environment
+import it.unibo.alchemist.model.interfaces.EnvironmentWithObstacles
+import it.unibo.alchemist.model.interfaces.Layer
+import it.unibo.alchemist.model.interfaces.Molecule
 import it.unibo.alchemist.model.interfaces.Node
 import it.unibo.alchemist.model.interfaces.Pedestrian
 import it.unibo.alchemist.model.interfaces.Position2D
 import it.unibo.alchemist.model.interfaces.Reaction
+import it.unibo.alchemist.model.interfaces.environments.PhysicsEnvironment
 import it.unibo.alchemist.model.interfaces.geometry.GeometricTransformation
 import it.unibo.alchemist.model.interfaces.geometry.Vector2D
+import java.lang.IllegalArgumentException
 
 /**
- * Moves the pedestrian where the scalar field is higher.
+ * Moves the pedestrian where the given scalar field is higher.
  */
 class FollowScalarField<T, P, A>(
-    env: Environment<T, P>,
+    /**
+     * The environment the pedestrian is into.
+     */
+    private val env: Environment<T, P>,
     reaction: Reaction<T>,
     pedestrian: Pedestrian<T, P, A>,
-    concentrationIn: (P) -> Double,
-    center: P? = null
-) : AbstractScalarFieldAction<T, P, A>(env, reaction, pedestrian, concentrationIn, center)
+    /**
+     * A function mapping each position to a scalar value.
+     */
+    private val concentrationIn: (P) -> Double,
+    /**
+     * The position of either maximum or minimum concentration of the scalar field, can be null if such a position
+     * doesn't exists or isn't known. Its use is explained in [nextPosition].
+     */
+    private val center: P? = null
+) : AbstractSteeringAction<T, P, A>(env, reaction, pedestrian)
     where P : Position2D<P>, P : Vector2D<P>,
           A : GeometricTransformation<P> {
 
-    override fun Sequence<P>.selectPosition(currentPosition: P): P = this
-        .maxBy { concentrationIn(it) }
-        ?.takeIf { concentrationIn(it) > concentrationIn(currentPosition) }
-        ?: currentPosition
+
+    companion object {
+        /**
+         * Creates an instance of [FollowScalarField] from a [Layer]. The client can specify the [molecule] contained
+         * in the layer. If [invertValues] is true the layer will be inverted, which means the returned action will
+         * move the pedestrian where the scalar field is lower.
+         */
+        inline fun <P, reified C : FollowScalarField<Double, P, *>> fromLayer(
+            env: Environment<Double, P>,
+            molecule: Molecule,
+            invertValues: Boolean,
+            builder: ((P) -> Double, P?) -> C
+        ): C where P : Position2D<P>, P : Vector2D<P> {
+            val layer = env.getLayer(molecule).orElseThrow { IllegalArgumentException("no layer containing $molecule") }
+            val center = (layer as? BidimensionalGaussianLayer<P>)?.let { env.makePosition(it.centerX, it.centerY) }
+            val concentrationIn: (P) -> Double = if (invertValues) {
+                { position -> -layer.getValue(position) }
+            } else {
+                layer::getValue
+            }
+            return builder(concentrationIn, center)
+        }
+    }
+
+    /**
+     * @returns the next relative position reached by the pedestrian. The set of reachable positions is discretized
+     * using [Vector2D.surrounding] from the current position (radius is [maxWalk]). If the scalar field has a
+     * [center], two more positions are taken into account: one towards the center along the direction connecting the
+     * latter to the current position, and another away from the center along the same direction. The position with
+     * maximum concentration is then selected: if its concentration is higher than the concentration in the current
+     * position, the pedestrian moves there. Otherwise, it doesn't move at all.
+     */
+    override fun nextPosition(): P = currentPosition.let { currentPosition ->
+        val centerProjectedPositions = center?.let {
+            val direction = (center - currentPosition).coerceAtMost(maxWalk)
+            listOf(currentPosition + direction, currentPosition - direction)
+        } ?: emptyList()
+        (currentPosition.surrounding(maxWalk) + centerProjectedPositions)
+            .asSequence()
+            .enforceObstacles(currentPosition)
+            .filter { canFit(it) }
+            /*
+             * Next relative position.
+             */
+            .maxOr(currentPosition) - currentPosition
+    }
 
     override fun cloneAction(n: Node<T>, r: Reaction<T>): Action<T> =
         requireNodeTypeAndProduce<Pedestrian<T, P, A>, FollowScalarField<T, P, A>>(n) {
             FollowScalarField(env, reaction,  it, concentrationIn, center)
         }
+
+    private fun Sequence<P>.enforceObstacles(currentPosition: P): Sequence<P> =
+        if (env is EnvironmentWithObstacles<*, T, P>) map { env.next(currentPosition, it) } else this
+
+    private fun canFit(position: P): Boolean =
+        env !is PhysicsEnvironment<T, P, *, *> || env.canNodeFitPosition(pedestrian, position)
+
+    private fun Sequence<P>.maxOr(currentPosition: P): P = this
+        .maxBy { concentrationIn(it) }
+        ?.takeIf { concentrationIn(it) > concentrationIn(currentPosition) }
+        ?: currentPosition
 }

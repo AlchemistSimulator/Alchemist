8395391c04f9fc07fa7c4000148a99c60ffbad79
Vuksa Mihajlovic <mihajlovic.vuksa@gmail.com>
2020-07-22 21:17:05 +0200

    Add copyright notice to all files in alchemist-fxui that were missing it

--- alchemist/alchemist-fxui/src/main/java/it/unibo/alchemist/boundary/gui/effects/package-info.java
+++ alchemist/alchemist-fxui/src/main/java/it/unibo/alchemist/boundary/gui/effects/package-info.java
@@ -1,4 +1,13 @@
 
+/*
+ * Copyright (C) 2010-2020, Danilo Pianini and contributors
+ * listed in the main project's alchemist/build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
 /**
  * This package contains all the
  * {@link it.unibo.alchemist.boundary.gui.effects.EffectFX effects} and the

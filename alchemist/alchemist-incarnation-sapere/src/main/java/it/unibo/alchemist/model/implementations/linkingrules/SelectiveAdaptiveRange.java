8f5964390e33758145b9f64fa3e565d95262b345
Danilo Pianini <danilo.pianini@unibo.it>
2020-07-23 17:24:13 +0200

    Refactor long line

--- alchemist/alchemist-incarnation-sapere/src/main/java/it/unibo/alchemist/model/implementations/linkingrules/SelectiveAdaptiveRange.java
+++ alchemist/alchemist-incarnation-sapere/src/main/java/it/unibo/alchemist/model/implementations/linkingrules/SelectiveAdaptiveRange.java
@@ -148,7 +148,13 @@ public class SelectiveAdaptiveRange<T, P extends Position<P>> extends AdaptiveRa
      *            the amount of metres the range will be changed if out of the
      *            bounds
      */
-    public SelectiveAdaptiveRange(final double radius, final double minrange, final int desiredNeighborsCount, final int tolerance, final double adjustment) {
+    public SelectiveAdaptiveRange(
+            final double radius,
+            final double minrange,
+            final int desiredNeighborsCount,
+            final int tolerance,
+            final double adjustment
+    ) {
         this(radius, minrange, DEFAULT_MAXRANGE, desiredNeighborsCount, tolerance, adjustment, DEFAULT_MOLECULETYPE);
     }
 

8b1358f871e820c396b1abdd5499170965038f57
Danilo Pianini <danilo.pianini@unibo.it>
2020-07-23 15:18:34 +0200

    Refactor long line

--- alchemist/alchemist-implementationbase/src/main/java/it/unibo/alchemist/model/implementations/timedistributions/AnyRealDistribution.java
+++ alchemist/alchemist-implementationbase/src/main/java/it/unibo/alchemist/model/implementations/timedistributions/AnyRealDistribution.java
@@ -91,7 +91,12 @@ public class AnyRealDistribution<T> extends AbstractDistribution<T> {
 
     @SuppressFBWarnings(value = "FE_FLOATING_POINT_EQUALITY", justification = "We want to check for exact equality here")
     @Override
-    protected final void updateStatus(final Time curTime, final boolean executed, final double param, final Environment<T, ?> env) {
+    protected final void updateStatus(
+            final Time curTime,
+            final boolean executed,
+            final double param,
+            final Environment<T, ?> env
+    ) {
         if (param != getRate()) {
             throw new IllegalStateException(getClass().getSimpleName() + " does not allow to dynamically tune the rate.");
         }

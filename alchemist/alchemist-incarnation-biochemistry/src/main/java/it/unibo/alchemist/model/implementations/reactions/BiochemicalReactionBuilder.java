eaaf502ff3334a76925110b7b617fd5e40dd6bb4
Danilo Pianini <danilo.pianini@unibo.it>
2020-07-23 15:27:37 +0200

    Refactor parameter name

--- alchemist/alchemist-incarnation-biochemistry/src/main/java/it/unibo/alchemist/model/implementations/reactions/BiochemicalReactionBuilder.java
+++ alchemist/alchemist-incarnation-biochemistry/src/main/java/it/unibo/alchemist/model/implementations/reactions/BiochemicalReactionBuilder.java
@@ -294,9 +294,9 @@ public class BiochemicalReactionBuilder<P extends Position<P> & Vector<P>> {
 
         @Override
         public Reaction<Double> visitBiochemicalReactionRightInEnvContext(
-                final BiochemistrydslParser.BiochemicalReactionRightInEnvContextContext ctx
+                final BiochemistrydslParser.BiochemicalReactionRightInEnvContextContext context
         ) {
-            for (final BiochemicalReactionRightElemContext re : ctx.biochemicalReactionRightElem()) {
+            for (final BiochemicalReactionRightElemContext re : context.biochemicalReactionRightElem()) {
                 if (re.biomolecule() != null) {
                     final Biomolecule biomol = createBiomolecule(re.biomolecule());
                     final double concentration = createConcentration(re.biomolecule());

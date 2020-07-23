739570637c958b8f1cbc31076967ca353b932698
Danilo Pianini <danilo.pianini@unibo.it>
2020-07-23 18:13:21 +0200

    Refactor (long lines, parameter names)

--- alchemist/alchemist-loading/src/main/java/it/unibo/alchemist/loader/export/NumberOfNodes.java
+++ alchemist/alchemist-loading/src/main/java/it/unibo/alchemist/loader/export/NumberOfNodes.java
@@ -27,7 +27,12 @@ public final class NumberOfNodes implements Extractor {
     }
 
     @Override
-    public double[] extractData(final Environment<?, ?> environment, final Reaction<?> reaction, final it.unibo.alchemist.model.interfaces.Time time, final long step) {
+    public double[] extractData(
+            final Environment<?, ?> environment,
+            final Reaction<?> reaction,
+            final it.unibo.alchemist.model.interfaces.Time time,
+            final long step
+    ) {
         return new double[]{environment.getNodesNumber()};
     }
 

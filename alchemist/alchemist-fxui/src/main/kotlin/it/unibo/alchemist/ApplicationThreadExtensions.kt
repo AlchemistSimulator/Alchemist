d60cabcfa8283d328eb76252250e374328fed813
Vuksa Mihajlovic <mihajlovic.vuksa@gmail.com>
2020-08-26 18:43:02 +0200

    Remove timeout in syncRunLater extension functions

--- alchemist/alchemist-fxui/src/main/kotlin/it/unibo/alchemist/ApplicationThreadExtensions.kt
+++ alchemist/alchemist-fxui/src/main/kotlin/it/unibo/alchemist/ApplicationThreadExtensions.kt
@@ -12,34 +12,29 @@ package it.unibo.alchemist
 import javafx.application.Platform
 import javafx.application.Platform.runLater
 import java.util.concurrent.FutureTask
-import java.util.concurrent.TimeUnit
-
-private const val SYNC_RUN_LATER_TIMEOUT = 2000L
 
 /**
  * Runs the given task on the FX thread and waits for the task to finish, returning any value the task returns.
  * Throws an exception if the task takes more than a given amount of milliseconds.
  *
- * @param timeout the time in milliseconds to wait before throwing an exception, default is 2000.
  * @param task the task to execute.
  */
-fun <T : Any> syncRunLater(timeout: Long = SYNC_RUN_LATER_TIMEOUT, task: () -> T): T =
+fun <T : Any> syncRunLater(task: () -> T): T =
     FutureTask(task).run {
         runLater {
             run()
         }
-        get(timeout, TimeUnit.MILLISECONDS)
+        get()
     }
 
 /**
  * Checks if the current thread is the FX Application thread and calls [syncRunLater] if so, otherwise runs the task.
  *
- * @param timeout the time in milliseconds to wait before throwing an exception, default is 2000.
  * @param task the task to execute.
  */
-fun <T : Any> syncRunOnFXThread(timeout: Long = SYNC_RUN_LATER_TIMEOUT, task: () -> T): T =
+fun <T : Any> syncRunOnFXThread(task: () -> T): T =
     if (!Platform.isFxApplicationThread()) {
-        syncRunLater(timeout) { task() }
+        syncRunLater { task() }
     } else {
         task()
     }

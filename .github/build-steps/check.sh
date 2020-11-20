0e036559bb01379059005b8dfd664c40cae0329e
Danilo Pianini <danilo.pianini@gmail.com>
2020-11-20 12:08:30 +0000

    [Autodelivery] update Java multiplatform continuous integration pipeline from DanySK/centralized-automated-deployer@7717500

--- .github/build-steps/check.sh
+++ .github/build-steps/check.sh
@@ -8,6 +8,9 @@ elif [ -x 'gradlew' ]; then
     if ./gradlew tasks | grep '^check\s'; then
         echo 'Detected check task'
         ./gradlew check --parallel
+        if ./gradlew tasks | grep '^jacocoTestReport\s'; then
+            ./gradlew jacocoTestReport --parallel
+        fi
     else
         echo 'No known check tasks'
     fi

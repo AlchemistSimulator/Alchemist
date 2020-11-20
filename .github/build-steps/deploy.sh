a05705fbbbccfa32f39c838c79e26e4f85864ddc
Danilo Pianini <danilo.pianini@gmail.com>
2020-11-20 11:12:20 +0000

    [Autodelivery] update Java multiplatform continuous integration pipeline from DanySK/centralized-automated-deployer@6c11f54

---
+++ .github/build-steps/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+CUSTOM_BUILD_SCRIPT=${CUSTOM_BUILD_SCRIPT:-.github/scripts/deploy}
+if [ -x $CUSTOM_BUILD_SCRIPT ]; then
+    echo 'Detected custom deploy instructions'
+    $CUSTOM_BUILD_SCRIPT
+elif [ -x 'gradlew' ]; then
+    echo 'Detected gradle wrapper, checking for known tasks'
+    if ./gradlew tasks | grep '^deploy\s'; then
+        echo 'Detected deploy task'
+        ./gradlew deploy --parallel
+    elif ./gradlew tasks | grep '^publish\s'; then
+        echo 'Detected publish task'
+        ./gradlew publish --parallel
+    else
+        echo 'No deploy task'
+    fi
+elif [ -f 'pom.xml' ]; then
+    echo 'Detected Maven pom.xml, running mvn deploy'
+    mvn deploy
+else
+    echo 'No valid configuration detected, failing'
+    echo "To fix, provide an *executable* build script in $CUSTOM_BUILD_SCRIPT"
+    exit 1
+fi

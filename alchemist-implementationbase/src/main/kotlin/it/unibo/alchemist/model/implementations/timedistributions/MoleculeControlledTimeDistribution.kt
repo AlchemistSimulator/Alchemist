445ea1d4296e73e7a9adbcc128b63442ed3faa61
Danilo Pianini <danilo.pianini@gmail.com>
2021-01-28 10:46:20 +0100

    Fix error message

--- alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/implementations/timedistributions/MoleculeControlledTimeDistribution.kt
+++ alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/implementations/timedistributions/MoleculeControlledTimeDistribution.kt
@@ -162,7 +162,7 @@ class MoleculeControlledTimeDistribution<T> @JvmOverloads constructor(
             }
             require(currentValue >= 0) {
                 "You requested to be scheduled with a delta of $currentValue in molecule $molecule at node ${node.id}" +
-                    "Alchemist loves causality and won't let you go back in time"
+                    ". Alchemist loves causality and won't let you go back in time"
             }
             return currentValue
         }

5471e6faebd34c6fd654c6b6959b91422c767420
Danilo Pianini <danilo.pianini@gmail.com>
2021-07-09 17:23:35 +0200

    Protect the internal representation

--- alchemist-implementationbase/src/main/java/it/unibo/alchemist/model/implementations/actions/AbstractAction.java
+++ alchemist-implementationbase/src/main/java/it/unibo/alchemist/model/implementations/actions/AbstractAction.java
@@ -21,6 +21,7 @@ import org.danilopianini.util.ListSet;
 import it.unibo.alchemist.model.interfaces.Action;
 import it.unibo.alchemist.model.interfaces.Molecule;
 import it.unibo.alchemist.model.interfaces.Node;
+import org.danilopianini.util.ListSets;
 
 
 /**
@@ -77,7 +78,7 @@ public abstract class AbstractAction<T> implements Action<T> {
      */
     @Override
     public final ListSet<? extends Dependency> getOutboundDependencies() {
-        return dependencies;
+        return ListSets.unmodifiableListSet(dependencies);
     }
 
     /**

acad5b8a6c692fe635cfc2ecdccdd12a4a4aef93
Danilo Pianini <danilo.pianini@gmail.com>
2021-07-16 10:42:52 +0200

    Improve formatting

--- alchemist-loading/src/main/java/it/unibo/alchemist/loader/export/Exporter.java
+++ alchemist-loading/src/main/java/it/unibo/alchemist/loader/export/Exporter.java
@@ -54,10 +54,10 @@ public final class Exporter<T, P extends Position<? extends P>> implements Outpu
      * @throws FileNotFoundException if the file can not be opened for writing
      */
     public Exporter(
-            final String target,
-            final double space,
-            final String header,
-            final List<Extractor> columns
+        final String target,
+        final double space,
+        final String header,
+        final List<Extractor> columns
     ) throws FileNotFoundException {
         this.sampleSpace = space;
         try {

e1de485fc459dfef6c909e41a680588ca0860b28
Andrea Acampora <andrea.acampora@studio.unibo.it>
2021-10-28 14:35:41 +0200

    tests the creation of only CSVExporter

--- alchemist-loading/src/test/kotlin/it/unibo/alchemist/test/TestExportersCreation.kt
+++ alchemist-loading/src/test/kotlin/it/unibo/alchemist/test/TestExportersCreation.kt
@@ -9,7 +9,6 @@
 package it.unibo.alchemist.test
 
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.ints.shouldBeExactly
 import io.kotest.matchers.ints.shouldBeGreaterThan
 import it.unibo.alchemist.loader.InitializedEnvironment
 import it.unibo.alchemist.loader.LoadAlchemist
@@ -27,7 +26,7 @@ class TestExportersCreation<T, P : Position<P>> : StringSpec({
         assertNotNull(loader)
         val initialized: InitializedEnvironment<T, P> = loader.getDefault()
         val exporters: List<GenericExporter<T, P>> = initialized.exporters
-        exporters.size shouldBeExactly 2
+        exporters.size shouldBeGreaterThan 0
         exporters.forEach {
             it shouldNotBe null
         }

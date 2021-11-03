b7d0ecbc742c5ca81868e00337b567f698aed2ea
Andrea Acampora <andrea.acampora@studio.unibo.it>
2021-11-03 17:16:36 +0100

    Create MongoDBExporter class

---
+++ alchemist-loading/src/main/kotlin/it/unibo/alchemist/loader/export/MongoDBExporter.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2010-2021, Danilo Pianini and contributors
+ * listed in the main project's alchemist/build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.loader.export
+
+import com.mongodb.ConnectionString
+import com.mongodb.MongoClientSettings
+import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoClients
+import com.mongodb.client.MongoCollection
+import com.mongodb.client.MongoDatabase
+import it.unibo.alchemist.model.interfaces.Environment
+import it.unibo.alchemist.model.interfaces.Position
+import it.unibo.alchemist.model.interfaces.Reaction
+import it.unibo.alchemist.model.interfaces.Time
+import org.bson.Document
+
+/**
+ * Exports data provided by a list of [Extractor]s on a MongoDB instance.
+ * @param uri the connection URI of the database instance.
+ * @param dbname the name the database to export data to.
+ * @param interval the sampling time, defaults to [AbstractExporter.DEFAULT_INTERVAL].
+ */
+
+class MongoDBExporter<T, P : Position<P>> @JvmOverloads constructor(
+    val uri: String,
+    val dbname: String = DEFAULT_DATABASE,
+    val interval: Double = DEFAULT_INTERVAL
+) : AbstractExporter<T, P>() {
+
+    companion object {
+        /**
+         *  The default name used if no database name parameter is specified.
+         */
+        const val DEFAULT_DATABASE = "test"
+    }
+
+    private lateinit var database: MongoDatabase
+    private lateinit var collection: MongoCollection<Document>
+
+    override fun setupExportEnvironment(environment: Environment<T, P>) {
+        val mongoClient: MongoClient = MongoClients.create(
+            MongoClientSettings.builder()
+                .applyConnectionString(ConnectionString(uri))
+                .build()
+        )
+        database = mongoClient.getDatabase(dbname)
+        val collectionName: String = variablesDescriptor + System.currentTimeMillis()
+        database.createCollection(collectionName)
+        collection = database.getCollection(collectionName)
+    }
+
+    override fun exportData(environment: Environment<T, P>, reaction: Reaction<T>?, time: Time, step: Long) {
+        val curSample: Long = (time.toDouble() / interval).toLong()
+        if (curSample > count) {
+            count = curSample
+            pushToDB(environment, reaction, time, step)
+        }
+    }
+
+    override fun closeExportEnvironment(environment: Environment<T, P>, time: Time, step: Long) {
+        // TODO
+    }
+
+    private fun pushToDB(environment: Environment<T, P>, reaction: Reaction<T>?, time: Time, step: Long) {
+        val document = Document()
+        dataExtractor.stream()
+            .forEach {
+                it.extractData(environment, reaction, time, step).forEach {
+                    value -> document.append(it.names.toString(), value)
+                }
+            }
+        collection.insertOne(document)
+    }
+}

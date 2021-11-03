0f2032a4a95eda8e2830b338de65b874a48d9081
Andrea Acampora <andrea.acampora@studio.unibo.it>
2021-11-03 10:10:54 +0100

    add test for export interval

---
+++ alchemist-loading/src/test/kotlin/it/unibo/alchemist/test/TestExportInterval.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2010-2019, Danilo Pianini and contributors listed in the main project's alchemist/build.gradle file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.test
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.ints.shouldBeGreaterThan
+import it.unibo.alchemist.loader.InitializedEnvironment
+import it.unibo.alchemist.loader.LoadAlchemist
+import io.kotest.matchers.shouldNotBe
+import it.unibo.alchemist.loader.export.CSVExporter
+import it.unibo.alchemist.loader.export.GenericExporter
+import it.unibo.alchemist.model.interfaces.Position
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.kaikikm.threadresloader.ResourceLoader
+
+class TestExportInterval<T, P : Position<P>> : StringSpec({
+    "test export interval" {
+        val file = ResourceLoader.getResource("testExportInterval.yml")
+        assertNotNull(file)
+        val loader = LoadAlchemist.from(file)
+        assertNotNull(loader)
+        val initialized: InitializedEnvironment<T, P> = loader.getDefault()
+        val exporter: CSVExporter<T, P> = initialized.exporters[0] as CSVExporter<T, P>
+        exporter.interval shouldNotBe 1
+    }
+})

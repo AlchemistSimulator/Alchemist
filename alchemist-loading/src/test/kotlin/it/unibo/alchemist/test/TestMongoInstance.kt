613506ff4a53975db9c7ef512d8b1ead1d3bff9f
Andrea Acampora <andrea.acampora@studio.unibo.it>
2021-11-11 12:14:15 +0100

    test(loading): test the local instance of mongo

---
+++ alchemist-loading/src/test/kotlin/it/unibo/alchemist/test/TestMongoInstance.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2010-2019, Danilo Pianini and contributors listed in the main project's alchemist/build.gradle file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.test
+
+import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoClients
+import com.mongodb.client.MongoCollection
+import com.mongodb.client.MongoDatabase
+import com.mongodb.client.model.Filters.eq
+
+import de.flapdoodle.embed.mongo.MongodExecutable
+import de.flapdoodle.embed.mongo.MongodProcess
+import de.flapdoodle.embed.mongo.MongodStarter
+import de.flapdoodle.embed.mongo.config.ImmutableMongodConfig
+import de.flapdoodle.embed.mongo.config.MongodConfig
+import de.flapdoodle.embed.mongo.config.Net
+import de.flapdoodle.embed.mongo.distribution.Version
+import de.flapdoodle.embed.process.runtime.Network
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.longs.shouldBeGreaterThan
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+
+import org.bson.Document
+
+class TestMongoInstance : StringSpec({
+    "test the local instance of mongo" {
+
+        val starter: MongodStarter = MongodStarter.getDefaultInstance()
+        val mongodConfig: ImmutableMongodConfig = MongodConfig.builder()
+            .version(Version.Main.PRODUCTION)
+            .net(Net("localhost", 27017, Network.localhostIsIPv6()))
+            .build()
+        val mongodExecutable: MongodExecutable = starter.prepare(mongodConfig)
+        mongodExecutable shouldNotBe null
+        val mongodProcess: MongodProcess = mongodExecutable.start()
+        mongodProcess shouldNotBe null
+        val mongoClient: MongoClient = MongoClients.create()
+        mongoClient shouldNotBe null
+        val defaultDatabase: MongoDatabase = mongoClient.getDatabase("test")
+        defaultDatabase shouldNotBe null
+        val mongoCollection: MongoCollection<Document> = mongoClient.getDatabase("test").getCollection("mongo-test-collection")
+        mongoCollection.insertOne(Document("name","mongo-test-document"))
+        mongoCollection.countDocuments() shouldBeGreaterThan 0
+        mongoCollection.find(eq("name","mongo-test-document")).count() shouldBe 1
+        mongodProcess.stop()
+        mongodExecutable.stop()
+    }
+})

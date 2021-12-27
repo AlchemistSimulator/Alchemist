aea72a1aa4312f7898a3541383380eb9e6070573
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2021-12-27 21:15:17 +0100

    test(loading): add a test for the issue #1002 that verifies the column alignment problem of CSVExport

--- alchemist-loading/src/test/kotlin/it/unibo/alchemist/test/TestCSVExporter.kt
+++ alchemist-loading/src/test/kotlin/it/unibo/alchemist/test/TestCSVExporter.kt
@@ -12,6 +12,8 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.file.shouldExist
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import io.kotest.matchers.string.shouldNotBeEmpty
 import it.unibo.alchemist.core.implementations.Engine
 import it.unibo.alchemist.loader.InitializedEnvironment
 import it.unibo.alchemist.loader.LoadAlchemist
@@ -24,24 +26,53 @@ import java.io.File
 
 class TestCSVExporter<T, P : Position<P>> : StringSpec({
     "test exporting data on CSV file" {
-        val file = ResourceLoader.getResource("testCSVExporter.yml")
-        assertNotNull(file)
-        val loader = LoadAlchemist.from(file)
-        assertNotNull(loader)
-        val initialized: InitializedEnvironment<T, P> = loader.getDefault()
-        val simulation = Engine(initialized.environment)
-        simulation.addOutputMonitor(GlobalExporter(initialized.exporters))
-        simulation.play()
-        simulation.run()
+        val initialized: InitializedEnvironment<T, P> = loadAlchemist("testCSVExporter.yml")
+        runSimulationUsingExporters(initialized)
         initialized.exporters.size shouldBe 1
-        val exporter = initialized.exporters.first()
-        require(exporter is CSVExporter) {
-            "Invalid exporter type '${exporter::class.simpleName}'"
-        }
+        val exporter = getCSVExporter(initialized)
         val outputFile = File(exporter.exportPath)
             .listFiles()
             ?.find { it.name.startsWith("00-testing_csv_export_") && it.extension == exporter.fileExtension }
         outputFile.shouldNotBeNull()
         outputFile.shouldExist()
     }
-})
+    "test on column alignment" {
+        val initialized: InitializedEnvironment<T, P> = loadAlchemist("testCSVExportColumnAlignment.yml")
+        runSimulationUsingExporters(initialized)
+        val exporter = getCSVExporter(initialized)
+        // Get the first line of the output produce by CSVExporter
+        val exporterFirstLine = File(exporter.exportPath)
+            .listFiles()?.first { it.name.startsWith("column-alignment") }
+            ?.readLines()
+            ?.dropWhile { !it.contains("d c b a") } // remove the lines before the column names
+            ?.drop(1) // I am not interested in column head
+            ?.first()
+        exporterFirstLine.shouldNotBeNull()
+        exporterFirstLine.shouldNotBeEmpty()
+        exporterFirstLine.shouldContain("0 1 2 3")
+    }
+}) {
+    /* common utility functions */
+    companion object {
+        fun <T, P : Position<P>> loadAlchemist(simulationFile: String): InitializedEnvironment<T, P> {
+            val file = ResourceLoader.getResource(simulationFile)
+            assertNotNull(file)
+            val loader = LoadAlchemist.from(file)
+            assertNotNull(loader)
+            return loader.getDefault()
+        }
+        fun <T, P : Position<P>> runSimulationUsingExporters(env: InitializedEnvironment<T, P>) {
+            val simulation = Engine(env.environment)
+            simulation.addOutputMonitor(GlobalExporter(env.exporters))
+            simulation.play()
+            simulation.run()
+        }
+        fun <T, P : Position<P>> getCSVExporter(env: InitializedEnvironment<T, P>): CSVExporter<T, P> {
+            val exporter = env.exporters.first()
+            require(exporter is CSVExporter) {
+                "Invalid exporter type '${exporter::class.simpleName}'"
+            }
+            return exporter
+        }
+    }
+}

7e744e9da4b10835959d4a30c91740cc9fcd16bd
Danilo Pianini <danilo.pianini@gmail.com>
2021-12-28 13:14:38 +0100

    test(loading): refactor test utilities into alchemist-test

--- alchemist-loading/src/test/kotlin/it/unibo/alchemist/test/TestCSVExporter.kt
+++ alchemist-loading/src/test/kotlin/it/unibo/alchemist/test/TestCSVExporter.kt
@@ -14,20 +14,18 @@ import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
 import io.kotest.matchers.string.shouldNotBeEmpty
-import it.unibo.alchemist.core.implementations.Engine
 import it.unibo.alchemist.loader.InitializedEnvironment
-import it.unibo.alchemist.loader.LoadAlchemist
 import it.unibo.alchemist.loader.export.exporters.CSVExporter
-import it.unibo.alchemist.loader.export.exporters.GlobalExporter
 import it.unibo.alchemist.model.interfaces.Position
-import org.junit.jupiter.api.Assertions.assertNotNull
-import org.kaikikm.threadresloader.ResourceLoader
+import it.unibo.alchemist.testsupport.createSimulation
+import it.unibo.alchemist.testsupport.loadAlchemist
+import it.unibo.alchemist.testsupport.runInCurrentThread
 import java.io.File
 
 class TestCSVExporter<T, P : Position<P>> : StringSpec({
     "test exporting data on CSV file" {
         val initialized: InitializedEnvironment<T, P> = loadAlchemist("testCSVExporter.yml")
-        runSimulationUsingExporters(initialized)
+        initialized.createSimulation().runInCurrentThread()
         initialized.exporters.size shouldBe 1
         val exporter = getCSVExporter(initialized)
         val outputFile = File(exporter.exportPath)
@@ -36,13 +34,13 @@ class TestCSVExporter<T, P : Position<P>> : StringSpec({
         outputFile.shouldNotBeNull()
         outputFile.shouldExist()
     }
-    "test on column alignment" {
+    "column order should replicate" {
         val initialized: InitializedEnvironment<T, P> = loadAlchemist("testCSVExportColumnAlignment.yml")
-        runSimulationUsingExporters(initialized)
+        initialized.createSimulation().runInCurrentThread()
         val exporter = getCSVExporter(initialized)
         // Get the first line of the output produce by CSVExporter
-        val exporterFirstLine = File(exporter.exportPath)
-            .listFiles()?.first { it.name.startsWith("column-alignment") }
+        val exporterFirstLine = File(exporter.exportPath).listFiles()
+            ?.first { it.name.startsWith("column-alignment") }
             ?.readLines()
             ?.dropWhile { !it.contains("d c b a") } // remove the lines before the column names
             ?.drop(1) // I am not interested in column head
@@ -54,19 +52,6 @@ class TestCSVExporter<T, P : Position<P>> : StringSpec({
 }) {
     /* common utility functions */
     companion object {
-        fun <T, P : Position<P>> loadAlchemist(simulationFile: String): InitializedEnvironment<T, P> {
-            val file = ResourceLoader.getResource(simulationFile)
-            assertNotNull(file)
-            val loader = LoadAlchemist.from(file)
-            assertNotNull(loader)
-            return loader.getDefault()
-        }
-        fun <T, P : Position<P>> runSimulationUsingExporters(env: InitializedEnvironment<T, P>) {
-            val simulation = Engine(env.environment)
-            simulation.addOutputMonitor(GlobalExporter(env.exporters))
-            simulation.play()
-            simulation.run()
-        }
         fun <T, P : Position<P>> getCSVExporter(env: InitializedEnvironment<T, P>): CSVExporter<T, P> {
             val exporter = env.exporters.first()
             require(exporter is CSVExporter) {

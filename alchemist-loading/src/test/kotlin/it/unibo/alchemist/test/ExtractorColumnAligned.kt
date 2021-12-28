4125dd9ea6f30a0e8e2f5835e62cb7848c32b0af
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2021-12-28 10:41:34 +0100

    test(loading): add regression test for #1002 (#1005)
    * test(loading): add a test for the issue #1002 that verifies the column alignment problem of CSVExport
    
    * style(loading): solve detekt debts

---
+++ alchemist-loading/src/test/kotlin/it/unibo/alchemist/test/ExtractorColumnAligned.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2010-2021, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.test
+
+import it.unibo.alchemist.loader.export.Extractor
+import it.unibo.alchemist.model.interfaces.Environment
+import it.unibo.alchemist.model.interfaces.Reaction
+import it.unibo.alchemist.model.interfaces.Time
+
+/**
+ * Utility used to verify the correctness of CSVExport.
+ * When the columnNames matches the key set returned by extract data,
+ * the values for each line should follow the columns name
+ */
+class ExtractorColumnAligned : Extractor<Int> {
+    override val columnNames = listOf("d", "c", "b", "a")
+    override fun <T> extractData(
+        environment: Environment<T, *>,
+        reaction: Reaction<T>?,
+        time: Time,
+        step: Long
+    ): Map<String, Int> {
+        val leftMap = columnNames.zip(columnNames.indices).toMap()
+        return HashMap<String, Int>(leftMap)
+    }
+}

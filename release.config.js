aee1e74cc289f530d4f96fb620c015971f6f3e72
Danilo Pianini <danilo.pianini@unibo.it>
2022-01-18 18:50:41 +0100

    ci: deploy the Hugo website

--- release.config.js
+++ release.config.js
@@ -1,9 +1,10 @@
 var publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
-git push --force origin \${nextRelease.version}
+git --git-dir build/website/.git commit -m "chore: update website to version \${nextRelease.version}" || exit 3
+git --git-dir build/website/.git push || exit 4
 ./gradlew shadowJar --parallel || ./gradlew shadowJar --parallel || exit 1
 ./gradlew releaseKotlinMavenOnMavenCentralNexus --parallel || exit 2
-./gradlew orchidDeploy || ./gradlew orchidDeploy || exit 3
+git push --force origin \${nextRelease.version}
 ./gradlew publishKotlinMavenPublicationToGithubRepository --continue || true
 `
 var config = require('semantic-release-preconfigured-conventional-commits');

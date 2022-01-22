a49a0ca8c369c41fb092d8ae6d4e9adcad8efa77
Danilo Pianini <danilo.pianini@unibo.it>
2022-01-22 12:12:13 +0100

    ci(release): immediately push the annotated tag

--- release.config.js
+++ release.config.js
@@ -1,11 +1,11 @@
 var publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
+git push --force origin \${nextRelease.version} || exit 6
 git --git-dir build/website/.git add . || exit 1
 git --git-dir build/website/.git commit -m "chore: update website to version \${nextRelease.version}" || exit 2
 git --git-dir build/website/.git push || exit 3
 ./gradlew shadowJar --parallel || ./gradlew shadowJar --parallel || exit 4
 ./gradlew releaseKotlinMavenOnMavenCentralNexus --parallel || exit 5
-git push --force origin \${nextRelease.version} || exit 6
 ./gradlew publishKotlinMavenPublicationToGithubRepository --continue || true
 `
 var config = require('semantic-release-preconfigured-conventional-commits');

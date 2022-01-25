7805062893629eac28b438a3f6a4bda97980460b
Danilo Pianini <danilo.pianini@gmail.com>
2022-01-25 01:13:57 +0100

    ci: restructure in multiple tasks

--- release.config.js
+++ release.config.js
@@ -1,9 +1,10 @@
 var publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
 git push --force origin \${nextRelease.version} || exit 6
-git --git-dir build/website/.git add . || exit 1
-git --git-dir build/website/.git commit -m "chore: update website to version \${nextRelease.version}" || exit 2
-git --git-dir build/website/.git push || exit 3
+sed 's/version\\s*=\\s*".*"\\s*$/version = "\${nextRelease.version}"/' -i src/main/hugo/config.toml
+git -C build/website/ add . || exit 1
+git -C build/website/ commit -m "chore: update website to version \${nextRelease.version}" || exit 2
+git -C build/website/ push || exit 3
 ./gradlew shadowJar --parallel || ./gradlew shadowJar --parallel || exit 4
 ./gradlew releaseKotlinMavenOnMavenCentralNexus --parallel || exit 5
 ./gradlew publishKotlinMavenPublicationToGithubRepository --continue || true

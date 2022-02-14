ade58104e1b7ed8a37ec8fdc844664be75e707e7
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2022-02-14 11:59:34 +0100

    feat: implement PedestrianCognitiveCapability

---
+++ alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/implementations/capabilities/BasePedestrianCognitiveCapability.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2010-2022, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.model.implementations.capabilities
+
+import it.unibo.alchemist.model.cognitiveagents.CognitiveModel
+import it.unibo.alchemist.model.interfaces.capabilities.PedestrianCognitiveCapability
+
+class BasePedestrianCognitiveCapability(
+    override val cognitiveModel: CognitiveModel
+) : PedestrianCognitiveCapability

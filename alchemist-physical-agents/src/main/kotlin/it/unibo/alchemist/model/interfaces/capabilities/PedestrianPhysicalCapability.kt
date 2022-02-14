c7cf4fdf381ce409acefd60d533f77c3e66cb344
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2022-02-14 11:21:51 +0100

    feat: create pedestrian physical capability

---
+++ alchemist-physical-agents/src/main/kotlin/it/unibo/alchemist/model/interfaces/capabilities/PedestrianPhysicalCapability.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2010-2022, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.model.interfaces.capabilities
+
+import it.unibo.alchemist.model.implementations.positions.Euclidean2DPosition
+import it.unibo.alchemist.model.interfaces.Position
+import it.unibo.alchemist.model.interfaces.geometry.GeometricShape
+import it.unibo.alchemist.model.interfaces.geometry.GeometricShapeFactory
+import it.unibo.alchemist.model.interfaces.geometry.GeometricTransformation
+import it.unibo.alchemist.model.interfaces.geometry.Vector
+import it.unibo.alchemist.model.interfaces.nodes.NodeWithShape
+
+interface PedestrianPhysicalCapability<T, P, A, F>
+    : PhysicalCapability<T, P, A, F>
+    where P : Position<P>, P : Vector<P>,
+          A : GeometricTransformation<P>,
+          F : GeometricShapeFactory<P, A> {
+
+    /**
+     * The comfort ray of this pedestrian, this is added to the radius of its [shape] to obtain the [comfortArea].
+     */
+    val comfortRay: Double
+
+    /**
+     * The comfort area of this pedestrian, it's a circle of radius [shape].radius + [comfortRay].
+     */
+    val comfortArea: GeometricShape<P, A>
+
+    /**
+     * Computes the repulsion force caused by a node that entered the [comfortArea]. This is derived from the work
+     * of [Pelechano et al](https://bit.ly/3e3C7Tb).
+     */
+    fun repulsionForce(other: NodeWithShape<T, Euclidean2DPosition, *>): Euclidean2DPosition
+}

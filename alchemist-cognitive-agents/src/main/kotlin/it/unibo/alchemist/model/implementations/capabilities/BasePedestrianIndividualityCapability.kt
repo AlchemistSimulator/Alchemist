6dce39564d853c916bc247e804ea4005a04ef919
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2022-02-14 11:42:47 +0100

    feat: implement PedestrainIndividualityCapability

---
+++ alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/implementations/capabilities/BasePedestrianIndividualityCapability.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2010-2022, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.model.implementations.capabilities
+
+import it.unibo.alchemist.model.cognitiveagents.impact.individual.Age
+import it.unibo.alchemist.model.cognitiveagents.impact.individual.Compliance
+import it.unibo.alchemist.model.cognitiveagents.impact.individual.Gender
+import it.unibo.alchemist.model.cognitiveagents.impact.individual.HelpAttitude
+import it.unibo.alchemist.model.cognitiveagents.impact.individual.Speed
+import it.unibo.alchemist.model.interfaces.capabilities.PedestrianIndividualityCapability
+import it.unibo.alchemist.model.interfaces.geometry.GeometricTransformation
+import it.unibo.alchemist.model.interfaces.geometry.Vector
+
+class BasePedestrianIndividualityCapability<T, S : Vector<S>, A : GeometricTransformation<S>>(
+    override val age: Age,
+    override val gender: Gender,
+    override val speed: Speed,
+    override val compliance: Double = Compliance(age, gender).level,
+    override val helpAttitude: HelpAttitude = HelpAttitude(age, gender)
+) : PedestrianIndividualityCapability<T, S, A>

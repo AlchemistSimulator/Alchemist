7ab554a4d85ca335e32616e45700d02082d6e6ad
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2022-02-16 17:02:23 +0100

    refactor: split 2D version of orienting capability in its own file

--- alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/implementations/capabilities/BaseOrientingCapability.kt
+++ alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/implementations/capabilities/BaseOrientingCapability.kt
@@ -10,10 +10,8 @@
 package it.unibo.alchemist.model.implementations.capabilities
 
 import it.unibo.alchemist.model.implementations.actions.takePercentage
-import it.unibo.alchemist.model.implementations.geometry.euclidean2d.Ellipse
 import it.unibo.alchemist.model.implementations.geometry.euclidean2d.graph.UndirectedNavigationGraph
 import it.unibo.alchemist.model.implementations.geometry.euclidean2d.graph.pathExists
-import it.unibo.alchemist.model.implementations.positions.Euclidean2DPosition
 import it.unibo.alchemist.model.interfaces.Node
 import it.unibo.alchemist.model.interfaces.Position
 import it.unibo.alchemist.model.interfaces.capabilities.OrientingCapability
@@ -28,20 +26,9 @@ import org.jgrapht.alg.spanning.PrimMinimumSpanningTree
 import org.jgrapht.graph.AsWeightedGraph
 import org.jgrapht.graph.DefaultEdge
 import it.unibo.alchemist.model.interfaces.Node.Companion.asCapability
-import it.unibo.alchemist.model.interfaces.capabilities.Spatial2DCapability
 import it.unibo.alchemist.model.interfaces.capabilities.SpatialCapability
-import it.unibo.alchemist.model.interfaces.geometry.euclidean2d.ConvexPolygon
-import it.unibo.alchemist.model.interfaces.geometry.euclidean2d.Euclidean2DShapeFactory
-import it.unibo.alchemist.model.interfaces.geometry.euclidean2d.Euclidean2DTransformation
 import it.unibo.alchemist.model.interfaces.geometry.euclidean2d.graph.NavigationGraph
-import it.unibo.alchemist.nextDouble
 import org.apache.commons.math3.random.RandomGenerator
-import java.awt.geom.Ellipse2D
-import java.awt.geom.Rectangle2D
-
-private typealias Position2D = Euclidean2DPosition
-private typealias Transformation2D = Euclidean2DTransformation
-private typealias ShapeFactory = Euclidean2DShapeFactory
 
 /**
  * Base implementation of a node's [OrientingCapability].
@@ -124,38 +111,4 @@ abstract class BaseOrientingCapability<T, P, A, N, L, F> @JvmOverloads construct
      * returned landmark must contain at least one of them.
      */
     abstract override fun createLandmarkIn(area: N): L
-}
-
-/**
- * Basic implementation of a node's [OrientingCapability] in a 2D space.
- */
-class BaseOrienting2DCapability<T, N : ConvexPolygon, E>(
-    override val randomGenerator: RandomGenerator,
-    override val node: Node<T>,
-    override val knowledgeDegree: Double,
-    /**
-     * The starting width and height of the generated Ellipses will be a random quantity in
-     * ([minSide, maxSide] * the diameter of this pedestrian).
-     */
-    private val minSide: Double = 30.0,
-    private val maxSide: Double = 60.0,
-) : BaseOrientingCapability<T, Position2D, Transformation2D, N, Ellipse, ShapeFactory>(
-    randomGenerator,
-    node,
-    knowledgeDegree,
-) {
-    override fun createLandmarkIn(area: N): Ellipse = with(area) {
-        val width = randomEllipseSide()
-        val height = randomEllipseSide()
-        val frame = Rectangle2D.Double(centroid.x, centroid.y, width, height)
-        while (!contains(frame)) {
-            frame.width /= 2
-            frame.height /= 2
-        }
-        Ellipse(Ellipse2D.Double(frame.x, frame.y, frame.width, frame.height))
-    }
-
-    private fun randomEllipseSide(): Double =
-        randomGenerator.nextDouble(minSide, maxSide) *
-            node.asCapability<T, Spatial2DCapability<T, Position2D, Transformation2D>>().shape.diameter
 }

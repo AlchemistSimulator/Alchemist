67703a38f4aa0f226d224aceb1c9e5b4a18080fb
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2022-03-01 17:13:11 +0100

    refactor: use default Speed

--- alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/implementations/capabilities/BasePedestrianIndividuality2DCapability.kt
+++ alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/implementations/capabilities/BasePedestrianIndividuality2DCapability.kt
@@ -16,15 +16,17 @@ import it.unibo.alchemist.model.cognitiveagents.impact.individual.HelpAttitude
 import it.unibo.alchemist.model.cognitiveagents.impact.individual.Speed
 import it.unibo.alchemist.model.interfaces.Node
 import it.unibo.alchemist.model.interfaces.capabilities.PedestrianIndividuality2DCapability
+import org.apache.commons.math3.random.RandomGenerator
 
 /**
  * A pedestrian's individual characteristics.
  */
-data class BasePedestrianIndividuality2DCapability<T> @JvmOverloads constructor(
+class BasePedestrianIndividuality2DCapability<T> @JvmOverloads constructor(
+    randomGenerator: RandomGenerator,
     override val node: Node<T>,
     override val age: Age,
     override val gender: Gender,
-    override val speed: Speed,
+    override val speed: Speed = Speed(age, gender, randomGenerator),
     override val compliance: Double = Compliance(age, gender).level,
     override val helpAttitude: HelpAttitude = HelpAttitude(age, gender)
 ) : PedestrianIndividuality2DCapability<T>

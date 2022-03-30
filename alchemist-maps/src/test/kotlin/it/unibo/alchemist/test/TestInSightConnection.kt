98170e7c21c4d8d7e69a48b510d4a63d4585d973
Danilo Pianini <danilo.pianini@gmail.com>
2022-03-30 22:54:37 +0200

    test: work around broken shouldNotContain call in kotest

--- alchemist-maps/src/test/kotlin/it/unibo/alchemist/test/TestInSightConnection.kt
+++ alchemist-maps/src/test/kotlin/it/unibo/alchemist/test/TestInSightConnection.kt
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2010-2021, Danilo Pianini and contributors
- * listed in the main project's alchemist/build.gradle.kts file.
+ * Copyright (C) 2010-2022, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
  *
  * This file is part of Alchemist, and is distributed under the terms of the
  * GNU General Public License, with a linking exception,
@@ -10,7 +10,7 @@
 package it.unibo.alchemist.test
 
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.collections.shouldNotContain
+import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.doubles.shouldBeGreaterThan
 import io.kotest.matchers.doubles.shouldBeLessThan
 import io.kotest.matchers.shouldBe
@@ -31,7 +31,7 @@ class TestInSightConnection : StringSpec(
             environment.getDistanceBetweenNodes(node0, node1) shouldBeLessThan 100.0
             val route = environment.computeRoute(node0, node1)
             route.length() shouldBeGreaterThan 100.0
-            environment.getNeighborhood(node0) shouldNotContain node1
+            environment.getNeighborhood(node0).contains(node1).shouldBeFalse()
         }
     }
 )

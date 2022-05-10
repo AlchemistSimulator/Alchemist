b476edda09140a999292eab17f84efecb732f75c
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2022-05-10 14:42:44 +0200

    chore: move ImageEnvironmentWithGraph in physics module

--- alchemist-physics/src/main/kotlin/it/unibo/alchemist/model/implementations/environments/ImageEnvironmentWithGraph.kt
+++ alchemist-physics/src/main/kotlin/it/unibo/alchemist/model/implementations/environments/ImageEnvironmentWithGraph.kt
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2010-2020, Danilo Pianini and contributors
- * listed in the main project's alchemist/build.gradle.kts file.
+ * Copyright (C) 2010-2022, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
  *
  * This file is part of Alchemist, and is distributed under the terms of the
  * GNU General Public License, with a linking exception,

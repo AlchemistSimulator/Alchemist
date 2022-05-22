f230a50cc760d768243f68485dfec1a443f415d5
Danilo Pianini <danilo.pianini@gmail.com>
2022-05-22 17:54:22 +0200

    refactor!: rename module alchemist-interfaces into alchemist-api

--- alchemist-smartcam/build.gradle.kts
+++ alchemist-smartcam/build.gradle.kts
@@ -1,8 +1,17 @@
+/*
+ * Copyright (C) 2010-2022, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
 import Libs.alchemist
 import Libs.incarnation
 
 dependencies {
-    api(alchemist("interfaces"))
+    api(alchemist("api"))
 
     implementation(alchemist("euclidean-geometry"))
     implementation(alchemist("implementationbase"))

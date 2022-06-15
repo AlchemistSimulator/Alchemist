aea419e2dde5cd3acadf6bc4f570a7094b578fea
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2022-06-15 08:59:13 +0200

    docs(physics): add PhysicsUpdate documentation

--- alchemist-physics/src/main/kotlin/it/unibo/alchemist/model/implementations/reactions/PhysicsUpdate.kt
+++ alchemist-physics/src/main/kotlin/it/unibo/alchemist/model/implementations/reactions/PhysicsUpdate.kt
@@ -20,7 +20,13 @@ import it.unibo.alchemist.model.interfaces.environments.Dynamics2DEnvironment
 import org.danilopianini.util.ImmutableListSet
 import org.danilopianini.util.ListSet
 
+/**
+ * A global Reaction responsible for updating the physics of an [Dynamics2DEnvironment].
+ */
 class PhysicsUpdate<T>(
+    /**
+     * The environment to update.
+     */
     val environment: Dynamics2DEnvironment<T>,
     override val timeDistribution: TimeDistribution<T>,
 ) : GlobalReaction<T> {

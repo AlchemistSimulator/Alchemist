75c33d8cd3de51bbde738fa2bad002c7f5c10672
Danilo Pianini <danilo.pianini@unibo.it>
2022-07-29 11:34:56 +0200

    style(cognitive-agents): reduce duplication via type alias

--- alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/cognitiveagents/impact/individual/HelpAttitudeSpec.kt
+++ alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/cognitiveagents/impact/individual/HelpAttitudeSpec.kt
@@ -1,6 +1,17 @@
+/*
+ * Copyright (C) 2010-2022, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
 package it.unibo.alchemist.model.cognitiveagents.impact.individual
 
 import com.uchuhimo.konf.ConfigSpec
+
+private typealias DoublePair = Pair<Double, Double>
 
 /**
  * A specification of the parameters regarding help attitudes to load from a config file.
@@ -15,32 +26,32 @@ object HelpAttitudeSpec : ConfigSpec() {
         /**
          * The attitude of an adult male to help a male child.
          */
-        val childMale by required<Pair<Double, Double>>()
+        val childMale by required<DoublePair>()
 
         /**
          * The attitude of an adult male to help a male adult.
          */
-        val adultMale by required<Pair<Double, Double>>()
+        val adultMale by required<DoublePair>()
 
         /**
          * The attitude of an adult male to help a male elderly.
          */
-        val elderlyMale by required<Pair<Double, Double>>()
+        val elderlyMale by required<DoublePair>()
 
         /**
          * The attitude of an adult male to help a female child.
          */
-        val childFemale by required<Pair<Double, Double>>()
+        val childFemale by required<DoublePair>()
 
         /**
          * The attitude of an adult male to help an adult female.
          */
-        val adultFemale by required<Pair<Double, Double>>()
+        val adultFemale by required<DoublePair>()
 
         /**
          * The attitude of an adult male to help an elderly woman.
          */
-        val elderlyFemale by required<Pair<Double, Double>>()
+        val elderlyFemale by required<DoublePair>()
     }
 
     /**
@@ -51,32 +62,32 @@ object HelpAttitudeSpec : ConfigSpec() {
         /**
          * The attitude of an adult female to help a male child.
          */
-        val childMale by required<Pair<Double, Double>>()
+        val childMale by required<DoublePair>()
 
         /**
          * The attitude of an adult female to help a male adult.
          */
-        val adultMale by required<Pair<Double, Double>>()
+        val adultMale by required<DoublePair>()
 
         /**
          * The attitude of an adult female to help a male elderly.
          */
-        val elderlyMale by required<Pair<Double, Double>>()
+        val elderlyMale by required<DoublePair>()
 
         /**
          * The attitude of an adult female to help a female child.
          */
-        val childFemale by required<Pair<Double, Double>>()
+        val childFemale by required<DoublePair>()
 
         /**
          * The attitude of an adult female to help an adult female.
          */
-        val adultFemale by required<Pair<Double, Double>>()
+        val adultFemale by required<DoublePair>()
 
         /**
          * The attitude of an adult female to help an elderly woman.
          */
-        val elderlyFemale by required<Pair<Double, Double>>()
+        val elderlyFemale by required<DoublePair>()
     }
 
     /**
@@ -87,32 +98,32 @@ object HelpAttitudeSpec : ConfigSpec() {
         /**
          * The attitude of an elderly male to help a male child.
          */
-        val childMale by required<Pair<Double, Double>>()
+        val childMale by required<DoublePair>()
 
         /**
          * The attitude of an elderly male to help a male adult.
          */
-        val adultMale by required<Pair<Double, Double>>()
+        val adultMale by required<DoublePair>()
 
         /**
          * The attitude of an elderly male to help a male elderly.
          */
-        val elderlyMale by required<Pair<Double, Double>>()
+        val elderlyMale by required<DoublePair>()
 
         /**
          * The attitude of an elderly male to help a female child.
          */
-        val childFemale by required<Pair<Double, Double>>()
+        val childFemale by required<DoublePair>()
 
         /**
          * The attitude of an elderly male to help an adult female.
          */
-        val adultFemale by required<Pair<Double, Double>>()
+        val adultFemale by required<DoublePair>()
 
         /**
          * The attitude of an elderly male to help an elderly woman.
          */
-        val elderlyFemale by required<Pair<Double, Double>>()
+        val elderlyFemale by required<DoublePair>()
     }
 
     /**
@@ -123,31 +134,31 @@ object HelpAttitudeSpec : ConfigSpec() {
         /**
          * The attitude of an elderly female to help a male child.
          */
-        val childMale by required<Pair<Double, Double>>()
+        val childMale by required<DoublePair>()
 
         /**
          * The attitude of an elderly female to help a male adult.
          */
-        val adultMale by required<Pair<Double, Double>>()
+        val adultMale by required<DoublePair>()
 
         /**
          * The attitude of an elderly female to help a male elderly.
          */
-        val elderlyMale by required<Pair<Double, Double>>()
+        val elderlyMale by required<DoublePair>()
 
         /**
          * The attitude of an elderly female to help a female child.
          */
-        val childFemale by required<Pair<Double, Double>>()
+        val childFemale by required<DoublePair>()
 
         /**
          * The attitude of an elderly female to help an adult female.
          */
-        val adultFemale by required<Pair<Double, Double>>()
+        val adultFemale by required<DoublePair>()
 
         /**
          * The attitude of an elderly female to help an elderly woman.
          */
-        val elderlyFemale by required<Pair<Double, Double>>()
+        val elderlyFemale by required<DoublePair>()
     }
 }

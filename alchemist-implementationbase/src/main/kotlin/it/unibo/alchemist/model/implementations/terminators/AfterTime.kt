8a77fa21c71eeb743decd86aa94ea558970c215c
Danilo Pianini <danilo.pianini@unibo.it>
2022-07-29 18:02:53 +0200

    style: use check() instead of throwing an IllegalStateException in AfterTime

--- alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/implementations/terminators/AfterTime.kt
+++ alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/implementations/terminators/AfterTime.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2010-2022, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
 package it.unibo.alchemist.model.implementations.terminators
 
 import it.unibo.alchemist.model.interfaces.Environment
@@ -15,6 +24,7 @@ class AfterTime(val endTime: Time) : Predicate<Environment<*, *>> {
      * Otherwise, reads the current time, and flips to true once it got past the provided [endTime].
      */
     override fun test(environment: Environment<*, *>): Boolean =
-        environment.simulation?.time?.let { it >= endTime }
-            ?: throw IllegalStateException("No simulation available for environment $environment, unable to read time.")
+        checkNotNull(environment.simulation?.time?.let { it >= endTime }) {
+            "No simulation available for environment $environment, unable to read time."
+        }
 }

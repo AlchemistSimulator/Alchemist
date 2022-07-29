950b5701540d8e45146a7fd0710e9340ac004176
Danilo Pianini <danilo.pianini@unibo.it>
2022-07-29 16:16:18 +0200

    build: remove deprecated feature preview

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,5 +1,13 @@
+/*
+ * Copyright (C) 2010-2022, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
 // Using the same version catalog for buildSrc as for the other projects.
-enableFeaturePreview("VERSION_CATALOGS")
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {

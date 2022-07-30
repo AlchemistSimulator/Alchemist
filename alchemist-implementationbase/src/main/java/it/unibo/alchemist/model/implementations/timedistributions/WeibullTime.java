44412293ff7350db116ffdcdb4ea1a6abbf5c7c7
Danilo Pianini <danilo.pianini@gmail.com>
2022-07-30 17:45:28 +0200

    style(implementationbase): improve indentation in WeibullTime

--- alchemist-implementationbase/src/main/java/it/unibo/alchemist/model/implementations/timedistributions/WeibullTime.java
+++ alchemist-implementationbase/src/main/java/it/unibo/alchemist/model/implementations/timedistributions/WeibullTime.java
@@ -132,9 +132,9 @@ public class WeibullTime<T> extends AbstractDistribution<T> {
      * @return a new {@link WeibullDistribution}
      */
     protected static WeibullDistribution weibullFromMean(
-            final double mean,
-            final double deviation,
-            final RandomGenerator random
+        final double mean,
+        final double deviation,
+        final RandomGenerator random
     ) {
         final double t = FastMath.log((deviation * deviation) / (mean * mean) + 1);
         double kmin = 0, kmax = 1;
@@ -154,10 +154,10 @@ public class WeibullTime<T> extends AbstractDistribution<T> {
         final double shapeParameter = 1 / k;
         final double scaleParameter = mean / FastMath.exp(Gamma.logGamma(1 + k));
         return new WeibullDistribution(
-                random,
-                shapeParameter,
-                scaleParameter,
-                WeibullDistribution.DEFAULT_INVERSE_ABSOLUTE_ACCURACY
+            random,
+            shapeParameter,
+            scaleParameter,
+            WeibullDistribution.DEFAULT_INVERSE_ABSOLUTE_ACCURACY
         );
     }
 

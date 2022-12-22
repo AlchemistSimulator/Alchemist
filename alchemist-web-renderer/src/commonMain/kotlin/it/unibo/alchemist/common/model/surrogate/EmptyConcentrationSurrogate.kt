12f823e2409a253edade5c18f581d9fa19da4042
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-12-22 12:13:10 +0100

    feat(web-renderer): add EmptyConcentrationSurrogate

---
+++ alchemist-web-renderer/src/commonMain/kotlin/it/unibo/alchemist/common/model/surrogate/EmptyConcentrationSurrogate.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2010-2022, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.common.model.surrogate
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * An object that represents an Empty Concentration.
+ */
+@SerialName("EmptyConcentration")
+@Serializable
+object EmptyConcentrationSurrogate

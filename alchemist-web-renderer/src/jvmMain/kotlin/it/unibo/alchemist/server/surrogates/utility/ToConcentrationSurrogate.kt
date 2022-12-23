19a29d71f19edf8e7d84f99d9707a517f1f6ff58
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-12-23 11:01:25 +0100

    feat(web-renderer): add toConcentrationSurrogate mapping functions

---
+++ alchemist-web-renderer/src/jvmMain/kotlin/it/unibo/alchemist/server/surrogates/utility/ToConcentrationSurrogate.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2010-2022, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.server.surrogates.utility
+
+import it.unibo.alchemist.common.model.surrogate.EmptyConcentrationSurrogate
+
+/**
+ * A set of functions to map the concentrations to the corresponding surrogate classes.
+ */
+object ToConcentrationSurrogate {
+
+    /**
+     * @return A function that maps any object to an [EmptyConcentrationSurrogate].
+     */
+    val toEmptyConcentration: (Any) -> EmptyConcentrationSurrogate = {
+        EmptyConcentrationSurrogate
+    }
+}

501580e5cbd4fb351d77338b93e87acd5117f7b3
Danilo Pianini <danilo.pianini@gmail.com>
2023-01-01 20:45:41 +0100

    test: adapt the MongoDB testing utility to embed-mongo 4

--- alchemist-loading/src/test/kotlin/it/unibo/alchemist/test/MongoTestUtility.kt
+++ alchemist-loading/src/test/kotlin/it/unibo/alchemist/test/MongoTestUtility.kt
@@ -9,52 +9,43 @@
 
 package it.unibo.alchemist.test
 
-import de.flapdoodle.embed.mongo.MongodExecutable
-import de.flapdoodle.embed.mongo.MongodProcess
-import de.flapdoodle.embed.mongo.MongodStarter
-import de.flapdoodle.embed.mongo.config.ImmutableMongodConfig
-import de.flapdoodle.embed.mongo.config.MongodConfig
 import de.flapdoodle.embed.mongo.config.Net
 import de.flapdoodle.embed.mongo.distribution.Version
-import de.flapdoodle.embed.process.distribution.Distribution
+import de.flapdoodle.embed.mongo.transitions.Mongod
 import de.flapdoodle.embed.process.runtime.Network
 import de.flapdoodle.os.ImmutablePlatform
 import de.flapdoodle.os.OS
+import de.flapdoodle.os.Platform
 import de.flapdoodle.os.linux.LinuxDistribution
 import de.flapdoodle.os.linux.UbuntuVersion
-import io.kotest.matchers.shouldNotBe
+import de.flapdoodle.reverse.StateID
+import de.flapdoodle.reverse.transitions.Start
 import org.slf4j.LoggerFactory
 
-private val starter: MongodStarter = MongodStarter.getDefaultInstance()
 private val logger by lazy { LoggerFactory.getLogger("TestsWithMongoDB") }
+
+private inline fun <reified T : Any> T.toTransition() = Start.to(StateID.of(T::class.java)).initializedWith(this)
 
 internal fun withMongo(operation: () -> Unit) {
     if (System.getProperty("os.name").lowercase().contains("win")) {
         logger.warn("Testing with MongoDB is disabled on Windows due to flaky behavior")
     } else {
-        val mongodConfig: ImmutableMongodConfig = MongodConfig.builder()
-            .version(Version.Main.V4_4)
-            .net(Net("localhost", 27017, Network.localhostIsIPv6()))
-            .build()
-        val detectedDistribution = Distribution.detectFor(mongodConfig.version())
-        val actualDistribution = detectedDistribution
-            .takeUnless { with(it.platform()) { operatingSystem() == OS.Linux && distribution().isEmpty } }
-            ?: Distribution.of(
-                mongodConfig.version(),
-                ImmutablePlatform.builder()
-                    .from(detectedDistribution.platform())
-                    .distribution(LinuxDistribution.Ubuntu)
-                    .version(UbuntuVersion.Ubuntu_20_10)
-                    .build()
-            )
-        val mongodExecutable: MongodExecutable = starter.prepare(mongodConfig, actualDistribution)
-        mongodExecutable shouldNotBe null
-        val mongodProcess: MongodProcess = mongodExecutable.start()
-        mongodProcess shouldNotBe null
+        val detectedPlatform = Platform.detect()
+        val platform: Platform = detectedPlatform
+            .takeUnless { it.operatingSystem() == OS.Linux && it.distribution().isEmpty }
+            ?: ImmutablePlatform.builder()
+                .from(detectedPlatform)
+                .distribution(LinuxDistribution.Ubuntu)
+                .version(UbuntuVersion.Ubuntu_20_10)
+                .build()
+        val mongod = Mongod.instance()
+            .withNet(Net.of("localhost", 27017, Network.localhostIsIPv6()).toTransition())
+            .withPlatform(platform.toTransition())
+        val runningState = mongod.start(Version.Main.V6_0)
         try {
             operation()
         } finally {
-            mongodExecutable.stop()
+            runningState.current().stop()
         }
     }
 }

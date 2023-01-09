fb2960887bf46a6f91f05631d999d007e8ccb173
Danilo Pianini <danilo.pianini@unibo.it>
2023-01-09 15:58:09 +0100

    fix(euclidean-geometry): make `Ellipse.euclideanShape` a `val`

--- alchemist-euclidean-geometry/src/main/kotlin/it/unibo/alchemist/model/implementations/geometry/euclidean2d/Ellipse.kt
+++ alchemist-euclidean-geometry/src/main/kotlin/it/unibo/alchemist/model/implementations/geometry/euclidean2d/Ellipse.kt
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2010-2020, Danilo Pianini and contributors
- * listed in the main project's alchemist/build.gradle.kts file.
+ * Copyright (C) 2010-2023, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
  *
  * This file is part of Alchemist, and is distributed under the terms of the
  * GNU General Public License, with a linking exception,
@@ -9,8 +9,8 @@
 
 package it.unibo.alchemist.model.implementations.geometry.euclidean2d
 
-import it.unibo.alchemist.model.implementations.positions.Euclidean2DPosition
 import it.unibo.alchemist.model.implementations.geometry.AwtShapeCompatible
+import it.unibo.alchemist.model.implementations.positions.Euclidean2DPosition
 import it.unibo.alchemist.model.interfaces.geometry.euclidean2d.Euclidean2DConvexShape
 import it.unibo.alchemist.model.interfaces.geometry.euclidean2d.Euclidean2DShape
 import it.unibo.alchemist.model.interfaces.geometry.euclidean2d.Euclidean2DTransformation
@@ -23,7 +23,7 @@ class Ellipse(
     private val ellipse: Ellipse2D
 ) : Euclidean2DConvexShape, AwtShapeCompatible {
 
-    private var euclideanShape = AwtEuclidean2DShape(ellipse)
+    private val euclideanShape = AwtEuclidean2DShape(ellipse)
 
     override val centroid = euclideanShape.centroid
 

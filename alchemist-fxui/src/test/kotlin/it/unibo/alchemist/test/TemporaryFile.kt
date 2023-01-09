c1f0a5f570240c89851055b3f840e1894bd633bc
Danilo Pianini <danilo.pianini@unibo.it>
2023-01-09 17:45:27 +0100

    style(fxui): annotate platform type in `TemporaryFile`

--- alchemist-fxui/src/test/kotlin/it/unibo/alchemist/test/TemporaryFile.kt
+++ alchemist-fxui/src/test/kotlin/it/unibo/alchemist/test/TemporaryFile.kt
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2010-2019, Danilo Pianini and contributors
- * listed in the main project's alchemist/build.gradle.kts file.
+ * Copyright (C) 2010-2023, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
  *
  * This file is part of Alchemist, and is distributed under the terms of the
  * GNU General Public License, with a linking exception,
@@ -20,7 +20,7 @@ object TemporaryFile {
      * Creates a temporary file named after the method that calls the function, for testing purposes only.
      */
     @JvmStatic
-    fun create() = File.createTempFile(
+    fun create(): File = File.createTempFile(
         StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE).walk { stack ->
             stack.skip(1)
                 .map(StackWalker.StackFrame::getMethodName)

18c76347ed46d83555db8fee2448b449b7b81b02
Angelo Filaseta <filaseta.angelo@gmail.com>
2023-01-09 17:15:30 +0100

    test(web-renderer): add common state test

---
+++ alchemist-web-renderer/src/commonTest/kotlin/it/unibo/alchemist/common/state/CommonStateTest.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2010-2022, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.common.state
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldNotBe
+
+class CommonStateTest : StringSpec({
+    "CommonState should be initialized with a default renderer" {
+        val state = CommonState()
+        state.renderer shouldNotBe null
+    }
+})

1fae1937b3c3a963df0a4d02b5ee521f1ee9d54d
Danilo Pianini <danilo.pianini@unibo.it>
2023-01-09 16:22:57 +0100

    style(loading): use `error()` instead of throwing an `IllegalStateException` in `JVMConstructor`

--- alchemist-loading/src/main/kotlin/it/unibo/alchemist/loader/m2m/JVMConstructor.kt
+++ alchemist-loading/src/main/kotlin/it/unibo/alchemist/loader/m2m/JVMConstructor.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2022, Danilo Pianini and contributors
+ * Copyright (C) 2010-2023, Danilo Pianini and contributors
  * listed, for each module, in the respective subproject's build.gradle.kts file.
  *
  * This file is part of Alchemist, and is distributed under the terms of the
@@ -241,7 +241,7 @@ sealed class JVMConstructor(val typeName: String) {
                             emptyList()
                         }
                         compatibleSubtypes.size > 1 -> {
-                            throw IllegalStateException(
+                            error(
                                 "Ambiguous mapping: $compatibleSubtypes all match the requested type $typeName for " +
                                     "parameter #$mappedIndex:$potentialType of $constructor"
                             )

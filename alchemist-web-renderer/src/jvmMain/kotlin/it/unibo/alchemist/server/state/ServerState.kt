47601c63c744b62975330ead69c603cfe3a66f55
Angelo Filaseta <filaseta.angelo@gmail.com>
2023-01-13 15:23:41 +0100

    docs(web-renderer): improve ServerState and ServerStore documentation

--- alchemist-web-renderer/src/jvmMain/kotlin/it/unibo/alchemist/server/state/ServerState.kt
+++ alchemist-web-renderer/src/jvmMain/kotlin/it/unibo/alchemist/server/state/ServerState.kt
@@ -18,8 +18,14 @@ import it.unibo.alchemist.server.state.reducers.simulationReducer
 
 /**
  * The state of the server.
+ * The [ServerState] is managed using the
+ * <a href="https://reduxkotlin.org/introduction/core-concepts">Core concepts of the ReduxKotlin library</a>.
+ * Like in the original Redux library the state is stored in a single class that contains other objects via composition.
+ * The state can be changed using actions that must be defined in advance.
+ * The unique store that encapsulate the [ServerState] is {@link it.unibo.alchemist.server.state.ServerStore}.
  * @param simulation the simulation. Defaults to null.
  * @param environmentSurrogate the current environment surrogate. Defaults to an uninitializedEnvironment.
+ * @see <a href="https://reduxkotlin.org/">ReduxKotlin Documentation</a>
  */
 data class ServerState(
     val simulation: Simulation<Any, Nothing>? = null,

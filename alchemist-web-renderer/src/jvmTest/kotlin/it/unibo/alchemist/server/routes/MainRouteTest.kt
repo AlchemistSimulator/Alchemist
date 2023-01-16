56b276505c94ac74dad463a23d2a22fb7b3684fd
Angelo Filaseta <filaseta.angelo@gmail.com>
2023-01-16 17:56:14 +0100

    test(web-renderer): add main route test

---
+++ alchemist-web-renderer/src/jvmTest/kotlin/it/unibo/alchemist/server/routes/MainRouteTest.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2010-2022, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.server.routes
+
+import io.kotest.matchers.shouldBe
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.ApplicationTestBuilder
+
+suspend fun ApplicationTestBuilder.mainRouteTest() {
+    val response = client.get("/")
+    response.status shouldBe HttpStatusCode.OK
+    response.bodyAsText() shouldBe this::class.java.classLoader.getResource("index.html")?.readText()
+}

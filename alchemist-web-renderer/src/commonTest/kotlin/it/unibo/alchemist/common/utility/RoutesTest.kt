dcafc1206c14ff5c095700981698660a8e4e525f
Angelo Filaseta <filaseta.angelo@gmail.com>
2023-01-16 17:44:58 +0100

    test(web-renderer): add Routes test

---
+++ alchemist-web-renderer/src/commonTest/kotlin/it/unibo/alchemist/common/utility/RoutesTest.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2010-2023, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.common.utility
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import it.unibo.alchemist.common.utility.Ruotes.environmentClientPath
+import it.unibo.alchemist.common.utility.Ruotes.environmentServerPath
+import it.unibo.alchemist.common.utility.Ruotes.simulationPausePath
+import it.unibo.alchemist.common.utility.Ruotes.simulationPlayPath
+import it.unibo.alchemist.common.utility.Ruotes.simulationStatusPath
+
+class RoutesTest : StringSpec({
+    "All the routes strings should be correct" {
+        environmentClientPath shouldBe "/environment/client"
+        environmentServerPath shouldBe "/environment/server"
+        simulationStatusPath shouldBe "/simulation/status"
+        simulationPlayPath shouldBe "/simulation/play"
+        simulationPausePath shouldBe "/simulation/pause"
+    }
+})

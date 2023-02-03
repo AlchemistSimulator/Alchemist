a3a51901a36f58648d236504dfc59901dc49f610
Angelo Filaseta <filaseta.angelo@gmail.com>
2023-02-03 10:46:38 +0100

    feat(web-render): switch to the Java Desktop API to open the default browser

--- alchemist-web-renderer/src/jvmMain/kotlin/it/unibo/alchemist/server/modules/UtilityModule.kt
+++ alchemist-web-renderer/src/jvmMain/kotlin/it/unibo/alchemist/server/modules/UtilityModule.kt
@@ -9,17 +9,17 @@
 
 package it.unibo.alchemist.server.modules
 
-import io.ktor.server.application.Application
-import java.net.InetAddress
+import io.ktor.server.engine.ApplicationEngineEnvironment
+import java.awt.Desktop
+import java.net.URI
 
 /**
  * Start the default browser of the user on the server address.
  */
-fun Application.startBrowserModule() {
-    Runtime.getRuntime().exec("xdg-open ${InetAddress.getLocalHost().hostAddress}:$port")
+fun ApplicationEngineEnvironment.startBrowserModule() {
+    if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+        connectors.forEach {
+            Desktop.getDesktop().browse(URI("${it.type.name.lowercase()}://${it.host}:${it.port}"))
+        }
+    }
 }
-
-/**
- * Retrieve the port used in the Ktor application.conf file.
- */
-val Application.port get() = environment.config.property("ktor.deployment.port").getString().toInt()

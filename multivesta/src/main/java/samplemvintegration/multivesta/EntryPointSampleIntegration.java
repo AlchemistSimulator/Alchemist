5d40cf69bbd4644e751308c9a2f31bcf8837cfee
gianmarco <gianmarco.magnani3@studio.unibo.it>
2023-02-07 00:59:47 +0100

    feat(multivesta): integrate MultiVesta and Alchemist introducing the module alchemist-adapter

---
+++ multivesta/src/main/java/samplemvintegration/multivesta/EntryPointSampleIntegration.java
@@ -0,0 +1,10 @@
+package samplemvintegration.multivesta;
+
+import entrypointmultivesta.UniqueEntryPoint;
+
+public class EntryPointSampleIntegration {
+
+	public static void main(String[] args) {
+		UniqueEntryPoint.main(args);
+	}
+}

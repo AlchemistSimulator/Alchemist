7c55987fca4c93655efc650a0a061516e19043d5
Angelo Filaseta <filaseta.angelo@gmail.com>
2023-02-13 10:22:15 +0100

    feat(web-renderer): add Client State (#1962)
    * feat(web-render): add Action class
    
    * test(web-render): add Action Test class
    
    * feat(web-render): add SetPlayButton redux action
    
    * feat(web-render): add SetRenderMode redux action
    
    * feat(web-render): add SetStatusSurrogate redux action
    
    * feat(web-render): add SetBitmap redux action
    
    * feat(web-render): add PlayButton redux reducer
    
    * feat(web-render): add RenderMode redux reducer
    
    * feat(web-render): add Bitmap redux reducer
    
    * feat(web-render): add StatusSurrogate redux reducer
    
    * feat(web-render): add ClientState
    
    * feat(web-render): add ClientStore
    
    * test(web-render): add ClientStore test

---
+++ alchemist-web-renderer/src/jsMain/kotlin/it/unibo/alchemist/client/state/actions/SetBitmap.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2010-2022, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.client.state.actions
+
+import com.soywiz.korim.bitmap.Bitmap
+
+/**
+ * Redux action to set the bitmap to display.
+ * @param bitmap the new bitmap to set.
+ */
+data class SetBitmap(val bitmap: Bitmap?)

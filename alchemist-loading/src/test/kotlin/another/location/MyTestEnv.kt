ae6c80490d0218dfa81ada28785c02d68a3064ec
Danilo Pianini <danilo.pianini@unibo.it>
2023-02-20 11:58:24 +0100

    test(loading): add regression test loading a class from a custom package

---
+++ alchemist-loading/src/test/kotlin/another/location/MyTestEnv.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2010-2023, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package another.location
+
+import it.unibo.alchemist.model.implementations.environments.Continuous2DEnvironment
+import it.unibo.alchemist.model.implementations.positions.Euclidean2DPosition
+import it.unibo.alchemist.model.interfaces.Incarnation
+
+class MyTestEnv<T> (incarnation: Incarnation<T, Euclidean2DPosition>) : Continuous2DEnvironment<T>(incarnation)

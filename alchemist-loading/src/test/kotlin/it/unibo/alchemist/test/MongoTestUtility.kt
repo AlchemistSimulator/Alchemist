1c0aa6e7bbbb73c016d76a59a0b2b8a7a274e193
Danilo Pianini <danilo.pianini@unibo.it>
2023-02-28 09:14:06 +0100

    test(loading): update the usage of `Platform.detect()` in `MongoTestUtility`

--- alchemist-loading/src/test/kotlin/it/unibo/alchemist/test/MongoTestUtility.kt
+++ alchemist-loading/src/test/kotlin/it/unibo/alchemist/test/MongoTestUtility.kt
@@ -12,15 +12,15 @@ package it.unibo.alchemist.test
 import de.flapdoodle.embed.mongo.config.Net
 import de.flapdoodle.embed.mongo.distribution.Version
 import de.flapdoodle.embed.mongo.transitions.Mongod
-import de.flapdoodle.net.Net as Network
+import de.flapdoodle.os.CommonOS
 import de.flapdoodle.os.ImmutablePlatform
-import de.flapdoodle.os.OS
 import de.flapdoodle.os.Platform
 import de.flapdoodle.os.linux.LinuxDistribution
 import de.flapdoodle.os.linux.UbuntuVersion
 import de.flapdoodle.reverse.StateID
 import de.flapdoodle.reverse.transitions.Start
 import org.slf4j.LoggerFactory
+import de.flapdoodle.net.Net as Network
 
 private val logger by lazy { LoggerFactory.getLogger("TestsWithMongoDB") }
 
@@ -30,22 +30,25 @@ internal fun withMongo(operation: () -> Unit) {
     if (System.getProperty("os.name").lowercase().contains("win")) {
         logger.warn("Testing with MongoDB is disabled on Windows due to flaky behavior")
     } else {
-        val detectedPlatform = Platform.detect()
-        val platform: Platform = detectedPlatform
-            .takeUnless { it.operatingSystem() == OS.Linux && it.distribution().isEmpty }
-            ?: ImmutablePlatform.builder()
-                .from(detectedPlatform)
-                .distribution(LinuxDistribution.Ubuntu)
-                .version(UbuntuVersion.Ubuntu_20_10)
-                .build()
-        val mongod = Mongod.instance()
-            .withNet(Net.of("localhost", 27017, Network.localhostIsIPv6()).toTransition())
-            .withPlatform(platform.toTransition())
-        val runningState = mongod.start(Version.Main.V6_0)
-        try {
-            operation()
-        } finally {
-            runningState.current().stop()
-        }
+        runCatching { Platform.detect(CommonOS.list()) }
+            .onFailure { logger.warn("Failed to detect platform", it) }
+            .onSuccess { detectedPlatform ->
+                val platform: Platform = detectedPlatform
+                    .takeUnless { it.operatingSystem() == CommonOS.Linux && it.distribution().isEmpty }
+                    ?: ImmutablePlatform.builder()
+                        .from(detectedPlatform)
+                        .distribution(LinuxDistribution.Ubuntu)
+                        .version(UbuntuVersion.Ubuntu_20_10)
+                        .build()
+                val mongod = Mongod.instance()
+                    .withNet(Net.of("localhost", 27017, Network.localhostIsIPv6()).toTransition())
+                    .withPlatform(platform.toTransition())
+                val runningState = mongod.start(Version.Main.V6_0)
+                try {
+                    operation()
+                } finally {
+                    runningState.current().stop()
+                }
+            }
     }
 }

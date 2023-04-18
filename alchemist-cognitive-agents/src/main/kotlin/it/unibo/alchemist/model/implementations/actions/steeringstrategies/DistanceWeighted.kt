ce256721b2136acbc6a3ec38ea3b6398bce72f8f
Danilo Pianini <danilo.pianini@gmail.com>
2023-04-19 00:10:01 +0200

    refactor(cognitive-agents): move  `targetDistanceTo` into `SteeringActionWithTarget`

--- alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/implementations/actions/steeringstrategies/DistanceWeighted.kt
+++ alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/implementations/actions/steeringstrategies/DistanceWeighted.kt
@@ -1,11 +1,8 @@
 package it.unibo.alchemist.model.implementations.actions.steeringstrategies
 
-import it.unibo.alchemist.model.interfaces.Environment
 import it.unibo.alchemist.model.interfaces.Node
-import it.unibo.alchemist.model.interfaces.Position
 import it.unibo.alchemist.model.interfaces.SteeringActionWithTarget
 import it.unibo.alchemist.model.interfaces.environments.Euclidean2DEnvironment
-import it.unibo.alchemist.model.interfaces.geometry.Vector
 
 /**
  * [Weighted] strategy where the weight of each steering action is the inverse of the node's distance from the
@@ -29,17 +26,9 @@ class DistanceWeighted<T>(
     node,
     {
         if (this is SteeringActionWithTarget) {
-            this.targetDistanceTo(node, environment).let { if (it > 0.0) 1 / it else it }
+            targetDistanceTo(node, environment).let { if (it > 0.0) 1 / it else it }
         } else {
             defaultWeight
         }
     }
 )
-
-/**
- * Computes the distance between this action's target and the given [node].
- */
-fun <T, P> SteeringActionWithTarget<T, P>.targetDistanceTo(
-    node: Node<T>,
-    environment: Environment<T, P>
-): Double where P : Position<P>, P : Vector<P> = target().distanceTo(environment.getPosition(node))

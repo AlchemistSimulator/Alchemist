49341472fafe08f77a852cf26bb35be3dc91e8fc
Danilo Pianini <danilo.pianini@gmail.com>
2023-04-19 00:57:59 +0200

    style(cognitive-agents): fix style issues in `OrientingAgent`

--- alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/interfaces/OrientingAgent.kt
+++ alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/interfaces/OrientingAgent.kt
@@ -27,9 +27,9 @@ import org.jgrapht.graph.DefaultEdge
  * @param R the type of edges of the [cognitiveMap], representing the [R]elations between landmarks.
  */
 interface OrientingAgent<V, A, L, R> where
-V : Vector<V>,
-A : GeometricTransformation<V>,
-L : ConvexGeometricShape<V, A> {
+      V : Vector<V>,
+      A : GeometricTransformation<V>,
+      L : ConvexGeometricShape<V, A> {
 
     /**
      * The knowledge degree of the agent concerning the environment. This is a Double value in [0, 1] describing the

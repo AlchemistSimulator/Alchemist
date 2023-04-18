bfa6e62eceb708d2c188962a692e68f08aae13ef
Danilo Pianini <danilo.pianini@gmail.com>
2023-04-19 00:44:15 +0200

    style(cognitive-agents): fix style issues in `Family`

--- alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/implementations/groups/Family.kt
+++ alchemist-cognitive-agents/src/main/kotlin/it/unibo/alchemist/model/implementations/groups/Family.kt
@@ -16,7 +16,7 @@ import it.unibo.alchemist.model.interfaces.Node
  * A [Family] is modeled as a group of pedestrians with a leader.
  */
 class Family<T>(
-    comparator: Comparator<Node<T>> = Comparator { a, b -> a.id.compareTo(b.id) }
+    comparator: Comparator<Node<T>> = Comparator { a, b -> a.id.compareTo(b.id) },
 ) : GenericGroup<T, Node<T>>(),
     GroupWithLeader<T, Node<T>> {
 

ef8ef010f51caba8c4ae98d15a6f4c364f350df3
Danilo Pianini <danilo.pianini@gmail.com>
2023-04-18 21:07:05 +0200

    ci: actualize `yarn.lock` automatically before building in CI

--- release.config.js
+++ release.config.js
@@ -10,6 +10,7 @@
 var publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
 git push --force origin \${nextRelease.version} || exit 6
+./gradlew kotlinUpgradeYarnLock || exit 8
 ./gradlew injectVersionInWebsite || exit 7
 git -C build/website/ add . || exit 1
 git -C build/website/ commit -m "chore: update website to version \${nextRelease.version}" || exit 2

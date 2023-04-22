5425554c30a81178a23b5a4a8f4eeaa8f1dbdb08
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2023-04-22 11:03:47 +0200

    style(alchemist-engine): move the static modifier to the right place according to codacy

--- alchemist-ui-tooling/src/main/java/it/unibo/alchemist/boundary/ui/impl/AbstractWormhole2D.java
+++ alchemist-ui-tooling/src/main/java/it/unibo/alchemist/boundary/ui/impl/AbstractWormhole2D.java
@@ -36,7 +36,6 @@ import static it.unibo.alchemist.boundary.ui.impl.PointAdapter.from;
  * @param <P> the position type
  */
 public abstract class AbstractWormhole2D<P extends Position2D<? extends P>> implements Wormhole2D<P> {
-    private final static double ZOOM_FACTOR = 0.10;
     private final Environment<?, P> environment;
     private final ViewPort view;
     private PointAdapter<P> position;
@@ -46,6 +45,8 @@ public abstract class AbstractWormhole2D<P extends Position2D<? extends P>> impl
     private double rotation;
     private Mode mode = Mode.ISOMETRIC;
     private PointAdapter<P> effectCenter = from(0, 0);
+    private final static double ZOOM_FACTOR = 0.10;
+
 
     /**
      * Wormhole constructor for any {@link ViewPort}.

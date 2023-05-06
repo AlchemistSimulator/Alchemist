d51a459b7915e2efab1cade36ce02113ffeffdf4
Danilo Pianini <danilo.pianini@gmail.com>
2023-05-06 09:45:27 +0200

    refactor!: more refactoring (squash this commit)

--- alchemist-loading/src/main/java/it/unibo/alchemist/model/deployments/Circle.java
+++ alchemist-loading/src/main/java/it/unibo/alchemist/model/deployments/Circle.java
@@ -1,15 +1,19 @@
 /*
- * Copyright (C) 2010-2019, Danilo Pianini and contributors listed in the main project's alchemist/build.gradle file.
+ * Copyright (C) 2010-2023, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
  *
  * This file is part of Alchemist, and is distributed under the terms of the
  * GNU General Public License, with a linking exception,
  * as described in the file LICENSE in the Alchemist distribution's top directory.
  */
-package it.unibo.alchemist.loader.deployments;
+package it.unibo.alchemist.model.deployments;
 
+import it.unibo.alchemist.loader.deployments.AbstractRandomDeployment;
 import it.unibo.alchemist.model.Environment;
 import it.unibo.alchemist.model.Position;
 import org.apache.commons.math3.random.RandomGenerator;
+
+import javax.annotation.Nonnull;
 
 import static org.apache.commons.math3.util.FastMath.PI;
 import static org.apache.commons.math3.util.FastMath.cos;
@@ -48,6 +52,7 @@ public final class Circle<P extends Position<? extends P>> extends AbstractRando
     }
 
     @Override
+    @Nonnull
     protected P indexToPosition(final int i) {
         final double angle = randomDouble(0, 2 * PI);
         final double rad = radius * sqrt(randomDouble());
--- alchemist-loading/src/main/java/it/unibo/alchemist/model/deployments/Rectangle.java
+++ alchemist-loading/src/main/java/it/unibo/alchemist/model/deployments/Rectangle.java
@@ -1,12 +1,14 @@
 /*
- * Copyright (C) 2010-2019, Danilo Pianini and contributors listed in the main project's alchemist/build.gradle file.
+ * Copyright (C) 2010-2023, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
  *
  * This file is part of Alchemist, and is distributed under the terms of the
  * GNU General Public License, with a linking exception,
  * as described in the file LICENSE in the Alchemist distribution's top directory.
  */
-package it.unibo.alchemist.loader.deployments;
+package it.unibo.alchemist.model.deployments;
 
+import it.unibo.alchemist.loader.deployments.AbstractRandomDeployment;
 import org.apache.commons.math3.random.RandomGenerator;
 
 import it.unibo.alchemist.model.Environment;

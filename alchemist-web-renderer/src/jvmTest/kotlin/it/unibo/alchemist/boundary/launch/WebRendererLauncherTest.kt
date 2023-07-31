da17ace6329633cc9cb720e730c6f214655948dd
Kyrillos Ntronov <ntronov.kyrillos@gmail.com>
2023-07-31 09:09:26 +0200

    fix(web-renderer): fix test

--- alchemist-web-renderer/src/jvmTest/kotlin/it/unibo/alchemist/boundary/launch/WebRendererLauncherTest.kt
+++ alchemist-web-renderer/src/jvmTest/kotlin/it/unibo/alchemist/boundary/launch/WebRendererLauncherTest.kt
@@ -10,12 +10,21 @@
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import it.unibo.alchemist.AlchemistExecutionOptions
+import it.unibo.alchemist.boundary.LoadAlchemist
 import it.unibo.alchemist.boundary.launch.Validation
 import it.unibo.alchemist.boundary.launch.WebRendererLauncher
+import org.kaikikm.threadresloader.ResourceLoader
+import java.io.File
 
 class WebRendererLauncherTest : StringSpec(
     {
 
+        val simulationFile = "simulation.yml"
+        val placeholderLoader = LoadAlchemist.from(
+            ResourceLoader.getResource(simulationFile)
+                ?: File(simulationFile).takeIf { it.exists() && it.isFile }?.toURI()?.toURL()
+                ?: error("No classpath resource or file $simulationFile was found"),
+        )
         val expectedLauncherName = "Web Renderer Launcher"
 
         fun checkOptionsAreInvalid(options: AlchemistExecutionOptions, reason: String) {
@@ -32,49 +41,49 @@ class WebRendererLauncherTest : StringSpec(
 
         "Web Renderer Launcher is not compatible with distributed execution" {
             checkOptionsAreInvalid(
-                AlchemistExecutionOptions(configuration = "placeholder", distributed = "something"),
+                AlchemistExecutionOptions(loader = placeholderLoader, distributed = "something"),
                 "$expectedLauncherName is not compatible with distributed execution",
             )
         }
 
         "Web Renderer Launcher is not compatible with graphics mode" {
             checkOptionsAreInvalid(
-                AlchemistExecutionOptions(configuration = "placeholder", graphics = "something"),
+                AlchemistExecutionOptions(loader = placeholderLoader, graphics = "something"),
                 "$expectedLauncherName is not compatible with graphics mode",
             )
             checkOptionsAreInvalid(
-                AlchemistExecutionOptions(configuration = "placeholder", fxui = true),
+                AlchemistExecutionOptions(loader = placeholderLoader, fxui = true),
                 "$expectedLauncherName is not compatible with graphics mode",
             )
             checkOptionsAreInvalid(
-                AlchemistExecutionOptions(configuration = "placeholder", graphics = "something", fxui = true),
+                AlchemistExecutionOptions(loader = placeholderLoader, graphics = "something", fxui = true),
                 "$expectedLauncherName is not compatible with graphics mode",
             )
         }
 
         "Web Renderer Launcher is not compatible with grid execution" {
             checkOptionsAreInvalid(
-                AlchemistExecutionOptions(configuration = "placeholder", server = "something"),
+                AlchemistExecutionOptions(loader = placeholderLoader, server = "something"),
                 "$expectedLauncherName is not compatible with Alchemist grid computing server mode",
             )
         }
 
         "Web Renderer Launcher is not compatible with headless mode" {
             checkOptionsAreInvalid(
-                AlchemistExecutionOptions(configuration = "placeholder", headless = true),
+                AlchemistExecutionOptions(loader = placeholderLoader, headless = true),
                 "$expectedLauncherName is not compatible with headless mode",
             )
         }
 
         "Web Renderer Launcheris not compatible with variable exploration mode" {
             checkOptionsAreInvalid(
-                AlchemistExecutionOptions(configuration = "placeholder", variables = listOf("something")),
+                AlchemistExecutionOptions(loader = placeholderLoader, variables = listOf("something")),
                 "$expectedLauncherName is not compatible with variable exploration mode",
             )
         }
 
-        "Web Renderer Launcher can contain only configuration" {
-            checkOptionsAreValid(AlchemistExecutionOptions(configuration = "placeholder"))
+        "Web Renderer Launcher can contain only loader" {
+            checkOptionsAreValid(AlchemistExecutionOptions(loader = placeholderLoader))
         }
     },
 )

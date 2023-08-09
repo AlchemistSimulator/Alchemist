a6e40f49e0c268dbd423265ad5e9af7c235e6834
Danilo Pianini <danilo.pianini@unibo.it>
2023-08-10 00:34:54 +0200

    style: indent multiline Kotlin typealiases

--- alchemist-loading/src/main/kotlin/it/unibo/alchemist/boundary/loader/SimulationModel.kt
+++ alchemist-loading/src/main/kotlin/it/unibo/alchemist/boundary/loader/SimulationModel.kt
@@ -67,7 +67,7 @@ import it.unibo.alchemist.boundary.loader.syntax.DocumentRoot.Layer as LayerSynt
  */
 private typealias Seeds = Pair<RandomGenerator, RandomGenerator>
 private typealias ReactionComponentFunction<T, P, R> =
-(RandomGenerator, Environment<T, P>, Node<T>?, TimeDistribution<T>, Actionable<T>, String?) -> R
+    (RandomGenerator, Environment<T, P>, Node<T>?, TimeDistribution<T>, Actionable<T>, String?) -> R
 
 /*
  * UTILITY FUNCTIONS

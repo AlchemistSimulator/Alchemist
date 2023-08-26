ed67561c9bb0fe5eedfd9ad1f6781b289f49aced
Kyrillos Ntronov <ntronov.kyrillos@gmail.com>
2023-08-26 12:09:16 +0200

    refactor(engine): detek refactor

--- alchemist-engine/src/main/java/it/unibo/alchemist/core/model/BatchEngineConfiguration.kt
+++ alchemist-engine/src/main/java/it/unibo/alchemist/core/model/BatchEngineConfiguration.kt
@@ -11,12 +11,21 @@ package it.unibo.alchemist.core.model
 
 import it.unibo.alchemist.model.BatchEngineConfiguration
 
+/**
+ * Contains the fixed size batch engine configuration parameters.
+ * @property batchSize size of batch to be processed in parallel
+ */
 class FixedBatchEngineConfiguration(
     override val outputReplayStrategy: String,
     override val workersNumber: Int,
     val batchSize: Int,
 ) : BatchEngineConfiguration
 
+/**
+ * Contains the epsilon dynamic batch engine configuration parameters.
+ * @property epsilonValue epsilon sensitivity value used to build a batch
+ * to be processed in parallel
+ */
 class EpsilonBatchEngineConfiguration(
     override val outputReplayStrategy: String,
     override val workersNumber: Int,

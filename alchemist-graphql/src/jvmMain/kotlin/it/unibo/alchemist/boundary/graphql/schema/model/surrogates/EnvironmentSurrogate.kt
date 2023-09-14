73f77726537016c065923a7aa45801fe7d05e15b
S-furi <stefano.furi@studio.unibo.it>
2023-09-14 16:58:09 +0200

    feat(alchemist-graphql): add basic `Environment` GraphQL surrogate class

---
+++ alchemist-graphql/src/jvmMain/kotlin/it/unibo/alchemist/boundary/graphql/schema/model/surrogates/EnvironmentSurrogate.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2010-2023, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.boundary.graphql.schema.model.surrogates
+
+import com.expediagroup.graphql.generator.annotations.GraphQLDescription
+import com.expediagroup.graphql.generator.annotations.GraphQLIgnore
+import it.unibo.alchemist.model.Environment
+import it.unibo.alchemist.model.Position
+
+/**
+ * A surrogate for [Environment].
+ * @param T the concentration type
+ * @param P the position
+ * @param dimensions the number of dimensions of this environment.
+ */
+@GraphQLDescription("The simulation environment")
+data class EnvironmentSurrogate<T, P : Position<P>>(
+    @GraphQLIgnore override val origin: Environment<T, P>,
+    val dimensions: Int = origin.dimensions,
+) : GraphQLSurrogate<Environment<T, P>>(origin) {
+    /**
+     * The nodes inside this environment.
+     * @return the nodes in this environment.
+     */
+    fun nodes() = origin.nodes.map { NodeSurrogate(it) }
+
+    /**
+     * Returns the node with the given id.
+     * @param id the id of the node
+     */
+    fun nodeById(id: Int): NodeSurrogate<T> = origin.getNodeByID(id).toGraphQLNodeSurrogate()
+}
+
+/**
+ * Converts an [Environment] to a [EnvironmentSurrogate].
+ * @param T the concentration type
+ * @param P the position
+ * @return a [EnvironmentSurrogate] representing the given [Environment]
+ */
+fun <T, P : Position<P>> Environment<T, P>.toGraphQLEnvironmentSurrogate() = EnvironmentSurrogate(this)
---
+++ alchemist-graphql/src/jvmTest/kotlin/it/unibo/alchemist/boundary/graphql/schema/model/EnvironmentSurrogateTest.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2010-2023, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.boundary.graphql.schema.model
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import it.unibo.alchemist.boundary.TestingEnvironments.graphqlTestEnvironmnets
+import it.unibo.alchemist.boundary.graphql.schema.model.surrogates.toGraphQLEnvironmentSurrogate
+import it.unibo.alchemist.boundary.graphql.schema.model.surrogates.toGraphQLNodeSurrogate
+import it.unibo.alchemist.model.Position
+import it.unibo.alchemist.model.Time
+import it.unibo.alchemist.model.geometry.Vector
+
+class EnvironmentSurrogateTest<T, P> : StringSpec({
+    "EnvironmentSurrogate should map an Environment to a GraphQL compliant object" {
+        graphqlTestEnvironmnets<T, P>().forEach {
+            val envSurrogate = it.toGraphQLEnvironmentSurrogate()
+
+            it.dimensions shouldBe envSurrogate.dimensions
+
+            it.nodes.size shouldBe envSurrogate.nodes().size
+            it.nodes.forEach { node ->
+                val nodeSurrogate = envSurrogate.nodeById(node.id)
+                checkNodeSurrogate<T, P>(node, nodeSurrogate)
+            }
+
+            // Testing propagation of changes
+            val newNode = it.nodes.first().cloneNode(Time.ZERO)
+            val newPosition = it.makePosition(0.0, 0.0)
+            it.addNode(newNode, newPosition)
+            it.getNodeByID(newNode.id).toGraphQLNodeSurrogate() shouldBe envSurrogate.nodeById(newNode.id)
+        }
+    }
+}) where T : Any, P : Position<P>, P : Vector<P>

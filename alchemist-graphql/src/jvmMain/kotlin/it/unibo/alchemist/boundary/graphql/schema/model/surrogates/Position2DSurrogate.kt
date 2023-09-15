3b7a947ef5f177565f8681e224f60e0d8e54f5c2
S-furi <stefano.furi@studio.unibo.it>
2023-09-15 10:30:53 +0200

    feat(alchemist-graphql): add `Position2D` GraphQL surrogate class

---
+++ alchemist-graphql/src/jvmMain/kotlin/it/unibo/alchemist/boundary/graphql/schema/model/surrogates/Position2DSurrogate.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2010-2023, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.boundary.graphql.schema.model.surrogates
+
+import it.unibo.alchemist.model.Position
+
+/**
+ * A 2D surrogate for a [Position] object.
+ *
+ * @param x the x coordinate of the position.
+ * @param y the y coordinate of the position.
+ */
+data class Position2DSurrogate(
+    val x: Double,
+    val y: Double,
+    override val coordinates: List<Double> = listOf(x, y),
+    override val dimensions: Int = 2,
+) : PositionSurrogate

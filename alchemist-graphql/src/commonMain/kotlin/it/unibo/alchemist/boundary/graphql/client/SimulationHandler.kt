f18fdb65bf6b306358ace64234e24b653f0944e6
S-furi <stefano.furi@studio.unibo.it>
2023-11-03 13:55:32 +0100

    chore(graphql): add class for handling simulation queries

---
+++ alchemist-graphql/src/commonMain/kotlin/it/unibo/alchemist/boundary/graphql/client/SimulationHandler.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2010-2023, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.boundary.graphql.client
+
+import com.apollographql.apollo3.api.Mutation
+
+/**
+ * Utility class to handle simulation lifecycle through GraphQL queries.
+ *
+ * @param graphqlClient the GraphQL client connected to the server
+ */
+class SimulationHandler(private val graphqlClient: GraphQLClient) {
+
+    /**
+     * Pauses the simulation.
+     */
+    suspend fun pause() =
+        handleSimulation(
+            PauseSimulationMutation(),
+            "PAUSING SIMULATION",
+        ) as PauseSimulationMutation.Data
+
+    /**
+     * Terminates the simulation.
+     */
+    suspend fun terminate() =
+        handleSimulation(
+            TerminateSimulationMutation(),
+            "TERMINATING SIMULATION",
+        ) as TerminateSimulationMutation.Data
+
+    /**
+     * Plays the simulation.
+     */
+    suspend fun play(): PlaySimulationMutation.Data {
+        val status = (
+            handleSimulation(
+                PlaySimulationMutation(),
+                "STARTING SIMULATION",
+            ) as PlaySimulationMutation.Data
+            )
+        require(status.play == "RUNNING") { status.play }
+        return status
+    }
+
+    /**
+     * Returns the status of the simulation.
+     */
+    suspend fun status() = graphqlClient.query(SimulationStatusQuery()).execute().data?.simulationStatus
+
+    private suspend fun handleSimulation(action: Mutation<*>, message: String) =
+        graphqlClient.mutation(action).execute().also { println(message) }.data!!
+}

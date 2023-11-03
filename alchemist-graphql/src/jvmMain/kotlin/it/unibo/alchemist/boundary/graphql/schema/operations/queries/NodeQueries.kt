2fa28bf19972ff9eade41e9431653c5b595548cf
S-furi <stefano.furi@studio.unibo.it>
2023-11-03 13:51:47 +0100

    chore(graphql): extracted Node specific queries

---
+++ alchemist-graphql/src/jvmMain/kotlin/it/unibo/alchemist/boundary/graphql/schema/operations/queries/NodeQueries.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2010-2023, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.boundary.graphql.schema.operations.queries
+
+import com.expediagroup.graphql.generator.annotations.GraphQLDescription
+import com.expediagroup.graphql.server.operations.Query
+import it.unibo.alchemist.boundary.graphql.schema.model.surrogates.EnvironmentSurrogate
+import it.unibo.alchemist.boundary.graphql.schema.model.surrogates.NeighborhoodSurrogate
+import it.unibo.alchemist.boundary.graphql.schema.model.surrogates.PositionSurrogate
+import it.unibo.alchemist.boundary.graphql.schema.model.surrogates.toGraphQLEnvironmentSurrogate
+import it.unibo.alchemist.model.Environment
+import it.unibo.alchemist.model.Position
+
+/**
+ * Set of GraphQL queries to compute on nodes.
+ */
+class NodeQueries<T, P : Position<out P>>(private val environment: Environment<T, P>) : Query {
+
+    private val environmentSurrogate: EnvironmentSurrogate<T, P> = environment.toGraphQLEnvironmentSurrogate()
+
+    /**
+     * Returns the node position in sapce.
+     *
+     * @param nodeId the node id
+     * @return the position in space of the given node
+     */
+    @GraphQLDescription("The position in space of the given node.")
+    fun nodePosition(nodeId: Int): PositionSurrogate? =
+        environmentSurrogate.nodeToPos()[nodeId]
+
+    /**
+     * Returns the neighborhood with the provided node as a center.
+     *
+     * @param nodeId the neighborhood center's node id.
+     * @return the neighborhood with this node as a center
+     */
+    @GraphQLDescription("The neighborhood which the given node is the center.")
+    fun neighborhood(nodeId: Int): NeighborhoodSurrogate<T> =
+        environmentSurrogate.getNeighborhood(nodeId)
+}
--- alchemist-graphql/src/jvmMain/kotlin/it/unibo/alchemist/boundary/graphql/server/modules/GraphQLModule.kt
+++ alchemist-graphql/src/jvmMain/kotlin/it/unibo/alchemist/boundary/graphql/server/modules/GraphQLModule.kt
@@ -23,6 +23,7 @@ import io.ktor.server.websocket.pingPeriod
 import io.ktor.server.websocket.timeout
 import it.unibo.alchemist.boundary.graphql.schema.operations.mutations.SimulationControl
 import it.unibo.alchemist.boundary.graphql.schema.operations.queries.EnvironmentQueries
+import it.unibo.alchemist.boundary.graphql.schema.operations.queries.NodeQueries
 import it.unibo.alchemist.boundary.graphql.schema.operations.subscriptions.EnvironmentSubscriptions
 import it.unibo.alchemist.boundary.graphql.server.attributes.SimulationAttributeKey
 import java.time.Duration
@@ -60,6 +61,7 @@ fun Application.graphQLModule() {
             )
             queries = listOf(
                 EnvironmentQueries(environment),
+                NodeQueries(environment),
             )
             mutations = listOf(
                 SimulationControl(environment),

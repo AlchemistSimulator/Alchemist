9beaece5cf8ced00be82d4a3bf983a570eceab7b
Marco Sternini <mkarko01@gmail.com>
2024-01-18 16:49:36 +0100

    fix(cd): windows and macOs packages release

--- release.config.js
+++ release.config.js
@@ -16,7 +16,6 @@ git -C build/website/ add . || exit 1
 git -C build/website/ commit -m "chore: update website to version \${nextRelease.version}" || exit 2
 git -C build/website/ push || exit 3
 ./gradlew shadowJar --parallel || ./gradlew shadowJar --parallel || exit 4
-./gradlew jpackageFull || exit 9
 RELEASE_ON_CENTRAL="./gradlew uploadKotlinOSSRH uploadKotlinMultiplatform uploadJvm uploadJs release --parallel"
 eval "$RELEASE_ON_CENTRAL" || eval "$RELEASE_ON_CENTRAL" || eval "$RELEASE_ON_CENTRAL" || exit 5
 ./gradlew publishKotlinOSSRHPublicationToGithubRepository --continue || true

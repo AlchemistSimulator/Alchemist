2e79144d986d71c6ac144545bf098f88dea1f37b
Danilo Pianini <danilo.pianini@unibo.it>
2024-07-08 09:49:19 +0200

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -7,7 +7,7 @@
  * as described in the file LICENSE in the Alchemist distribution's top directory.
  */
 
-var publishCmd = `
+const publishCmd = `
 ./gradlew kotlinUpgradeYarnLock || exit 2
 ./gradlew performWebsiteStringReplacements || exit 3
 git -C build/website/ add . || exit 4
@@ -16,14 +16,14 @@ git -C build/website/ push || exit 6
 RELEASE_ON_CENTRAL="./gradlew -PstagingRepositoryId=\${process.env.STAGING_REPO_ID} releaseStagingRepositoryOnMavenCentral"
 eval "$RELEASE_ON_CENTRAL" || eval "$RELEASE_ON_CENTRAL" || eval "$RELEASE_ON_CENTRAL" || exit 7
 ./publishToAUR.sh pkgbuild/PKGBUILD "$CUSTOM_SECRET_0" "$CUSTOM_SECRET_1" "$CUSTOM_SECRET_2" || exit 8
-`
+`;
 
 // This is then used by the workflow to signal success to release steps that need other operating systems to be executed
-var successCmd = `
+const successCmd = `
 touch RELEASED
-`
+`;
 
-var config = require('semantic-release-preconfigured-conventional-commits');
+import config from 'semantic-release-preconfigured-conventional-commits';
 config.plugins.push(
     ["@semantic-release/exec", {
         "publishCmd": publishCmd,
@@ -36,5 +36,5 @@ config.plugins.push(
          ]
     }],
     "@semantic-release/git",
-)
-module.exports = config
+);
+export default config;

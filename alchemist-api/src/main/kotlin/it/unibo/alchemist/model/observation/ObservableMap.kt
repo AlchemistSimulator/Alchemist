ae93aa1a57b329454216265126aaccf276b09eb8
Danilo Pianini <danilo.pianini@unibo.it>
2024-09-06 12:21:55 +0200

    feat: retry to implement the observables

--- alchemist-api/src/main/kotlin/it/unibo/alchemist/model/observation/ObservableMap.kt
+++ alchemist-api/src/main/kotlin/it/unibo/alchemist/model/observation/ObservableMap.kt
@@ -9,62 +9,57 @@
 
 package it.unibo.alchemist.model.observation
 
+import arrow.core.Option
+import arrow.core.none
+import arrow.core.some
 import java.util.WeakHashMap
 
+/**
+ * Represents an observable map with keys of type [K] and values of type [V].
+ */
 interface ObservableMap<K, out V> {
 
-    fun subscribe(subscriber: Any, onChange: (Map<K, V>) -> Unit)
+    /**
+     * Registers the [subscriber] to be notified for any map change.
+     */
+    fun onChange(subscriber: Any, callback: (Map<K, V>, Option<K>, Option<V>) -> Unit)
 
-    fun subscribe(subscriber: Any, key: K, onChange: (V) -> Unit)
+    /**
+     * Returns an [Observable] of all changes to values associated to the provided [key].
+     */
+    operator fun get(key: K): Observable<Option<V>>
 }
 
-class ObservableMutableMap<K, V> {
+/**
+ * An observable map that allows to modify its content.
+ */
+class ObservableMutableMap<K, V> : ObservableMap<K, V> {
 
     private val backingMap: MutableMap<K, V> = linkedMapOf()
-    private val globalObservers: WeakHashMap<Any, List<(Map<K, V>) -> Unit>> = WeakHashMap()
-    private val observers: WeakHashMap<Any, Map<K, List<(V) -> Unit>>> = WeakHashMap()
+    private val backingMapOfObservable: MutableMap<K, MutableObservable<Option<V>>> = linkedMapOf()
+    private val observers: WeakHashMap<Any, List<(Map<K, V>, Option<K>, Option<V>) -> Unit>> = WeakHashMap()
 
+    /**
+     * Puts the given [value] in the map at the given [key],
+     * notifying all subscribers to the map and all subscribers to the key.
+     */
     fun put(key: K, value: V) {
         val previous = backingMap.put(key, value)
         if (previous != value) {
-            globalObservers.values.forEach {
-                    callbacks ->
-                callbacks.forEach { it(backingMap) }
-            }
+            getAsMutable(key).set(value.some())
             observers.values.forEach { callbacks ->
-                callbacks[key].orEmpty().forEach { it(value) }
+                callbacks.forEach { it(backingMap, key.some(), value.some()) }
             }
         }
     }
 
-    fun onChange(subscriber: Any, callback: (Map<K, V>) -> Unit) {
-        globalObservers.compute(subscriber) { _, callbacks -> (callbacks ?: emptyList()) + callback }
-        callback(backingMap)
+    override fun onChange(subscriber: Any, callback: (Map<K, V>, Option<K>, Option<V>) -> Unit) {
+        observers.compute(subscriber) { _, callbacks -> callbacks.orEmpty() + callback }
+        callback(backingMap, none(), none())
     }
 
-    operator fun get(key: K): Observable<V> {
-        return object : Observable<V> {
-            override fun onChange(registrant: Any, callback: (V) -> Unit) {
-                observers.compute(registrant) { _, callbacks ->
-                    val newCallbacks = callbacks.orEmpty() +
-                        newCallbacks[key] = (newCallbacks[key] ?: emptyList()) + callback
-                    newCallbacks
-                }
-                callback(backingMap[key])
-            }
+    override operator fun get(key: K): Observable<Option<V>> = getAsMutable(key)
 
-            override fun <R> map(transform: (V) -> R): Observable<R> {
-                TODO("Not yet implemented")
-            }
-        }
-    }
-
-    fun onChange(subscriber: Any, key: K, onChange: (V) -> Unit) {
-        observers.compute(subscriber) { _, callbacks ->
-            val newCallbacks = callbacks.orEmpty() +
-                newCallbacks[key] = (newCallbacks[key] ?: emptyList()) + onChange
-            newCallbacks
-        }
-        onChange(backingMap[key])
-    }
+    private fun getAsMutable(key: K): MutableObservable<Option<V>> =
+        backingMapOfObservable.getOrPut(key) { MutableObservable.observableOf(none()) }
 }

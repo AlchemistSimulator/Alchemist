1e723d1ffcd7e8f80c01799076549898f4a5f800
Danilo Pianini <danilo.pianini@unibo.it>
2024-11-11 18:33:45 +0100

    test(loading): fix broken assertion in `TestConstructorOverloading`

--- alchemist-loading/src/test/kotlin/it/unibo/alchemist/test/TestConstructorOverloading.kt
+++ alchemist-loading/src/test/kotlin/it/unibo/alchemist/test/TestConstructorOverloading.kt
@@ -1,17 +1,15 @@
 package it.unibo.alchemist.test
 
-import another.location.MyTestEnv
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.nulls.beNull
-import io.kotest.matchers.shouldBe
+import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldNot
-import io.kotest.matchers.types.instanceOf
 import it.unibo.alchemist.test.AlchemistTesting.loadAlchemistFromResource
 
 class TestConstructorOverloading : StringSpec({
     "constructor overloading should not be ambiguous during loading" {
         val loader = loadAlchemistFromResource("regression/2024-depots-ambiguous-constructors.yml")
         loader shouldNot beNull()
-        loader.getDefault<Nothing, Nothing>().environment shouldBe instanceOf(MyTestEnv::class)
+        loader.getDefault<Nothing, Nothing>().environment.shouldNotBeNull()
     }
 })

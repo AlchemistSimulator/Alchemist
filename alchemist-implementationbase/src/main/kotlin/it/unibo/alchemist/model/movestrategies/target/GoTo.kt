86a1027c376cfcc8227193c5b9319c82ad6b61d0
Martina Baiardi <m.baiardi@unibo.it>
2024-11-11 15:03:52 +0100

    feat: add GoTo TargetSelectionStrategy implementation

---
+++ alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/movestrategies/target/GoTo.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2010-2024, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.model.movestrategies.target
+
+import it.unibo.alchemist.model.Environment
+import it.unibo.alchemist.model.Position
+import it.unibo.alchemist.model.movestrategies.TargetSelectionStrategy
+
+/**
+ * TargetSelectionStrategy that has the objective to go towards a fixed destination in the Environment.
+ *
+ * @param <T> Concentration type
+ * @param <P> position type
+ */
+class GoTo<T, P : Position<P>>(
+    private val destination: P,
+) : TargetSelectionStrategy<T, P> {
+
+    /**
+     * @param environment: the environment executing the simulation,
+     * @param destination: an indefinite number of [Number] values that indicates the coordinates of the destination.
+     * @returns a [TargetSelectionStrategy] which aim to go towards the fixed destination in the environment.
+     */
+    constructor(
+        environment: Environment<T, P>,
+        vararg destination: Number,
+    ) : this(environment.makePosition(*destination))
+
+    override fun getTarget(): P = destination
+
+    override fun toString() = "${GoTo::class.simpleName}:$destination"
+}

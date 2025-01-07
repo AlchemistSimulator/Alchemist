cc276bee23df0669e401970d1ed218c55dd2e625
Danilo Pianini <danilo.pianini@unibo.it>
2025-01-07 11:20:32 +0100

    fix(website): make the release process lighter

--- release.config.mjs
+++ release.config.mjs
@@ -8,13 +8,10 @@
  */
 
 const publishCmd = `
-./gradlew kotlinUpgradeYarnLock || exit 2
-./gradlew performWebsiteStringReplacements || exit 3
 git -C build/website/ add . || exit 4
 git -C build/website/ commit -m "chore: update website to version \${nextRelease.version}" || exit 5
 git -C build/website/ push || exit 6
-RELEASE_ON_CENTRAL="./gradlew -PstagingRepositoryId=\${process.env.STAGING_REPO_ID} releaseStagingRepositoryOnMavenCentral"
-eval "$RELEASE_ON_CENTRAL" || eval "$RELEASE_ON_CENTRAL" || eval "$RELEASE_ON_CENTRAL" || exit 7
+./gradlew kotlinUpgradeYarnLock -PstagingRepositoryId=\${process.env.STAGING_REPO_ID} releaseStagingRepositoryOnMavenCentral || exit 7
 ./publishToAUR.sh pkgbuild/PKGBUILD "$CUSTOM_SECRET_0" "$CUSTOM_SECRET_1" "$CUSTOM_SECRET_2" || exit 8
 `;
 
@@ -23,14 +20,15 @@ const successCmd = `
 touch RELEASED
 `;
 
-import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
+import config from 'semantic-release-preconfigured-conventional-commits' with {type: 'json'};
+
 config.plugins.push(
     ["@semantic-release/exec", {
         "publishCmd": publishCmd,
         "successCmd": successCmd
     }],
     ["@semantic-release/github", {
-        "assets": [ 
+        "assets": [
             { "path": "build/shadow/*-all.jar" },
             { "path": "build/package/*"}
          ]

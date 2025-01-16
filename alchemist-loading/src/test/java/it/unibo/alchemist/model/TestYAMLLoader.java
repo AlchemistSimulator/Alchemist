8b51bd6adf62fb3c6690096b3ab933f5e9efdca9
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-01-16 12:51:15 +0100

    chore: remove unused import

--- alchemist-loading/src/test/java/it/unibo/alchemist/model/TestYAMLLoader.java
+++ alchemist-loading/src/test/java/it/unibo/alchemist/model/TestYAMLLoader.java
@@ -30,7 +30,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 /**

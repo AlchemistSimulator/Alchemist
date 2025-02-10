29e8dd520d3b28c54f1257465307ce8211530eab
Danilo Pianini <danilo.pianini@unibo.it>
2025-02-10 15:45:29 +0100

    test: migrate tests requiring a pre-test setup from kotest to kotlin.test

--- alchemist-incarnation-biochemistry/src/test/kotlin/it/unibo/alchemist/model/biochemistry/molecules/TestMoleculeSwapWithinNeighborhood.kt
+++ alchemist-incarnation-biochemistry/src/test/kotlin/it/unibo/alchemist/model/biochemistry/molecules/TestMoleculeSwapWithinNeighborhood.kt
@@ -8,13 +8,6 @@
  */
 package it.unibo.alchemist.model.biochemistry.molecules
 
-import io.kotest.core.spec.style.StringSpec
-import io.kotest.core.test.TestCase
-import io.kotest.matchers.Matcher
-import io.kotest.matchers.MatcherResult
-import io.kotest.matchers.collections.shouldContain
-import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldHave
 import it.unibo.alchemist.model.Environment
 import it.unibo.alchemist.model.Node
 import it.unibo.alchemist.model.Reaction
@@ -26,7 +19,11 @@ import it.unibo.alchemist.model.linkingrules.ConnectWithinDistance
 import it.unibo.alchemist.model.positions.Euclidean2DPosition
 import it.unibo.alchemist.model.timedistributions.ExponentialTime
 import org.apache.commons.math3.random.MersenneTwister
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import kotlin.properties.Delegates
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 private const val DIRECT_REACTION = "[token] --> [token in neighbor]"
 private const val INVERSE_REACTION = "[token in neighbor] --> [token]"
@@ -34,34 +31,15 @@ private val INCARNATION = BiochemistryIncarnation()
 private val BIOMOLECULE = INCARNATION.createMolecule("token")
 private val RANDOM = MersenneTwister()
 private val TIME = ExponentialTime<Double>(1.0, RANDOM)
-private val LINKING_RULE =
-    ConnectWithinDistance<Double, Euclidean2DPosition>(5.0)
+private val LINKING_RULE = ConnectWithinDistance<Double, Euclidean2DPosition>(5.0)
 private val INITIAL_POSITIONS = Pair(Euclidean2DPosition(0.0, 0.0), Euclidean2DPosition(1.0, 0.0))
 private var environment: Environment<Double, Euclidean2DPosition> by Delegates.notNull()
 private var nodes: Pair<Node<Double>, Node<Double>> by Delegates.notNull()
 
-class TestMoleculeSwapWithinNeighborhood :
-    StringSpec({
-        "send molecule to a neighbor" {
-            val reaction = INCARNATION.createReaction(RANDOM, environment, nodes.first, TIME, DIRECT_REACTION)
-            reaction shouldHave 2.conditions
-            reaction shouldHave 1.neighborConditions
-            reaction shouldHave 2.actions
-            nodes.first.addReaction(reaction)
-            testSimulation()
-        }
-        "pick molecule from a neighbor" {
-            val reaction = INCARNATION.createReaction(RANDOM, environment, nodes.second, TIME, INVERSE_REACTION)
-            reaction shouldHave 1.conditions
-            reaction shouldHave 1.neighborConditions
-            reaction shouldHave 2.actions
-            nodes.second.addReaction(reaction)
-            testSimulation()
-        }
-    }) {
-    override suspend fun beforeTest(testCase: TestCase) {
-        environment =
-            BioRect2DEnvironment(INCARNATION)
+class TestMoleculeSwapWithinNeighborhood {
+    @BeforeEach
+    fun setUp() {
+        environment = BioRect2DEnvironment(INCARNATION)
         nodes =
             Pair(
                 INCARNATION.createNode(RANDOM, environment, null),
@@ -70,55 +48,47 @@ class TestMoleculeSwapWithinNeighborhood :
         environment.linkingRule = LINKING_RULE
         environment.addNode(nodes.first, INITIAL_POSITIONS.first)
         environment.addNode(nodes.second, INITIAL_POSITIONS.second)
-        environment.getNeighborhood(nodes.first).neighbors shouldContain nodes.second
-        environment.getNeighborhood(nodes.second).neighbors shouldContain nodes.first
+        assertTrue(environment.getNeighborhood(nodes.first).neighbors.contains(nodes.second))
+        assertTrue(environment.getNeighborhood(nodes.second).neighbors.contains(nodes.first))
         nodes.first.setConcentration(BIOMOLECULE, 1.0)
+    }
+
+    @Test
+    fun `it should be possible to send molecules to a neighbor`() {
+        val reaction = INCARNATION.createReaction(RANDOM, environment, nodes.first, TIME, DIRECT_REACTION)
+        assertEquals(2, reaction.conditions.size)
+        assertEquals(1, reaction.neighborConditions.size)
+        assertEquals(2, reaction.actions.size)
+        nodes.first.addReaction(reaction)
+        testSimulation()
+    }
+
+    @Test
+    fun `it should be possible to pick molecules from a neighbor`() {
+        val reaction = INCARNATION.createReaction(RANDOM, environment, nodes.second, TIME, INVERSE_REACTION)
+        assertEquals(1, reaction.conditions.size)
+        assertEquals(1, reaction.neighborConditions.size)
+        assertEquals(2, reaction.actions.size)
+        nodes.second.addReaction(reaction)
+        testSimulation()
     }
 }
 
 private fun testSimulation() =
     environment.startSimulationWithoutParameters(
         initialized = {
-            nodes.first.getConcentration(BIOMOLECULE) shouldBe 1.0
-            nodes.second.getConcentration(BIOMOLECULE) shouldBe 0.0
-            nodes
-                .toList()
-                .stream()
-                .mapToInt { it.reactions.count() }
-                .sum() shouldBe 1
+            assertEquals(1.0, nodes.first.getConcentration(BIOMOLECULE))
+            assertEquals(0.0, nodes.second.getConcentration(BIOMOLECULE))
+            assertEquals(1, nodes.toList().sumOf { it.reactions.count() })
         },
         stepDone = {
-            nodes
-                .toList()
-                .stream()
-                .mapToDouble { it.getConcentration(BIOMOLECULE) }
-                .sum() shouldBe 1.0
+            assertEquals(1.0, nodes.toList().sumOf { it.getConcentration(BIOMOLECULE) })
         },
         finished = {
-            nodes.first.getConcentration(BIOMOLECULE) shouldBe 0.0
-            nodes.second.getConcentration(BIOMOLECULE) shouldBe 1.0
+            assertEquals(0.0, nodes.first.getConcentration(BIOMOLECULE))
+            assertEquals(1.0, nodes.second.getConcentration(BIOMOLECULE))
         },
     )
 
-private val Int.conditions: Matcher<Reaction<Double>>
-    get() = sizeMatcher("conditions") { it.conditions }
-
-private val Int.neighborConditions: Matcher<Reaction<Double>>
-    get() = sizeMatcher("neighbor conditions") { it.conditions.filter { c -> c is AbstractNeighborCondition } }
-
-private val Int.actions: Matcher<Reaction<Double>>
-    get() = sizeMatcher("actions") { it.actions }
-
-private fun <T> Int.sizeMatcher(
-    collectionName: String,
-    collection: (Reaction<Double>) -> List<T>,
-) = object : Matcher<Reaction<Double>> {
-    override fun test(value: Reaction<Double>): MatcherResult {
-        val actualSize = collection.invoke(value).size
-        return MatcherResult(
-            actualSize == this@sizeMatcher,
-            { "reaction should have ${ this@sizeMatcher } $collectionName but it has $actualSize" },
-            { "reaction should not have ${ this@sizeMatcher } $collectionName conditions but it has" },
-        )
-    }
-}
+private val Reaction<Double>.neighborConditions: List<AbstractNeighborCondition<*>>
+    get() = conditions.filterIsInstance<AbstractNeighborCondition<*>>()

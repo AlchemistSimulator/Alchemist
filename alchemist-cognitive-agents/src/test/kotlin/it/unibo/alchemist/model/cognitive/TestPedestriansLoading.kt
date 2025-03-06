e772bdd94dcc134944a9a17b6078ecf31d1d4b11
Danilo Pianini <danilo.pianini@unibo.it>
2025-03-06 18:53:28 +0100

    test(cognitive-agents): migrate `TestPedestriansLoading` from Kotest to kotlin.test

--- alchemist-cognitive-agents/src/test/kotlin/it/unibo/alchemist/model/cognitive/TestPedestriansLoading.kt
+++ alchemist-cognitive-agents/src/test/kotlin/it/unibo/alchemist/model/cognitive/TestPedestriansLoading.kt
@@ -9,44 +9,52 @@
 
 package it.unibo.alchemist.model.cognitive
 
-import io.kotest.assertions.fail
-import io.kotest.assertions.throwables.shouldThrow
-import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.nulls.shouldNotBeNull
 import it.unibo.alchemist.model.Node.Companion.asPropertyOrNull
 import it.unibo.alchemist.model.Position
 import it.unibo.alchemist.model.geometry.Transformation
 import it.unibo.alchemist.model.geometry.Vector
 import it.unibo.alchemist.test.loadYamlSimulation
 import it.unibo.alchemist.test.startSimulation
+import org.junit.jupiter.api.Test
+import kotlin.test.assertFailsWith
+import kotlin.test.assertNotNull
+import kotlin.test.fail
 
-class TestPedestriansLoading<T, P, A> :
-    StringSpec({
+class TestPedestriansLoading<T, P, A> where P : Position<P>, P : Vector<P>, A : Transformation<P> {
+    @Test
+    fun `Homogeneous pedestrians loading`() {
+        loadYamlSimulation<T, P>("homogeneous-pedestrians.yml").startSimulation()
+    }
 
-        "homogeneous pedestrians loading" {
-            loadYamlSimulation<T, P>("homogeneous-pedestrians.yml").startSimulation()
+    @Test
+    fun `Heterogeneous pedestrians loading`() {
+        loadYamlSimulation<T, P>("heterogeneous-pedestrians.yml").startSimulation()
+    }
+
+    @Test
+    fun `Cognitive pedestrians loading`() {
+        loadYamlSimulation<T, P>("cognitive-pedestrians.yml").startSimulation()
+    }
+
+    @Test
+    fun `Can't give non-cognitive pedestrians cognitive characteristics`() {
+        assertFailsWith<RuntimeException>("An heterogeneous pedestrian can't have cognitive capabilities") {
+            loadYamlSimulation<T, P>("cant-give-cognitive-to-heterogeneous.yml").startSimulation()
+            fail("An exception was expected but not thrown")
         }
+    }
 
-        "heterogeneous pedestrians loading" {
-            loadYamlSimulation<T, P>("heterogeneous-pedestrians.yml").startSimulation()
-        }
-
-        "cognitive pedestrians loading" {
-            loadYamlSimulation<T, P>("cognitive-pedestrians.yml").startSimulation()
-        }
-
-        "can't give non-cognitive pedestrians cognitive characteristics" {
-            shouldThrow<RuntimeException> {
-                loadYamlSimulation<T, P>("cant-give-cognitive-to-heterogeneous.yml").startSimulation()
-                fail("An heterogeneous pedestrian can't have cognitive capabilities")
-            }
-        }
-
-        "groups of pedestrians loading" {
-            loadYamlSimulation<T, P>("groups.yml").startSimulation(
-                onceInitialized = { environment ->
-                    environment.nodes.forEach { it.asPropertyOrNull<T, SocialProperty<T>>().shouldNotBeNull() }
-                },
-            )
-        }
-    }) where P : Position<P>, P : Vector<P>, A : Transformation<P>
+    @Test
+    fun `Groups of pedestrians loading`() {
+        loadYamlSimulation<T, P>("groups.yml").startSimulation(
+            onceInitialized = { environment ->
+                environment.nodes.forEach { node ->
+                    assertNotNull(
+                        node.asPropertyOrNull<T, SocialProperty<T>>(),
+                        "Each pedestrian should have a social property",
+                    )
+                }
+            },
+        )
+    }
+}

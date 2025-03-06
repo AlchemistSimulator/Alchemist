62140ee166612e993f66773cd981622098098dbd
Danilo Pianini <danilo.pianini@unibo.it>
2025-03-06 11:54:20 +0100

    test(loading): auto-detect the latest available LTS version of Ubuntu in embed-mongo and use that for testing on Linux

--- alchemist-loading/src/test/kotlin/it/unibo/alchemist/test/MongoTestUtility.kt
+++ alchemist-loading/src/test/kotlin/it/unibo/alchemist/test/MongoTestUtility.kt
@@ -19,10 +19,12 @@ import de.flapdoodle.os.linux.LinuxDistribution
 import de.flapdoodle.os.linux.UbuntuVersion
 import de.flapdoodle.reverse.StateID
 import de.flapdoodle.reverse.transitions.Start
+import it.unibo.alchemist.util.BugReporting
 import org.slf4j.LoggerFactory
 import de.flapdoodle.net.Net as Network
 
 private val logger by lazy { LoggerFactory.getLogger("TestsWithMongoDB") }
+private val UBUNTU_VERSION_EXTRACTOR = Regex("""^\w+_(\d\d)_(\d\d)$""")
 
 private inline fun <reified T : Any> T.toTransition() = Start.to(StateID.of(T::class.java)).initializedWith(this)
 
@@ -43,8 +45,23 @@ internal fun withMongo(operation: () -> Unit) {
                                     .builder()
                                     .from(detectedPlatform)
                                     .distribution(LinuxDistribution.Ubuntu)
-                                    .version(UbuntuVersion.Ubuntu_20_10)
-                                    .build(),
+                                    .version(
+                                        UbuntuVersion.entries.maxBy {
+                                            val match = UBUNTU_VERSION_EXTRACTOR.matchEntire(it.name)
+                                            checkNotNull(match) {
+                                                BugReporting.reportBug(
+                                                    "Ubuntu version $it does not match the expected pattern",
+                                                    mapOf(
+                                                        "detectedPlatform" to detectedPlatform,
+                                                    ),
+                                                )
+                                            }
+                                            val (yearVersion, monthVersion) = match.destructured
+                                            val year = yearVersion.toInt()
+                                            val month = monthVersion.toInt()
+                                            if (year % 2 == 0 && month == 4) year else Int.MIN_VALUE
+                                        },
+                                    ).build(),
                             )
                         }.getOrThrow()
                 try {

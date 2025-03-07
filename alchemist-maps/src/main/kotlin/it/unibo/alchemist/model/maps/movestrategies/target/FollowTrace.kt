d2960f67c484ad189ff0cc6bff76508d4b3fcc11
Danilo Pianini <danilo.pianini@unibo.it>
2025-03-07 11:06:14 +0100

    fix: `FollowTrace.getTarget()` never returns `null`

--- alchemist-maps/src/main/kotlin/it/unibo/alchemist/model/maps/movestrategies/target/FollowTrace.kt
+++ alchemist-maps/src/main/kotlin/it/unibo/alchemist/model/maps/movestrategies/target/FollowTrace.kt
@@ -6,6 +6,7 @@
  * GNU General Public License, with a linking exception,
  * as described in the file LICENSE in the Alchemist distribution's top directory.
  */
+
 package it.unibo.alchemist.model.maps.movestrategies.target
 
 import it.unibo.alchemist.model.GeoPosition
@@ -16,18 +17,16 @@ import it.unibo.alchemist.model.maps.movestrategies.AbstractStrategyWithGPS
 import it.unibo.alchemist.model.movestrategies.TargetSelectionStrategy
 
 /**
- * This strategy follows a [it.unibo.alchemist.model.Route].
- * @param reaction the reaction
+ * A strategy that follows a [it.unibo.alchemist.model.Route].
  *
- *
+ * @param reaction the associated reaction
  */
 class FollowTrace<T>(private val reaction: Reaction<*>) :
     AbstractStrategyWithGPS(),
     TargetSelectionStrategy<T, GeoPosition> {
-    override fun getTarget(): GPSPoint? {
-        val time = reaction.tau
-        checkNotNull(trace.getNextPosition(time))
-        return trace.getNextPosition(time)
+
+    override fun getTarget(): GPSPoint = requireNotNull(trace.getNextPosition(reaction.tau)) {
+        "No valid position found at time ${reaction.tau}"
     }
 
     override fun cloneIfNeeded(destination: Node<T>, reaction: Reaction<T>): FollowTrace<T> = FollowTrace(reaction)

612747849063008fd5d2369d2dd3b5cc1d4c6d4e
Danilo Pianini <danilo.pianini@gmail.com>
2025-03-12 21:47:06 +0100

    fix(api): make `Transformation` a `fun interface`

--- alchemist-api/src/main/kotlin/it/unibo/alchemist/model/geometry/Transformation.kt
+++ alchemist-api/src/main/kotlin/it/unibo/alchemist/model/geometry/Transformation.kt
@@ -14,7 +14,7 @@ package it.unibo.alchemist.model.geometry
  * The operations allowed depend on the space the shape belongs to.
  * This interface is meant to be extended.
  */
-interface Transformation<S : Vector<S>> {
+fun interface Transformation<S : Vector<S>> {
     /**
      * Performs an absolute translation to the provided position.
      * @param position the new origin of the shape

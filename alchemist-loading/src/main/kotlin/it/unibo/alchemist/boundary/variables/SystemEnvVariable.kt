63dd62d60de0112e12ea784193117953b4a2ad87
Danilo Pianini <danilo.pianini@gmail.com>
2025-04-19 20:53:44 +0200

    fix(loading): improve SystemEnvVariable and make it extend DependentVariable

--- alchemist-loading/src/main/kotlin/it/unibo/alchemist/boundary/variables/SystemEnvVariable.kt
+++ alchemist-loading/src/main/kotlin/it/unibo/alchemist/boundary/variables/SystemEnvVariable.kt
@@ -9,9 +9,8 @@
 
 package it.unibo.alchemist.boundary.variables
 
-import it.unibo.alchemist.boundary.Variable
+import it.unibo.alchemist.boundary.DependentVariable
 import java.io.Serializable
-import java.util.stream.Stream
 
 /**
  * A variable that retrieves its value from a system environment variable.
@@ -19,22 +18,27 @@ import java.util.stream.Stream
  * @param name the name of the environment variable
  * @param defaultValue the default value to return if the environment variable is not set
  */
-class SystemEnvVariable(private val name: String, private val defaultValue: Serializable?) : Variable<Serializable> {
-    constructor(name: String) : this(name, null)
+class SystemEnvVariable @JvmOverloads constructor(
+    private val name: String,
+    private val defaultValue: Serializable? = null,
+) : DependentVariable<Serializable> {
 
-    override fun getDefault(): Serializable {
-        val envValue = System.getenv(name)
-        return envValue?.let { convertToValue(it) }
-            ?: defaultValue
-            ?: error("Environment variable '$name' is not set and no default value is provided.")
+    override fun getWith(variables: MutableMap<String, Any>?): Serializable = when (val value = System.getenv(name)) {
+        null -> defaultValue ?: error("Environment variable '$name' is not set and no default value is provided.")
+        else -> {
+            converters.mapNotNull { it(value) }.firstOrNull() ?: value
+        }
     }
 
-    override fun stream(): Stream<Serializable> = Stream.of(getDefault())
-
-    private fun convertToValue(value: String): Serializable = when {
-        value.toBooleanStrictOrNull() != null -> value.toBoolean()
-        value.toIntOrNull() != null -> value.toInt()
-        value.toDoubleOrNull() != null -> value.toDouble()
-        else -> value
+    private companion object {
+        val converters: Sequence<(String) -> Serializable?> = sequenceOf(
+            String::toBooleanStrictOrNull,
+            String::toIntOrNull,
+            String::toDoubleOrNull,
+            String::toLongOrNull,
+            String::toFloatOrNull,
+            String::toBigIntegerOrNull,
+            String::toBigDecimalOrNull,
+        )
     }
 }

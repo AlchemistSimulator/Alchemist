c766ebf1562b37bc8c777ce9effe1ed3f0adf4b9
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-04-22 17:57:30 +0200

    docs: add documentation

--- alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/terminators/MetricsStableForSteps.kt
+++ alchemist-implementationbase/src/main/kotlin/it/unibo/alchemist/model/terminators/MetricsStableForSteps.kt
@@ -4,11 +4,15 @@ import it.unibo.alchemist.model.Environment
 import it.unibo.alchemist.model.Position
 import it.unibo.alchemist.model.TerminationPredicate
 
+/**
+ * [stepInterval] for how many steps the [metricsToCheck] should be stable.
+ * [equalInterval] how many times it should be stable.
+ */
 class MetricsStableForSteps<T>(
     private val stepInterval: Long,
     private val equalInterval: Long,
     private val metricsToCheck: (Environment<T, Position<*>>) -> Map<String, T>,
-): TerminationPredicate<T, Position<*>> {
+) : TerminationPredicate<T, Position<*>> {
     private var stepsChecked: Long = 0
     private var equalSuccess: Long = 0
     private var lastUpdatedMetrics: Map<String, T> = emptyMap()
@@ -22,17 +26,17 @@ class MetricsStableForSteps<T>(
         require(metrics.isNotEmpty()) {
             "There should be at least one metric to check."
         }
-        return if (lastUpdatedMetrics == metrics) {
-            if (++stepsChecked >= stepInterval){
-                stepsChecked = 0
+        return when {
+            lastUpdatedMetrics == metrics -> {
+                if (++stepsChecked >= stepInterval) stepsChecked = 0
                 ++equalSuccess >= equalInterval
             }
-            else false
-        } else {
-            stepsChecked = 0
-            equalSuccess = 0
-            lastUpdatedMetrics = metrics
-            false
+            else -> {
+                stepsChecked = 0
+                equalSuccess = 0
+                lastUpdatedMetrics = metrics
+                false
+            }
         }
     }
 }

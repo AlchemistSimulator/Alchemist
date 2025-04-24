b4473deaae2630f3574d763dffdf074118a16188
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2025-04-24 16:10:17 +0200

    feat: added alert in case of error

--- alchemist-composeui/src/commonMain/kotlin/it/unibo/alchemist/boundary/composeui/viewmodels/SimulationStatusViewModel.kt
+++ alchemist-composeui/src/commonMain/kotlin/it/unibo/alchemist/boundary/composeui/viewmodels/SimulationStatusViewModel.kt
@@ -11,7 +11,9 @@ package it.unibo.alchemist.boundary.composeui.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.apollographql.apollo3.api.Error
 import it.unibo.alchemist.boundary.graphql.client.GraphQLClientFactory
+import it.unibo.alchemist.boundary.graphql.client.NodesSubscription
 import it.unibo.alchemist.boundary.graphql.client.PauseSimulationMutation
 import it.unibo.alchemist.boundary.graphql.client.PlaySimulationMutation
 import it.unibo.alchemist.boundary.graphql.client.SimulationStatusQuery
@@ -29,9 +31,19 @@ enum class SimulationStatus {
     Terminated,
 }
 
+var i = 0
+var j = 0
+
 class SimulationStatusViewModel : ViewModel() {
-    private val _uiState = MutableStateFlow(SimulationStatus.Init)
-    val uiState = _uiState.asStateFlow()
+    private val _simulationStatus = MutableStateFlow(SimulationStatus.Init)
+    val simulationStatus = _simulationStatus.asStateFlow()
+
+    private val _time = MutableStateFlow(0.0)
+    val time = _time.asStateFlow()
+    private var tempTime = 0.0
+
+    private val _errors = MutableStateFlow<List<Error>>(emptyList())
+    val errors = _errors.asStateFlow()
 
     // TODO: parameterize the host and port and separate client in different file
     private val client = GraphQLClientFactory.subscriptionClient(
@@ -51,14 +63,40 @@ class SimulationStatusViewModel : ViewModel() {
         }
     }
 
+    fun monitor() {
+        _errors.value = emptyList()
+        viewModelScope.launch {
+            client.subscription(NodesSubscription())
+                .toFlow()
+                .collect { response ->
+                    if (response.hasErrors()) {
+                        response.errors?.let { errors ->
+                            _errors.update { errors }
+                        }
+                    }
+                    i++
+                    println("data received $i")
+                    if (i > 99) {
+                        i = 0
+                        j++
+                        println("fetching data $j")
+                        response.data?.let { data ->
+                            _time.value = data.simulation.time
+                        }
+                    }
+                }
+        }
+    }
+
     init {
+        monitor()
         viewModelScope.launch {
             while (true) {
                 client.query(SimulationStatusQuery())
                     .toFlow()
                     .collect { response ->
                         response.data?.let { data ->
-                            _uiState.update {
+                            _simulationStatus.update {
                                 // True correlation can be achieved only moving
                                 // alchemist-api Status enum class to commonMain
                                 when (data.simulation.status) {

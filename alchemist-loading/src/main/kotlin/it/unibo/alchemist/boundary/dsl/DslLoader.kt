b1d946c36158e39227a1c9bc6169e61f7df3c551
marco <marco.frattarola@studio.unibo.it>
2025-09-05 11:51:35 +0200

    feat: add contents handling
    add dsl support for defining contents inside a deployment

--- alchemist-loading/src/main/kotlin/it/unibo/alchemist/boundary/dsl/DslLoader.kt
+++ alchemist-loading/src/main/kotlin/it/unibo/alchemist/boundary/dsl/DslLoader.kt
@@ -10,32 +10,17 @@
 package it.unibo.alchemist.boundary.dsl
 
 import it.unibo.alchemist.boundary.Loader
+import it.unibo.alchemist.boundary.dsl.model.SimulationContext
 import it.unibo.alchemist.core.Engine
 import it.unibo.alchemist.core.Simulation
 import it.unibo.alchemist.model.Environment
 import it.unibo.alchemist.model.Position
-import it.unibo.alchemist.model.SupportedIncarnations
-import org.apache.commons.math3.random.MersenneTwister
 
 abstract class DslLoader(private val ctx: SimulationContext) : Loader {
     @Suppress("UNCHECKED_CAST")
     override fun <T, P : Position<P>> getWith(values: Map<String, *>): Simulation<T, P> {
-        val incarnation = SupportedIncarnations.get<T, P>(ctx.incarnation.name).get()
-        val randomGenerator = MersenneTwister(0)
         val environment = ctx.envCtx?.environment as Environment<T, P>
         // Get deployments and add nodes
-        val deployments = ctx.envCtx?.ctxDeploy?.deployments ?: emptyList()
-        deployments.forEach { deployment ->
-            deployment.forEach { position ->
-                val node = incarnation.createNode(
-                    randomGenerator,
-                    environment,
-                    null,
-                )
-                environment.addNode(node, position as P)
-            }
-        }
-
         return Engine(environment)
     }
 }

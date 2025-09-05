a09052ce2b136c2379d7488e27f1bed3d3dde8ca
marco <marco.frattarola@studio.unibo.it>
2025-09-05 11:51:35 +0200

    feat: add contents handling
    add dsl support for defining contents inside a deployment

--- alchemist-loading/src/main/kotlin/it/unibo/alchemist/boundary/dsl/model/DeploymentsContext.kt
+++ alchemist-loading/src/main/kotlin/it/unibo/alchemist/boundary/dsl/model/DeploymentsContext.kt
@@ -10,16 +10,55 @@
 package it.unibo.alchemist.boundary.dsl.model
 
 import it.unibo.alchemist.model.Deployment
+import it.unibo.alchemist.model.Node
 import it.unibo.alchemist.model.Position
 import org.apache.commons.math3.random.MersenneTwister
 import org.apache.commons.math3.random.RandomGenerator
 
 class DeploymentsContext<T, P : Position<P>>(private val ctx: EnvironmentContext<T, P>) {
     var deployments: MutableList<Deployment<P>> = mutableListOf()
-    var generator: RandomGenerator = MersenneTwister(0)
+    var generator: RandomGenerator = MersenneTwister(10)
+
+    fun deploy(deployment: Deployment<*>, block: ContentsContext.() -> Unit) {
+        @Suppress("UNCHECKED_CAST")
+        deployments.add(deployment as Deployment<P>)
+        addNodes(deployment)
+        ContentsContext().apply(block)
+    }
+    private fun addNodes(deployment: Deployment<P>) {
+        deployment.forEach { position ->
+            val node = ctx.incarnation.createNode(
+                generator,
+                ctx.environment,
+                null,
+            )
+            ctx.environment.addNode(node, position)
+        }
+    }
 
     fun deploy(deployment: Deployment<*>) {
         @Suppress("UNCHECKED_CAST")
-        deployments.add(deployment as Deployment<P>)
+        this.deploy(deployment) {}
+    }
+    inner class ContentsContext {
+        fun all(block: ContentContext<T>.() -> Unit) {
+            val c = ContentContext<T>().apply(block)
+            ctx.environment.nodes.forEach { node ->
+                applyToNode(node, c)
+            }
+        }
+        private fun applyToNode(node: Node<T>, content: ContentContext<T>) {
+            val mol = ctx.incarnation.createMolecule(
+                content.molecule
+                    ?: error("Molecule not specified"),
+            )
+            val conc = ctx.incarnation.createConcentration(content.concentration)
+            node.setConcentration(mol, conc)
+        }
+
+        inner class ContentContext<T> {
+            var molecule: String? = null
+            var concentration: T? = null
+        }
     }
 }

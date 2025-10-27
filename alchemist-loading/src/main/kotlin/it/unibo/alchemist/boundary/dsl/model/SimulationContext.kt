728c00dfcf60eefa61cdedd7c686e0ac2a8ae72d
marco <marco.frattarola@studio.unibo.it>
2025-10-27 17:03:43 +0100

    fix: fix seeds problem

--- alchemist-loading/src/main/kotlin/it/unibo/alchemist/boundary/dsl/model/SimulationContext.kt
+++ alchemist-loading/src/main/kotlin/it/unibo/alchemist/boundary/dsl/model/SimulationContext.kt
@@ -11,13 +11,9 @@
 
 package it.unibo.alchemist.boundary.dsl.model
 
-import it.unibo.alchemist.boundary.DependentVariable
 import it.unibo.alchemist.boundary.Launcher
-import it.unibo.alchemist.boundary.Loader
 import it.unibo.alchemist.boundary.OutputMonitor
 import it.unibo.alchemist.boundary.Variable
-import it.unibo.alchemist.boundary.dsl.DslLoader
-import it.unibo.alchemist.boundary.dsl.model.Incarnation as Inc
 import it.unibo.alchemist.boundary.launchers.DefaultLauncher
 import it.unibo.alchemist.model.Environment
 import it.unibo.alchemist.model.GlobalReaction
@@ -25,20 +21,21 @@ import it.unibo.alchemist.model.Incarnation
 import it.unibo.alchemist.model.Layer
 import it.unibo.alchemist.model.LinkingRule
 import it.unibo.alchemist.model.Position
-import it.unibo.alchemist.model.SupportedIncarnations
 import it.unibo.alchemist.model.TerminationPredicate
-import it.unibo.alchemist.model.environments.Continuous2DEnvironment
 import it.unibo.alchemist.model.linkingrules.NoLinks
-import it.unibo.alchemist.model.positions.Euclidean2DPosition
 import java.io.Serializable
-import kotlin.jvm.optionals.getOrElse
+import org.apache.commons.math3.random.MersenneTwister
+import org.apache.commons.math3.random.RandomGenerator
 
 class SimulationContext<T, P : Position<P>>(val incarnation: Incarnation<T, P>, val environment: Environment<T, P>) {
     val buildSteps: MutableList<() -> Unit> = mutableListOf()
     val monitors: MutableList<OutputMonitor<T, P>> = mutableListOf()
     val exporters: MutableList<ExporterContext<T, P>> = mutableListOf()
     var launcher: Launcher = DefaultLauncher()
+    var scenarioGenerator: RandomGenerator = MersenneTwister(0L)
+    var simulationGenerator: RandomGenerator = MersenneTwister(0L)
 
+    // deploymemt -> node -> program (time distribution -> actionable ( simulation))
     private val layers: MutableMap<String, Layer<T, P>> = HashMap()
     private var _networkModel: LinkingRule<T, P> = NoLinks()
     var networkModel: LinkingRule<T, P>
@@ -89,32 +86,4 @@ class SimulationContext<T, P : Position<P>>(val incarnation: Incarnation<T, P>, 
         variablesContext.register(source)
     fun <T : Serializable> variable(source: () -> T): VariablesContext.DependentVariableProvider<T> =
         variablesContext.dependent(source)
-}
-
-fun <T, P : Position<P>> createLoader(dsl: SimulationContext<T, P>): Loader = object : DslLoader(dsl) {
-    override val constants: Map<String, Any?> = emptyMap() // not needed
-    override val dependentVariables: Map<String, DependentVariable<*>> = emptyMap() // not needed
-    override val variables: Map<String, Variable<*>> = dsl.variablesContext.variables
-    override val remoteDependencies: List<String> = emptyList() // TODO: to implement
-    override val launcher: Launcher = dsl.launcher
-}
-
-fun <T, P : Position<P>> Inc.incarnation(): Incarnation<T, P> = SupportedIncarnations.get<T, P>(this.name).getOrElse {
-    throw IllegalArgumentException("Incarnation $this not supported")
-}
-
-fun <T, P : Position<P>> simulation(
-    incarnation: Incarnation<T, P>,
-    environment: Environment<T, P>,
-    block: SimulationContext<T, P>.() -> Unit,
-): Loader = createLoader(SimulationContext(incarnation, environment).apply(block))
-
-fun <T, P : Position<P>> simulation(
-    incarnation: Incarnation<T, P>,
-    block: SimulationContext<T, Euclidean2DPosition>.() -> Unit,
-): Loader {
-    @Suppress("UNCHECKED_CAST")
-    val defaultEnv = Continuous2DEnvironment(incarnation as Incarnation<T, Euclidean2DPosition>)
-    val ctx = SimulationContext(incarnation, defaultEnv).apply(block)
-    return createLoader(ctx)
 }

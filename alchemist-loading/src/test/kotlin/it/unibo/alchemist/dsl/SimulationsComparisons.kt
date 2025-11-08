05388de02f0cca7a90aefb16fa3c9eb8f9fe52e3
marco <marco.frattarola@studio.unibo.it>
2025-11-08 13:38:26 +0100

    refactor: add doc and better testing

--- alchemist-loading/src/test/kotlin/it/unibo/alchemist/dsl/SimulationsComparisons.kt
+++ alchemist-loading/src/test/kotlin/it/unibo/alchemist/dsl/SimulationsComparisons.kt
@@ -15,86 +15,86 @@ class SimulationsComparisons {
 
     @Test
     fun <T, P : Position<P>> test01() {
-        { DslLoaderFunctions.test01Nodes<T, P>() }.shouldEqual("dsl/01-nodes.yml")
+        { DslLoaderFunctions.test01Nodes<T, P>() }.shouldEqual("dsl/yml/01-nodes.yml")
     }
 
     @Test
     fun <T, P : Position<P>> test02() {
-        { DslLoaderFunctions.test02ManyNodes<T, P>() }.shouldEqual("dsl/02-manynodes.yml")
+        { DslLoaderFunctions.test02ManyNodes<T, P>() }.shouldEqual("dsl/yml/02-manynodes.yml")
     }
 
     @Test
     fun <T, P : Position<P>> test03() {
-        { DslLoaderFunctions.test03Grid<T, P>() }.shouldEqual("dsl/03-grid.yml")
+        { DslLoaderFunctions.test03Grid<T, P>() }.shouldEqual("dsl/yml/03-grid.yml")
     }
 
     @Test
     fun <T, P : Position<P>> test05() {
-        { DslLoaderFunctions.test05Content<T, P>() }.shouldEqual("dsl/05-content.yml")
+        { DslLoaderFunctions.test05Content<T, P>() }.shouldEqual("dsl/yml/05-content.yml")
     }
 
     @Test
     fun <T, P : Position<P>> test06() {
-        { DslLoaderFunctions.test06ContentFiltered<T, P>() }.shouldEqual("dsl/06-filters.yml")
+        { DslLoaderFunctions.test06ContentFiltered<T, P>() }.shouldEqual("dsl/yml/06-filters.yml")
     }
 
     @Test
     fun <T, P : Position<P>> test07() {
-        { DslLoaderFunctions.test07Programs<T, P>() }.shouldEqual("dsl/07-program.yml")
+        { DslLoaderFunctions.test07Programs<T, P>() }.shouldEqual("dsl/yml/07-program.yml")
     }
 
     @Test
     fun <T, P : Position<P>> test08() {
-        DslLoaderFunctions.test08ProtelisPrograms<T, P>().shouldEqual("dsl/08-protelisprogram.yml")
+        DslLoaderFunctions.test08ProtelisPrograms<T, P>().shouldEqual("dsl/yml/08-protelisprogram.yml")
     }
 
     @Test
     fun <T, P : Position<P>> test09() {
-        { DslLoaderFunctions.test09TimeDistribution<T, P>() }.shouldEqual("dsl/09-timedistribution.yml")
+        { DslLoaderFunctions.test09TimeDistribution<T, P>() }.shouldEqual("dsl/yml/09-timedistribution.yml")
     }
 
     @Test
     fun <T, P : Position<P>> test10() {
-        { DslLoaderFunctions.test10Environment<T, P>() }.shouldEqual("dsl/10-environment.yml")
+        { DslLoaderFunctions.test10Environment<T, P>() }.shouldEqual("dsl/yml/10-environment.yml")
     }
 
     @Test
     fun <T, P : Position<P>> test11() {
-        { DslLoaderFunctions.test11monitors<T, P>() }.shouldEqual("dsl/11-monitors.yml")
+        { DslLoaderFunctions.test11monitors<T, P>() }.shouldEqual("dsl/yml/11-monitors.yml")
     }
 
     @Test
     fun <T, P : Position<P>> test12() {
-        { DslLoaderFunctions.test12Layers<T, P>() }.shouldEqual("dsl/12-layers.yml")
+        { DslLoaderFunctions.test12Layers<T, P>() }.shouldEqual("dsl/yml/12-layers.yml")
     }
 
     @Test
     fun <T, P : Position<P>> test13() {
-        { DslLoaderFunctions.test13GlobalReaction<T, P>() }.shouldEqual("dsl/13-globalreaction.yml")
+        { DslLoaderFunctions.test13GlobalReaction<T, P>() }.shouldEqual("dsl/yml/13-globalreaction.yml")
     }
 
     @Test
     fun <T, P : Position<P>> test14() {
-        { DslLoaderFunctions.test14Exporters<T, P>() }.shouldEqual("dsl/14-exporters.yml")
+        { DslLoaderFunctions.test14Exporters<T, P>() }.shouldEqual("dsl/yml/14-exporters.yml")
     }
 
     @Test
     fun <T, P : Position<P>> test15() {
-        { DslLoaderFunctions.test15Variables<T, P>() }.shouldEqual("dsl/15-variables.yml")
+        { DslLoaderFunctions.test15Variables<T, P>() }.shouldEqual("dsl/yml/15-variables.yml")
     }
 
     @Test
     fun <T, P : Position<P>> test16() {
-        { DslLoaderFunctions.test16ProgramsFilters<T, P>() }.shouldEqual("dsl/16-programsfilters.yml")
+        { DslLoaderFunctions.test16ProgramsFilters<T, P>() }.shouldEqual("dsl/yml/16-programsfilters.yml")
     }
 
     @Test
     fun <T, P : Position<P>> test17() {
-        { DslLoaderFunctions.test17CustomNodes<T, P>() }.shouldEqual("dsl/17-customnodes.yml")
+        { DslLoaderFunctions.test17CustomNodes<T, P>() }.shouldEqual("dsl/yml/17-customnodes.yml")
     }
 
     @Test
     fun <T, P : Position<P>> test18() {
-        { DslLoaderFunctions.test18NodeProperties<T, P>() }.shouldEqual("dsl/18-properties.yml")
+        { DslLoaderFunctions.test18NodeProperties<T, P>() }.shouldEqual("dsl/yml/18-properties.yml")
     }
 }

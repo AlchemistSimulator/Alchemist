40d8e63e65f500d4505cd25a6d08ea6322e0864b
S-furi <stefano.furi7@gmail.com>
2026-02-19 15:28:56 +0100

    fix(sapere): keep observing for neighbrhoods LSA spaces changes

--- alchemist-incarnation-sapere/src/main/java/it/unibo/alchemist/model/sapere/conditions/LsaStandardCondition.java
+++ alchemist-incarnation-sapere/src/main/java/it/unibo/alchemist/model/sapere/conditions/LsaStandardCondition.java
@@ -49,7 +49,7 @@ public class LsaStandardCondition extends AbstractLsaCondition {
     public LsaStandardCondition(final ILsaMolecule mol, final ILsaNode n) {
         super(n, Sets.newHashSet(new ILsaMolecule[] {mol}));
 
-        addObservableDependency(n.observeConcentration(mol.generalize()));
+        addObservableDependency(n.observeLsaSpace());
         setValidity(valid);
         setPropensity(MutableObservable.Companion.observe(-1d));
 

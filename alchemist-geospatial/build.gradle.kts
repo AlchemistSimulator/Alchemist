e4e0fbfa9643f8ef08c8c082b9dc0bbcc9ff3bd9
Emir Wanes Aouioua <emirwanes.aouioua@studio.unibo.it>
2026-06-20 17:34:01 +0200

    chore(geospatial): add basic gradle configuration

---
+++ alchemist-geospatial/build.gradle.kts
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2010-2026, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+import Libs.alchemist
+
+plugins {
+    id("kotlin-jvm-convention")
+}
+
+dependencies {
+    api(alchemist("api"))
+    implementation(libs.cdm.core)
+    compileOnly(libs.cdm.grib)
+    implementation(libs.gson)
+    implementation(libs.slf4j)
+}
+
+publishing.publications {
+    withType<MavenPublication> {
+        pom {
+            contributors {
+                contributor {
+                    name.set("Emir Wanes Aouioua")
+                    email.set("emirwanes.aouioua@studio.unibo.it")
+                }
+            }
+        }
+    }
+}

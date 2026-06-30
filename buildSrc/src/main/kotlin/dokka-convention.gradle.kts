77bbe2c8decb38d99b1845e1cf96176c2ec576b9
Danilo Pianini <danilo.pianini@unibo.it>
2026-06-30 21:25:03 +0200

    build: remove duplicate copyright block from dokka-convention.gradle.kts

--- buildSrc/src/main/kotlin/dokka-convention.gradle.kts
+++ buildSrc/src/main/kotlin/dokka-convention.gradle.kts
@@ -17,14 +17,6 @@ import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 import org.jetbrains.dokka.gradle.tasks.DokkaBaseTask
 
-/*
- * Copyright (C) 2010-2024, Danilo Pianini and contributors
- * listed, for each module, in the respective subproject's build.gradle.kts file.
- *
- * This file is part of Alchemist, and is distributed under the terms of the
- * GNU General Public License, with a linking exception,
- * as described in the file LICENSE in the Alchemist distribution's top directory.
- */
 plugins {
     id("org.jetbrains.dokka")
 }

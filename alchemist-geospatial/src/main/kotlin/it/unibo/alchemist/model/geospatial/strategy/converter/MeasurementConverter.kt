415cd8dd7e0393341a0b34e19443203d1fddec1c
Emir Wanes Aouioua <emirwanes.aouioua@studio.unibo.it>
2026-08-06 18:19:41 +0200

    feat(geospatial): replace missing value strategy with a raw Double convertion one

---
+++ alchemist-geospatial/src/main/kotlin/it/unibo/alchemist/model/geospatial/strategy/converter/MeasurementConverter.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2010-2026, Danilo Pianini and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of Alchemist, and is distributed under the terms of the
+ * GNU General Public License, with a linking exception,
+ * as described in the file LICENSE in the Alchemist distribution's top directory.
+ */
+
+package it.unibo.alchemist.model.geospatial.strategy.converter
+
+import java.io.Serializable
+
+/**
+ * Converts a raw `Double` value read from a GRIB / NetCDF file, into a value of type [T].
+ *
+ * Implementations of this interface must handle the conversion logic,
+ * taking into account that any missing/fill value is encoded as [Double.NaN].
+ *
+ * @param T the data type resulting from the conversion.
+ */
+fun interface MeasurementConverter<T> : Serializable {
+
+    /**
+     * Converts the raw value.
+     *
+     * @param value the numeric value to be converted.
+     * **Note:** it may be [Double.NaN] if it represents a missing/fill value.
+     * @return the converted value of type [T].
+     */
+    fun convert(value: Double): T
+}

23dce5653027f3804410bec8eda653525df6cc18
Nathekip <nathaniel.guitton@etudiant.univ-rennes1.fr>
2026-08-10 15:51:32 +0200

    feat: allow to round values in CSVExporter

--- alchemist-loading/src/test/kotlin/it/unibo/alchemist/test/TestCSVExporter.kt
+++ alchemist-loading/src/test/kotlin/it/unibo/alchemist/test/TestCSVExporter.kt
@@ -85,6 +85,29 @@ class TestCSVExporter<T, P : Position<P>> :
             exporterFirstLine.shouldNotBeEmpty()
             exporterFirstLine.shouldContain("0 1 2 3")
         }
+        "should round every column independently" {
+            val simulation: Simulation<T, P> = loadAlchemist("testCSVExporterMultipleColumns.yml")
+            simulation.runInCurrentThread()
+
+            val exporter = simulation.csvExporters().first()
+
+            val outputFile = File(exporter.exportPath)
+                .listFiles()
+                ?.first { it.name.startsWith("multiple-columns") }
+                .shouldNotBeNull()
+
+            val firstDataLine = outputFile
+                .readLines()
+                .first { line -> line.isNotBlank() && !line.startsWith("#") }
+
+            firstDataLine.split(" ") shouldBe listOf(
+                "0.0",
+                "1.23",
+                "2.35",
+                "3.0",
+                "4.57",
+            )
+        }
     }) {
     // common utility functions
     companion object {

7a7e870511aad1089e20cca6ab08e9e07a9a6261
Emir Wanes Aouioua <emirwanes.aouioua@studio.unibo.it>
2026-08-19 16:27:10 +0200

    build(maps): stop using a protobuf fixed version

--- alchemist-maps/build.gradle.kts
+++ alchemist-maps/build.gradle.kts
@@ -42,16 +42,6 @@ dependencies {
     testRuntimeOnly(incarnation("protelis"))
 }
 
-configurations.all {
-    resolutionStrategy {
-        eachDependency {
-            if (requested.group == "com.google.protobuf" && requested.name == "protobuf-java") {
-                useVersion("3.12.2")
-            }
-        }
-    }
-}
-
 publishing.publications {
     withType<MavenPublication> {
         pom {
